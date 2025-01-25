d2569bb3eb4049cc4cd464241dd6f8e3d6984332
Sigit Septiadi <sigitseptiadi1@gmail.com>
2025-01-25 20:00:32 +0700

    dfhhd

--- resources/views/frontend/00_atas/footer1.blade.php
+++ resources/views/frontend/00_atas/footer1.blade.php
@@ -10,15 +10,20 @@
                 <img src="/assets/icon/sipjakikbb.png" alt="Logo SIPJAKIKBB" style="margin-bottom: 10px; width: 58px; height: 58px; object-fit: cover; margin-right: 10px; padding: 0;">
                     
                 <h2 style="margin: 0;">
-                    <span style="color: white">Mas Jaki</span><span style="color: #28a745;">Blora</span>
+                    <span style="color: white">Mas Jaki</span><span style="color: #28a745;"> Blora</span>
                 </h2>
                 <p style="margin: 0; font-size: 16px; color: white" >Dinas Pekerjaan Umum & Penataan Ruang</p>
                 <p style="margin: 0; font-size: 16px; color: white" >Pemerintah Kabupaten Blora</p>
+                <p style="margin: 0; font-size: 16px; color: white" >Bidang Bangunan Gedung</p>
                         <br>
                     
                 {{-- <br><br><br><br><br> --}}
                 <p style="text-align: left">
-                    Sistem Informasi Pembina Jasa Konstruksi Pemerintah Kabupaten Bandung Barat adalah platform digital untuk meningkatkan efisiensi dan transparansi dalam pengelolaan proyek konstruksi. Sistem ini memantau pelaksanaan proyek, memastikan kepatuhan terhadap standar, dan memfasilitasi komunikasi antara pemerintah, kontraktor, pengawas, tenaga ahli, dan masyarakat. Hal ini diharapkan dapat meningkatkan kualitas infrastruktur dan mempercepat proses administrasi.
+                    Tentu! Berikut adalah versi yang telah diperbarui dengan "Pemerintah Kabupaten Blora":
+
+                    Sistem Informasi Pembina Jasa Konstruksi Pemerintah Kabupaten Blora adalah platform digital untuk meningkatkan efisiensi dan transparansi dalam pengelolaan proyek konstruksi. Sistem ini memantau pelaksanaan proyek, memastikan kepatuhan terhadap standar, dan memfasilitasi komunikasi antara pemerintah, kontraktor, pengawas, tenaga ahli, dan masyarakat. Hal ini diharapkan dapat meningkatkan kualitas infrastruktur dan mempercepat proses administrasi.
+                    
+                    Semoga sesuai dengan yang Anda inginkan!
                 </p>
                 <br><br><br><br><br>
                 <a href="mailto:your-email@example.com"><i class="fas fa-envelope"></i></a>
@@ -52,33 +57,31 @@
                 
             </div>
 
-            <div class="posts">
-                
+            {{-- <div class="posts">     
                 <h1 style="color: yellow">Berita Terakhir</h1>
                 <a href="#"><span>02<br>Juli</span>Pemerintah Kabupaten Bandung Barat Luncurkan SIPJAKI untuk Tingkatkan Transparansi Proyek Konstruksi</a>
-                {{-- <a href="#"><span>05<br>Juli</span>SIPJAKI: Inovasi Digital dalam Pengawasan Konstruksi di Bandung Barat</a> --}}
                 <a href="#"><span>07<br>Juli</span>Efisiensi Pengelolaan Proyek Konstruksi Melalui SIPJAKI di Kabupaten Bandung Barat</a>
                 <a href="#"><span>11<br>Juli</span>SIPJAKI Permudah Komunikasi antara Pemerintah dan Kontraktor Konstruksi di Bandung Barat</a>
                 <a href="#"><span>12<br>Juli</span>Peningkatan Kualitas Infrastruktur dengan SIPJAKI di Kabupaten Bandung Barat</a>
                 <a href="#"><span>15<br>Juli</span>Dukung Pembangunan Berkelanjutan, Kabupaten Bandung Barat Implementasikan SIPJAKI</a>
-                {{-- <a href="#"><span>16<br>Juli</span>SIPJAKI: Solusi Modern untuk Pengelolaan Proyek Konstruksi di Bandung Barat</a>
-                <a href="#"><span>17<br>Juli</span>Optimalisasi Proses Administrasi Proyek Konstruksi dengan SIPJAKI di Bandung Barat</a> --}}
-            </div>
+            </div> --}}
 
             
             <div class="contact">
-                <h1 style="color: yellow">Kontak Kami</h1>
+                <h1 style="color: green">Kontak Kami</h1>
                 <div style="width: 250px; height: 250px; margin-bottom: 10px;">
                     <iframe
                         width="250"
                         height="250"
                         frameborder="0" style="border:0"
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31673.108163505292!2d107.52254065710834!3d-6.867200429149782!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e5c72a86f287%3A0xe498330891b4d840!2sBandung%20Barat%2C%20West%20Java!5e0!3m2!1sen!2sid!4v1624103506424!5m2!1sen!2sid"
+                        src="
+                        https://www.google.com/maps/place/Dinas+Pekerjaan+Umum+dan+Penataan+Ruang+(DPUPR)+Kabupaten+Blora/@-6.9771767,111.4085298,806m/data=!3m2!1e3!4b1!4m6!3m5!1s0x2e77411bc797c1ed:0x5ddba5997190e35c!8m2!3d-6.977182!4d111.4111047!16s%2Fg%2F11b6dpfgz4!5m1!1e1?entry=ttu&g_ep=EgoyMDI1MDEyMi4wIKXMDSoASAFQAw%3D%3D"
+                        
                         allowfullscreen>
                     </iframe>
                 </div>
-                <p> <a href="https://bit.ly/PemKabBandungBarat">PemKab Bandung Barat</a></p>
-                <p style="text-align: left">5G56+HX3, Mekarsari, Kec. Ngamprah, Kabupaten Bandung Barat, Jawa Barat 40552 Indonesia</p>
+                <p> <a href="https://www.blorakab.go.id">Pemerintah Kabupaten Blora</a></p>
+                <p style="text-align: left">Jl. Nusantara No.62, Jetis, Kauman, Kec. Blora, Kabupaten Blora, Jawa Tengah 58214</p>
                 {{-- <p>Hello@contact-us.com</p> --}}
                 {{-- <p><a href=""></a> +6281 321 455 855</p> --}}
                 <p><a href="https://wa.me/6281321455855"><i class="fab fa-whatsapp me-2"></i>081321455855</a></p>
