ee07ef0935c876c7bc0e99b4d255e94d9bca9cb7
Sigit Septiadi <sigitseptiadi1@gmail.com>
2025-01-27 12:47:53 +0700

    dhfj

--- resources/views/frontend/00_atas/header1.blade.php
+++ resources/views/frontend/00_atas/header1.blade.php
@@ -68,7 +68,7 @@
         }
     </style>
      <div class="marquee-container" style="margin-left: 150px;">
-        <div class="marquee" style="color: white">Selamat Datang ! di Web Resmi Mas Jaki Blora Sistem Informasi Pembina Jasa Konstruksi Pemerintah Kabupaten Blora Jawa Tengah</div>
+        <div class="marquee" style="color: white">Selamat Datang ! di Web Resmi Mas Jaki Blora Sistem Informasi Pembina Jasa Konstruksi Pemerintah Kabupaten Blora Provinsi Jawa Tengah</div>
     </div>
     
         {{-- <p>Jam </p> --}}
