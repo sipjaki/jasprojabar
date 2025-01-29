7fc93150dc3b3eb0cb12f0e6d0eb4422b8a145c6
Sigit Septiadi <sigitseptiadi1@gmail.com>
2025-01-29 11:04:52 +0700

    dfh

--- resources/views/frontend/00_atas/header2_navbar.blade.php
+++ resources/views/frontend/00_atas/header2_navbar.blade.php
@@ -1,8 +1,8 @@
 <div class="container mt-0 mb-0">  
     <div style="display: flex; align-items: center; ">
         {{-- <img src="/assets/icon/pupr.png" alt="Logo SIPJAKIKBB" style="width: 50px; height: 50px; object-fit: cover; padding: 0;"> --}}
-        <img src="/assets/icon/logokabupatenblora.png" alt="Logo SIPJAKIKBB" style="width: 50px; height: 50px; object-fit: cover; padding: 0; margin-right:20px;">
-        <h1 style="margin: 0;">
+        <img src="/assets/icon/logokabupatenblora.png" alt="Logo SIPJAKIKBB" style="width: 50px; height: 50px; object-fit: cover; padding: 0;">
+        <h1 style="margin: 100px;">
             <span>MASJAKI</span>BLORA
             <p style="margin: 0; font-size: 16px; color: white" >Dinas Pekerjaan Umum & Penataan Ruang</p>
         </h1>
