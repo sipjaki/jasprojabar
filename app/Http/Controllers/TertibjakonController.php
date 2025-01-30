cc0f6010a960c566dec33c0038afe97b599b4632
Sigit Septiadi <sigitseptiadi1@gmail.com>
2025-01-31 01:48:47 +0700

    'dfg'

--- app/Http/Controllers/TertibjakonController.php
+++ app/Http/Controllers/TertibjakonController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\statustertibjakon;
 use App\Models\tertibjakon;
 use Illuminate\Http\Request;
 
@@ -12,10 +13,13 @@ class TertibjakonController extends Controller
     {
         $data_tertibjakon = tertibjakon::paginate(10); // Menggunakan paginate() untuk pagination
         $data_total = tertibjakon::count(); // Menggunakan paginate() untuk pagination
+        $data_statustertibjakon = statustertibjakon::all(); // Menggunakan paginate() untuk pagination
+
 
         return view('frontend.06_3_bloratertibjakon.index', [
             'title' => 'Tertib Jasa Konstruksi',
             'data' => $data_tertibjakon, // Mengirimkan data paginasi ke view
+            'data_statustertibjakon' => $data_statustertibjakon, // Mengirimkan data paginasi ke view
             'totaldata' => $data_total, // Mengirimkan data paginasi ke view
         ]);
     }
