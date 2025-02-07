d6dbe0f8f0c4a8fcfc69ff8073552868e56ffaeb
Sigit Septiadi <sigitseptiadi1@gmail.com>
2025-02-07 17:54:09 +0700

    dfg

--- resources/views/frontend/00_full/masjakiblora.blade.php
+++ resources/views/frontend/00_full/masjakiblora.blade.php
@@ -55,13 +55,13 @@
                   Kelembagaan
                 </a>
                 <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="about.html">About</a></li>
-                  <li><a class="dropdown-item" href="packages.html">Pricing</a></li>
-                  <li><a class="dropdown-item" href="faqs.html">FAQs</a></li>
-                  <li><a class="dropdown-item" href="privacy-policy.html">Terms & Conditions</a></li>
+                  <li><a class="dropdown-item" href="about.html">Struktur DPUPR Kab Blora</a></li>
+                  <li><a class="dropdown-item" href="packages.html">Renstra Kab Blora</a></li>
+                  <li><a class="dropdown-item" href="faqs.html">Tupoksi Jasa Konstruksi</a></li>
+                  {{-- <li><a class="dropdown-item" href="privacy-policy.html">Terms & Conditions</a></li>
                   <li><a class="dropdown-item" href="privacy-policy.html">Privacy Policy</a></li>
                   <li><a class="dropdown-item" href="blogs.html">Blogs</a></li>
-                  <li><a class="dropdown-item" href="blog-details.html">Blog Detail Page</a></li>
+                  <li><a class="dropdown-item" href="blog-details.html">Blog Detail Page</a></li> --}}
                 </ul>
               </li>
 
