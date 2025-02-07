4a723c7fffbbc96b10c2098d96dbad18dc99b22f
Sigit Septiadi <sigitseptiadi1@gmail.com>
2025-02-07 22:51:35 +0700

    dfg

--- resources/views/frontend/00_full/bahanbahan.blade.php
+++ resources/views/frontend/00_full/bahanbahan.blade.php
@@ -565,8 +565,8 @@
     <!-- ! Start FAQ's -->
     <div class="faqs" id="faqs">
       <div class="header" data-aos="fade-up" data-aos-delay="100">
-        <div class="title">FAQ's</div>
-        <p class="para">Lorem ipsum dolor sit amet</p>
+        <div class="title">FAQ's Jasa Konstruksi</div>
+        <p class="para">Pertanyaan Seputar Jasa Konstruksi Kabupaten Blora Provinsi Jawa Tengah</p>
       </div>
       <div class="container" data-aos="fade-up">
         <div class="row gy-4">
