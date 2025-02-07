d54cfdf7cc7f38f3910806ec076a5264ac9d845d
Sigit Septiadi <sigitseptiadi1@gmail.com>
2025-02-07 21:43:51 +0700

    dfg

--- resources/views/frontend/00_full/bahanbahan.blade.php
+++ resources/views/frontend/00_full/bahanbahan.blade.php
@@ -1,31 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Technoit</title>
-  <!-- ! Page Title Icon -->
-  <link rel="icon" href="/assets/masjakiblora/images/microchip-solid.svg" type="image/svg+xml">
-  <!-- ! Font Awesome Library -->
-  <link rel="stylesheet" href="/assets/masjakiblora/css/all.min.css">
-  <!-- ! Bootstrap -->
-  <link rel="stylesheet" href="/assets/masjakiblora/css/bootstrap.min.css">
-  <!-- ! Main Template CSS File -->
-  <link rel="stylesheet" href="/assets/masjakiblora/css/style.css">
-  <link rel="stylesheet" href="/assets/masjakiblora/css/darkMode.css">
-  <link rel="stylesheet" href="/assets/masjakiblora/css/aos.css">
-  <!-- ! Fonts -->
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&display=swap">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap">
-  <link rel="stylesheet"
-    href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Raleway:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap">
-  <!-- ! AOS Library -->
-  <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
-  <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-</head>
+@include('frontend.00_masjakibloraweb.header')
 
 <body data-aos-easing="ease-in-out" data-aos-duration="1000" data-aos-delay="0">
   <!-- ! Start Header -->
