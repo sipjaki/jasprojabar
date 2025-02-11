25b17c5e8ed21ecb4a7c03c584da9231365ba2be
Sigit Septiadi <sigitseptiadi1@gmail.com>
2025-02-11 13:36:21 +0700

    dfg

--- resources/views/frontend/00_full/masjakiblora.blade.php
+++ resources/views/frontend/00_full/masjakiblora.blade.php
@@ -55,11 +55,12 @@
         <div class="container">
           <ol class="breadcrumb" data-aos-delay="700">
             <li class="breadcrumb-item Home">
-              <a href="/" style="color: black">
-                <marquee behavior="scroll" direction="center" scrollamount="15" scrolldelay="0">
-                  Selamat Datang di Mas Jaki DPUPR Kabupaten Blora | Sistem Informasi Pembina Jasa Konsrtuksi Kabupaten Blora
+              <div style="display: flex; justify-content: center; align-items: center; height: 50px; overflow: hidden;">
+                <marquee behavior="scroll" direction="left" scrollamount="10">
+                    Selamat Datang di Mas Jaki DPUPR Kabupaten Blora | Sistem Informasi Pembina Jasa Konstruksi Kabupaten Blora
                 </marquee>
-              </a>
+            </div>
+            
               
             </li>
           </ol>
