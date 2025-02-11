34c0d8413589144cd90dda586f90145602f27858
Sigit Septiadi <sigitseptiadi1@gmail.com>
2025-02-11 11:51:29 +0700

    dfg

--- resources/views/frontend/00_full/masjakiblora.blade.php
+++ resources/views/frontend/00_full/masjakiblora.blade.php
@@ -52,7 +52,7 @@
       </div>
       <nav aria-label="breadcrumb" style="background: #09ff00d7;">
         <ol>
-        <div class="container" style="height: 5vh; width: 100%; margin-top: 15px; padding: 0;">
+        <div class="container" style="width: 100%; margin-top: 5px; padding: 0;">
           <!-- Running Text / Marquee Effect in Breadcrumb -->
           <div class="running-text" style="background: #F7D300; overflow: hidden; width: 100%; border-radius: 20px;">
             <p style="font-size: 14px; font-weight: bold; color: black; margin: 0;">
