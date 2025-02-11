fb1778933424b5c133c574ab45febbe3fc1340ca
Sigit Septiadi <sigitseptiadi1@gmail.com>
2025-02-11 13:59:14 +0700

    dfg

--- resources/views/frontend/00_masjakibloraweb/footer1.blade.php
+++ resources/views/frontend/00_masjakibloraweb/footer1.blade.php
@@ -1,4 +1,4 @@
-<footer id="footer" class="footer-section">
+<footer id="footer" class="footer-section" style="background: url('/assets/masjakiblora/images/sisteminformasi.jpg') center/cover;">
     <div class="container">
       <div class="footer-content pt-5 pb-5">
         <div class="row">
