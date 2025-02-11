8bdee9aad978d720fee4f9564a2e08bee351bbef
Sigit Septiadi <sigitseptiadi1@gmail.com>
2025-02-11 09:49:11 +0700

    dfgfg

---
+++ resources/views/frontend/00_masjakibloraweb/menunavbar.blade.php
@@ -0,0 +1,154 @@
+<header class="d-flex align-items-center fixed-top justify-content-around bg-transparent">
+    <!-- ! Start Navbar -->
+    <nav class="navbar navbar-expand-lg">
+      <div class="container">
+        <a class="navbar-brand" href="index.html">
+          <img src="/assets/icon/pupr.png" alt="Technoit" class="img-fluid" style="margin-right:5px; width:75%;">
+          <img src="/assets/icon/logokabupatenblora.png" alt="Technoit" class="img-fluid" style="width:75%; margin-right:7px;">
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span>
+            <i class="fa-solid fa-sliders"></i>
+          </span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item" style="font-size: 14px;">
+              <a style="margin-left: 20px;" class="nav-link" aria-current="page" href="/">Beranda</a>
+            </li>
+
+            <li class="nav-item dropdown" style="font-size:14px;">
+                <a class="nav-link dropdown-toggle" href="/404" role="button" data-bs-toggle="dropdown"
+                  aria-expanded="false">
+                  Kelembagaan
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="/404">Struktur DPUPR Kab Blora</a></li>    
+                    <li><a class="dropdown-item" href="/404">Renstra Program Jasa Konstruksi</a></li>    
+                    <li><a class="dropdown-item" href="/404">Tupoksi Jasa Konstruksi</a></li>    
+                </ul>
+              </li>
+
+              <li class="nav-item dropdown" style="font-size:14px;">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                  aria-expanded="false">
+                  Berita
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="/404">Jasa Konstruksi</a></li>    
+                    <li><a class="dropdown-item" href="/404">Dokumen Sertifikasi</a></li>    
+                    <li><a class="dropdown-item" href="/404">Agenda Sertifikasi</a></li>    
+                </ul>
+              </li>
+            
+              <li class="nav-item dropdown" style="font-size:14px;">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                  aria-expanded="false">
+                  Data Jakon
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="/404">BUJK Kontraktor</a></li>    
+                    <li><a class="dropdown-item" href="/404">BUJK Konsultan</a></li>    
+                    <li><a class="dropdown-item" href="/404">Tenaga Kerja</a></li>    
+                </ul>
+              </li>
+            
+              <li class="nav-item dropdown" style="font-size:14px;">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                  aria-expanded="false">
+                  Pembinaan
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="/404">Pelatihan</a></li>    
+                    <li><a class="dropdown-item" href="/404">Bimbingan Teknis</a></li>    
+                    <li><a class="dropdown-item" href="/404">SKK Tenaga Ahli</a></li>    
+                    <li><a class="dropdown-item" href="/404">Workshop</a></li>    
+                    <li><a class="dropdown-item" href="/404">Daftar Tenaga Ahli</a></li>    
+                </ul>
+              </li>
+            
+              <li class="nav-item dropdown" style="font-size:14px;">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                  aria-expanded="false">
+                  Pengawasan
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="/404">Pengawasan BUJK</a></li>    
+                    <li><a class="dropdown-item" href="/404">Kecelakaan Kerja</a></li>    
+                    <li><a class="dropdown-item" href="/404">Tertib Jasa Konstruksi</a></li>    
+                    {{-- <li><a class="dropdown-item" href="about.html">Workshop</a></li>    
+                    <li><a class="dropdown-item" href="about.html">Daftar Tenaga Ahli</a></li>     --}}
+                </ul>
+              </li>
+            
+              <li class="nav-item dropdown" style="font-size:14px;">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                  aria-expanded="false">
+                  Rantai Pasok
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="/404">Material Bangunan Gedung</a></li>    
+                    <li><a class="dropdown-item" href="/404">Peralatan Konstruksi</a></li>    
+                    <li><a class="dropdown-item" href="/404">Toko Bangunan Kab Blora</a></li>    
+                    <li><a class="dropdown-item" href="/404">Distribusi Kab Blora</a></li>    
+                    <li><a class="dropdown-item" href="/404">Harga Satuan Dasar</a></li>    
+                    <li><a class="dropdown-item" href="/404">HSP Konstruksi Umum</a></li>    
+                    <li><a class="dropdown-item" href="/404">SHST Konstruksi</a></li>    
+                    <li><a class="dropdown-item" href="/404">Daftar Material</a></li>    
+                </ul>
+              </li>
+            
+              {{-- <li class="nav-item dropdown" style="font-size:14px;">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                  aria-expanded="false">
+                  AHSP
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="about.html">Harga Satuan Dasar</a></li>    
+                    <li><a class="dropdown-item" href="about.html">HSP Konstruksi Umum</a></li>    
+                    <li><a class="dropdown-item" href="about.html">SHST Konstruksi</a></li>    
+                    <li><a class="dropdown-item" href="about.html">Daftar Material</a></li>    
+                </ul>
+              </li> --}}
+             
+              <li class="nav-item dropdown" style="font-size:14px;">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                  aria-expanded="false">
+                  Peraturan
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="/404">Undang - Undang</a></li>    
+                    <li><a class="dropdown-item" href="/404">Peraturan Pemerintah</a></li>    
+                    <li><a class="dropdown-item" href="/404">Peraturan Presiden </a></li>    
+                    <li><a class="dropdown-item" href="/404">Peraturan Menteri</a></li>    
+                    <li><a class="dropdown-item" href="/404">Keputusan Menteri</a></li>    
+                    <li><a class="dropdown-item" href="/404">Surat Edaran Menteri</a></li>    
+                    <li><a class="dropdown-item" href="/404">Referensi</a></li>    
+                    <li><a class="dropdown-item" href="/404">Peraturan Daerah</a></li>    
+                    <li><a class="dropdown-item" href="/404">Peraturan Gubernur</a></li>    
+                    <li><a class="dropdown-item" href="/404">Peraturan Walikota/Bupati</a></li>    
+                    <li><a class="dropdown-item" href="/404">Surat Keputusan</a></li>    
+                </ul>
+              </li>
+
+              <li class="nav-item dropdown" style="font-size:14px;">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                  aria-expanded="false">
+                  Masuk
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="/404">Login</a></li>    
+                    <li><a class="dropdown-item" href="/404">Register</a></li>        
+                </ul>
+              </li>
+
+
+          
+        </div>
+      </div>
+    </nav>
+    <!-- ! End Navbar -->
+  </header>
