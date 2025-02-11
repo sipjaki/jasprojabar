a78de0ba6c54988ddfe39b350b1c48c2cbfa06f7
Sigit Septiadi <sigitseptiadi1@gmail.com>
2025-02-11 11:39:57 +0700

    dfg

--- resources/views/frontend/00_full/masjakiblora.blade.php
+++ resources/views/frontend/00_full/masjakiblora.blade.php
@@ -53,7 +53,7 @@
       <nav aria-label="breadcrumb" style="background: #09ff00d7;">
         <div class="container" style="height:5vh; width:auto; margin-top:15px;">
           <!-- Running Text / Marquee Effect in Breadcrumb -->
-          <div class="running-text" style="background: #F7D300; overflow: hidden;">
+          <div class="running-text" style="background: #F7D300; overflow: hidden; rounded-full">
             <p style="font-size: 14px; font-weight: bold; color: black; margin: 0;">
               <span class="marquee-text">
                 Selamat Datang di Mas Jaki Dinas Pekerjaan Umum dan Penataan Ruang Pemerintah Kabupaten Blora Provinsi Jawa Tengah, Sistem Informasi Pembina Jasa Konstruksi Kabupaten Blora
