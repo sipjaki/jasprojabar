240e484f5113e85df016e1a4e854d3240eab72e9
Sigit Septiadi <sigitseptiadi1@gmail.com>
2025-02-11 13:52:13 +0700

    dfg

--- resources/views/frontend/00_full/masjakiblora.blade.php
+++ resources/views/frontend/00_full/masjakiblora.blade.php
@@ -37,7 +37,7 @@
     </div> --}}
     <!-- ! End Landing -->
 
-    <div class="breadcrumbs sticked-header-offset">
+    <div class="breadcrumbs sticked-header-offset" >
       <div class="page-header d-flex align-items-center">
         <div class="container position-relative">
           <div class="row d-flex justify-content-center">
