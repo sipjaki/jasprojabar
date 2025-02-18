d3bf99e210bc1b6ef84b1507b8095c894565b9d6
sipjakiblora <sipjakiblora@gmail.com>
2025-02-18 23:07:03 +0700

    dfg

--- resources/views/frontend/00_approve/01_cssterpisah/footer1.blade.php
+++ resources/views/frontend/00_approve/01_cssterpisah/footer1.blade.php
@@ -5,7 +5,7 @@
 			<div class="row">
 				<div class="col-lg-4">
 					<div class="footer-widget-logo">
-						<a href="index.html"><img src="/assets/icon/logokabupatenblora.png" class="img-fluid" alt="img-25" width="40"></a>
+						<a href="index.html"><img src="/assets/icon/logokabupatenblora.png" class="img-fluid" alt="img-25" width="65"></a>
 					</div><!-- footer-widget-logo -->
 					<div class="footer-widget-text">
 						<p>Dinas Pekerjaan Umum dan Penataan Ruang Pemerintah Kabupaten Blora</p>
