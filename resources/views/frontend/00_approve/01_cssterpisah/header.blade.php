3729d3971274ebd19cec01c129977c2a3d832324
sipjakiblora <sipjakiblora@gmail.com>
2025-02-18 14:11:06 +0700

    dfg

--- resources/views/frontend/00_approve/01_cssterpisah/header.blade.php
+++ resources/views/frontend/00_approve/01_cssterpisah/header.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Home One || TownGov</title>
+	<title>{{$title}}</title>
 <!-- google font -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
