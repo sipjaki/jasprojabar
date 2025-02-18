8f8598da56fd27ea0ecb7e78c825ebc2f3817832
sipjakiblora <sipjakiblora@gmail.com>
2025-02-18 22:20:06 +0700

    dfgfdg

--- resources/views/frontend/00_approve/01_cssterpisah/header.blade.php
+++ resources/views/frontend/00_approve/01_cssterpisah/header.blade.php
@@ -23,5 +23,5 @@
 	<link rel="icon" type="image/png" sizes="32x32" href="/assets/approve/image/favicon/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="/assets/approve/image/favicon/favicon-16x16.png">
 	<link rel="manifest" href="/assets/approve/image/favicons/site.webmanifest"> --}}
-    <link rel="icon" href="/assets/icon/iconkabblora.jpg" type="image/x-icon">
+    <link rel="icon" href="/assets/icon/logokabupatenblora.png" type="image/x-icon">
 </head>
