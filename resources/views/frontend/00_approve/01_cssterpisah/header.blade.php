5b9140e9fecbc4af711aa1b670068ef00b9f4eeb
sipjakiblora <sipjakiblora@gmail.com>
2025-02-18 22:16:22 +0700

    df

--- resources/views/frontend/00_approve/01_cssterpisah/header.blade.php
+++ resources/views/frontend/00_approve/01_cssterpisah/header.blade.php
@@ -21,6 +21,6 @@
 	<!-- favicons Icons -->
 	<link rel="apple-touch-icon" sizes="180x180" href="/assets/approve/image/favicon/apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="/assets/approve/image/favicon/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="/assets/approve/image/favicon/favicon-16x16.png">
+	{{-- <link rel="icon" type="image/png" sizes="16x16" href="/assets/approve/image/favicon/favicon-16x16.png"> --}}
 	<link rel="manifest" href="/assets/approve/image/favicons/site.webmanifest">
 </head>
