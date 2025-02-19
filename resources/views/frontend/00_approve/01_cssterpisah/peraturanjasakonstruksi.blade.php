7dec7bc90f380731b6f6773bcfb7ac302568b4d0
sipjakiblora <sipjakiblora@gmail.com>
2025-02-19 17:15:14 +0700

    dfg

---
+++ resources/views/frontend/00_approve/01_cssterpisah/peraturanjasakonstruksi.blade.php
@@ -0,0 +1,12 @@
+
+<li><a href="/peraturan/undangundang"><i class="fa-solid fa-arrow-left-long"></i>Kembali</a></li>
+<li><a href="services.html">Peraturan Pemerintah<i class="fa-solid fa-arrow-right-long"></i></a></li>
+<li><a href="services.html">Peraturan Presiden<i class="fa-solid fa-arrow-right-long"></i></a></li>
+<li><a href="services.html">Peraturan Menteri<i class="fa-solid fa-arrow-right-long"></i></a></li>
+<li><a href="services.html">Keputusan Menteri<i class="fa-solid fa-arrow-right-long"></i></a></li>
+<li><a href="services.html">Surat Edaran Menteri<i class="fa-solid fa-arrow-right-long"></i></a></li>
+<li><a href="services.html">Referensi<i class="fa-solid fa-arrow-right-long"></i></a></li>
+<li><a href="services.html">Peraturan Daerah<i class="fa-solid fa-arrow-right-long"></i></a></li>
+<li><a href="services.html">Peraturan Gubernur<i class="fa-solid fa-arrow-right-long"></i></a></li>
+<li><a href="services.html">Peraturan Walikota/Bupati<i class="fa-solid fa-arrow-right-long"></i></a></li>
+<li><a href="services.html">Surat Keputusan<i class="fa-solid fa-arrow-right-long"></i></a></li>
--- resources/views/frontend/09_masjaki_peraturan/01_undangundang/show.blade.php
+++ resources/views/frontend/09_masjaki_peraturan/01_undangundang/show.blade.php
@@ -27,17 +27,8 @@
 					<div class="sidebar">
 						<div class="sidebar-widget-list-inner">
 							<ul>
-								<li><a href="/peraturan/undangundang"><i class="fa-solid fa-arrow-left-long"></i>Kembali</a></li>
-								<li><a href="services.html">Peraturan Pemerintah<i class="fa-solid fa-arrow-right-long"></i></a></li>
-								<li><a href="services.html">Peraturan Presiden<i class="fa-solid fa-arrow-right-long"></i></a></li>
-								<li><a href="services.html">Peraturan Menteri<i class="fa-solid fa-arrow-right-long"></i></a></li>
-								<li><a href="services.html">Keputusan Menteri<i class="fa-solid fa-arrow-right-long"></i></a></li>
-								<li><a href="services.html">Surat Edaran Menteri<i class="fa-solid fa-arrow-right-long"></i></a></li>
-								<li><a href="services.html">Referensi<i class="fa-solid fa-arrow-right-long"></i></a></li>
-								<li><a href="services.html">Peraturan Daerah<i class="fa-solid fa-arrow-right-long"></i></a></li>
-								<li><a href="services.html">Peraturan Gubernur<i class="fa-solid fa-arrow-right-long"></i></a></li>
-								<li><a href="services.html">Peraturan Walikota/Bupati<i class="fa-solid fa-arrow-right-long"></i></a></li>
-								<li><a href="services.html">Surat Keputusan<i class="fa-solid fa-arrow-right-long"></i></a></li>
+
+                                @include('frontend.00_approve.01_cssterpisah.peraturanjasakonstruksi')
 
 							</ul><!-- ul -->
 						</div><!-- sidebar-widget-list-inner -->
