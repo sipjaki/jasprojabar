e0fe82526800cc6d33ae281759b19d64b19dec2c
sipjakiblora <sipjakiblora@gmail.com>
2025-02-19 09:24:59 +0700

    dfg

--- resources/views/frontend/00_approve/00_utama/index.blade.php
+++ resources/views/frontend/00_approve/00_utama/index.blade.php
@@ -421,7 +421,7 @@
 				<div class="col-lg-6">
 					<div class="mayor-img">
 						<img src="/assets/approve/image/shapes/shape-1.png" class="floated-image-one" alt="img-7">
-						<img src="/assets/00_dokmasjaki/01_berita/arifhidayat.jpg" alt="img-8" width="30">
+						<img src="/assets/00_dokmasjaki/01_berita/arifhidayat.jpg" alt="img-8" width="1000">
 						<div class="mayor-name">
 							Mohamad Arif Hidayat, ST
 						</div><!-- mayor-name -->
