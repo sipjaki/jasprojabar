8d2e47b192f4575c2629cc62edc6f7f9c03258d8
sipjakiblora <sipjakiblora@gmail.com>
2025-02-19 20:17:23 +0700

    fg

--- resources/views/frontend/09_masjaki_peraturan/01_undangundang/index.blade.php
+++ resources/views/frontend/09_masjaki_peraturan/01_undangundang/index.blade.php
@@ -95,12 +95,24 @@
 
           </div><!-- row -->
         </div><!-- tab-pane -->
-
-        <div class="pagination-container" style="text-align: center; margin-top: 30px;">
-            <div style="display: inline-block; padding: 10px; background-color: #f7f7f7; border-radius: 8px; box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);">
-                {{ $data->links}}
+        <div class="pagination-container" style="margin-top: 25px; margin-bottom: 200px; display: flex; flex-direction: column; align-items: center;">
+            <div class="pagination-info mb-2" style="margin-bottom: 100px; color: black; font-weight: 500;">
+                Data Ke {{ $data->firstItem() }} Sampai {{ $data->lastItem() }} Dari {{ $data->total() }} Jumlah {{$title}}
             </div>
+            <ul class="pagination-paginate" style="display: flex; padding-left: 0; list-style: none; margin-top: 10px;">
+                <li class="page-item {{ $data->onFirstPage() ? 'disabled' : '' }}" style="margin-right: 5px;">
+                    <a class="page-link" href="{{ $data->previousPageUrl() }}">
+                        <i class="fas fa-arrow-left" style="margin-right: 10px;"></i>Previous
+                    </a>
+                </li>
+                <li class="page-item {{ $data->hasMorePages() ? '' : 'disabled' }}" style="margin-right: 5px;">
+                    <a class="page-link" href="{{ $data->nextPageUrl() }}">
+                        Next <i class="fas fa-arrow-right" style="margin-left: 10px;"></i>
+                    </a>
+                </li>
+            </ul>
         </div>
+
 
       </div><!-- tab-content -->
     </div><!-- container -->
