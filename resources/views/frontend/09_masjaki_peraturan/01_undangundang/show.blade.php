f3ae3e44f9dc6594124feb4410f8bf9e0a1eb75e
sipjakiblora <sipjakiblora@gmail.com>
2025-02-19 17:09:35 +0700

    dfgfdg

--- resources/views/frontend/09_masjaki_peraturan/01_undangundang/show.blade.php
+++ resources/views/frontend/09_masjaki_peraturan/01_undangundang/show.blade.php
@@ -16,7 +16,7 @@
 				</ul><!-- list-unstyled -->
 			</div><!-- page-breadcrumbs -->
 			<div class="page-banner-title">
-				<h3>{{ $title }}</h3>
+				<h3 style="color: black;">{{ $title }}</h3>
 			</div><!-- page-banner-title -->
 		</div><!-- container -->
 	</section><!--page-banner-->
@@ -27,13 +27,18 @@
 					<div class="sidebar">
 						<div class="sidebar-widget-list-inner">
 							<ul>
-								<li><a href="services.html"><i class="fa-solid fa-arrow-left-long"></i>Kembali</a></li>
-								<li><a href="services.html">All Document<i class="fa-solid fa-arrow-right-long"></i></a></li>
-								<li><a href="services.html">Policing and Crime<i class="fa-solid fa-arrow-right-long"></i></a></li>
-								<li><a href="services.html">Agriculture and Food<i class="fa-solid fa-arrow-right-long"></i></a></li>
-								<li><a href="services.html">Housing and Land<i class="fa-solid fa-arrow-right-long"></i></a></li>
-								<li><a href="services.html">Art and Culture<i class="fa-solid fa-arrow-right-long"></i></a></li>
-								<li><a href="services.html">Roads & Transport<i class="fa-solid fa-arrow-right-long"></i></a></li>
+								<li><a href="/peraturan/undangundang"><i class="fa-solid fa-arrow-left-long"></i>Kembali</a></li>
+								<li><a href="services.html">Peraturan Pemerintah<i class="fa-solid fa-arrow-right-long"></i></a></li>
+								<li><a href="services.html">Peraturan Presiden<i class="fa-solid fa-arrow-right-long"></i></a></li>
+								<li><a href="services.html">Peraturan Menteri<i class="fa-solid fa-arrow-right-long"></i></a></li>
+								<li><a href="services.html">Keputusan Menteri<i class="fa-solid fa-arrow-right-long"></i></a></li>
+								<li><a href="services.html">Surat Edaran Menteri<i class="fa-solid fa-arrow-right-long"></i></a></li>
+								<li><a href="services.html">Referensi<i class="fa-solid fa-arrow-right-long"></i></a></li>
+								<li><a href="services.html">Peraturan Daerah<i class="fa-solid fa-arrow-right-long"></i></a></li>
+								<li><a href="services.html">Peraturan Gubernur<i class="fa-solid fa-arrow-right-long"></i></a></li>
+								<li><a href="services.html">Peraturan Walikota/Bupati<i class="fa-solid fa-arrow-right-long"></i></a></li>
+								<li><a href="services.html">Surat Keputusan<i class="fa-solid fa-arrow-right-long"></i></a></li>
+
 							</ul><!-- ul -->
 						</div><!-- sidebar-widget-list-inner -->
 						<div class="sidebar-widget sidebar-widget-card">
