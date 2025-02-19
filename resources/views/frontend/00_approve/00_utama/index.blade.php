3cb6fd4314f9218e4729d17da8db44456d5cb855
sipjakiblora <sipjakiblora@gmail.com>
2025-02-19 08:33:23 +0700

    dfg

--- resources/views/frontend/00_approve/00_utama/index.blade.php
+++ resources/views/frontend/00_approve/00_utama/index.blade.php
@@ -293,8 +293,8 @@
 								<i class="flaticon-running-man"></i>
 							</div><!-- funfact-counter-icon -->
 							<div class="funfact-counter-number">
-								<h3 class="counter-number">84</h3>
-								<span class="funfact-counter-number-postfix">k</span>
+								<h3 class="counter-number">231</h3>
+								<span class="funfact-counter-number-postfix"></span>
 							</div><!-- funfact-counter-number -->
 						</div><!-- funfact-counter-box -->
 						<p class="funfact-text">Pelatihan<br></p>
@@ -308,7 +308,7 @@
 							</div><!-- funfact-counter-icon -->
 							<div class="funfact-counter-number">
 								<h3 class="counter-number">234</h3>
-								<span class="funfact-counter-number-postfix">Orang</span>
+								<span class="funfact-counter-number-postfix"></span>
 							</div><!-- funfact-counter-number -->
 						</div><!-- funfact-counter-box -->
 						<p class="funfact-text">Bimbinan Teknis<br></p>
@@ -322,7 +322,7 @@
 							</div><!-- funfact-counter-icon -->
 							<div class="funfact-counter-number">
 								<h3 class="counter-number">213</h3>
-								<span class="funfact-counter-number-postfix">Orang</span>
+								<span class="funfact-counter-number-postfix"></span>
 							</div><!-- funfact-counter-number -->
 						</div><!-- funfact-counter-box -->
 						<p class="funfact-text">Workshop<br></p>
@@ -336,10 +336,10 @@
 							</div><!-- funfact-counter-icon -->
 							<div class="funfact-counter-number">
 								<h3 class="counter-number">32</h3>
-								<span class="funfact-counter-number-postfix">Event</span>
+								<span class="funfact-counter-number-postfix"></span>
 							</div><!-- funfact-counter-number -->
 						</div><!-- funfact-counter-box -->
-						{{-- <p class="funfact-text">Average Costs of Home <br> Ownership</p> --}}
+						<p class="funfact-text">Sertifikasi<br></p>
 					</div><!--funfact-counter-item-->
 				</div><!--col-xl-3 col-md-6-->
 			</div><!-- row -->
