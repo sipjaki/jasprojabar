3bc9e0693647518ec0e4e90082166aa8dbdd3bd3
sipjakiblora <sipjakiblora@gmail.com>
2025-02-19 08:53:16 +0700

    dfg

--- resources/views/frontend/00_approve/01_cssterpisah/footer1.blade.php
+++ resources/views/frontend/00_approve/01_cssterpisah/footer1.blade.php
@@ -88,8 +88,8 @@
 			<span></span>
 		</a><!-- mobile-nav-close -->
 		<div class="logo-box">
-			<a href="/"><img src="/assets/icon/logokabupatenblora.png" width="40" height="40" alt="26" style="margin-right: 5px;"></a>
-			<a href="/"><img src="/assets/icon/pupr.png" width="40" height="40" alt="26"></a>
+			<a href="#"><img src="/assets/icon/logokabupatenblora.png" width="50" height="50" alt="26" style="margin-right: 5px;"></a>
+			<a href="#"><img src="/assets/icon/pupr.png" width="50" height="50" alt="26"></a>
 		</div><!-- logo-box -->
 		<div class="mobile-nav-container"></div><!-- mobile-nav-container -->
 		<ul class="mobile-nav-contact list-unstyled">
