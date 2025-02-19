1a4174e8e7c794d0aa23b0f99400cdb37fa3c981
sipjakiblora <sipjakiblora@gmail.com>
2025-02-19 19:07:46 +0700

    dfgfdg

--- resources/views/frontend/00_approve/01_cssterpisah/peraturanjasakonstruksi.blade.php
+++ resources/views/frontend/00_approve/01_cssterpisah/peraturanjasakonstruksi.blade.php
@@ -1,5 +1,6 @@
 
-<li><a href="/peraturan/undangundang"><i class="fa-solid fa-arrow-left-long"></i>Kembali</a></li>
+{{-- <li><a href="/peraturan/undangundang"><i class="fa-solid fa-arrow-left-long"></i>Kembali</a></li> --}}
+<li><a href="javascript:history.back()"><i class="fa-solid fa-arrow-left-long"></i>Kembali</a></li>
 <li><a href="/404">Peraturan Pemerintah<i class="fa-solid fa-arrow-right-long"></i></a></li>
 <li><a href="/404">Peraturan Presiden<i class="fa-solid fa-arrow-right-long"></i></a></li>
 <li><a href="/404">Peraturan Menteri<i class="fa-solid fa-arrow-right-long"></i></a></li>
