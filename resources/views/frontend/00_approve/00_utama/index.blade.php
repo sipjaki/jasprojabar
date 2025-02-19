57d3336d59bdd2c656aa6f78264ca5f702d6d991
sipjakiblora <sipjakiblora@gmail.com>
2025-02-19 10:45:02 +0700

    fdg

--- resources/views/frontend/00_approve/00_utama/index.blade.php
+++ resources/views/frontend/00_approve/00_utama/index.blade.php
@@ -139,7 +139,7 @@
 	</section><!--department-section-->
 
 
-    <section class="portfolio-section" style="margin-top: -200px;">
+    <section class="portfolio-section" style="margin-top: -150px;">
 		<div class="section-title-box text-center">
 			<div class="section-tagline">Berita Jasa Konstruksi Dinas Pekerjaan Umum dan Penataan Ruang Pemerintah Kabupaten Blora</div>
 			<h2 class="section-title">Sistem Informasi Pembina Jasa Konstruksi</h2>
