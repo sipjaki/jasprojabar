e65794f1c6d5648100a50070b83b91912eb8f60c
sipjakiblora <sipjakiblora@gmail.com>
2025-02-20 20:27:01 +0700

    dg

--- resources/views/frontend/00_approve/01_cssterpisah/header.blade.php
+++ resources/views/frontend/00_approve/01_cssterpisah/header.blade.php
@@ -18,6 +18,7 @@
 	<link rel="stylesheet" type="text/css" href="/assets/approve/vendor/swiper/swiper-bundle.min.css">
 	<link rel="stylesheet" href="/assets/approve/vendor/youtube-popup/youtube-popup.css">
 	<link rel="stylesheet" type="text/css" href="/assets/approve/css/style.css">
+
     <!-- Menambahkan link FontAwesome di bagian head -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
