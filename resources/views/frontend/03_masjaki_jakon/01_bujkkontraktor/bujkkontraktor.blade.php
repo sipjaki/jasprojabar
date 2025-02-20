6ddf67dca54361330326d699df9d8b36adfc27ab
sipjakiblora <sipjakiblora@gmail.com>
2025-02-20 18:07:44 +0700

    dfg

--- resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/bujkkontraktor.blade.php
+++ resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/bujkkontraktor.blade.php
@@ -21,6 +21,92 @@
 				<div class="col-xxl-12 col-lg-12">
 					<div class="donate-box-inner">
 
+                        <h2>Responsive Table</h2>
+                                <div class="table-wrapper">
+                                    <table class="fl-table">
+                                        <thead>
+                                        <tr>
+                                            <th>Header 1</th>
+                                            <th>Header 2</th>
+                                            <th>Header 3</th>
+                                            <th>Header 4</th>
+                                            <th>Header 5</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                        </tr>
+                                        <tr>
+                                            <td>2</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                        </tr>
+                                        <tr>
+                                            <td>3</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                        </tr>
+                                        <tr>
+                                            <td>4</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                        </tr>
+                                        <tr>
+                                            <td>5</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                        </tr>
+                                        <tr>
+                                            <td>6</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                        </tr>
+                                        <tr>
+                                            <td>7</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                        </tr>
+                                        <tr>
+                                            <td>8</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                        </tr>
+                                        <tr>
+                                            <td>9</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                        </tr>
+                                        <tr>
+                                            <td>10</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                            <td>Content</td>
+                                        </tr>
+                                        <tbody>
+                                    </table>
+                                </div>
 
                     </div><!-- donate-box-inner -->
 				</div><!-- col-xl-8 col-lg-12 -->
