9289636ffd08c63135e391de22ef2a9063ae7123
sipjakiblora <sipjakiblora@gmail.com>
2025-02-20 07:10:17 +0700

    dfg

--- resources/views/frontend/01_masjaki_kelembagaan/01_strukturdpuprblora/index.blade.php
+++ resources/views/frontend/01_masjaki_kelembagaan/01_strukturdpuprblora/index.blade.php
@@ -51,13 +51,13 @@
 					<h3>Struktur Dinas Pekerjaan Umum dan Penataan Ruang Pemerintah Kabupaten Blora</h3>
 				</div><!-- portfolio-details-content-title -->
 				<div class="portfolio-details-content-text">
-					<p style="text-justify: center">
+					<p style="text-align: justify" >
                         Dinas Pekerjaan Umum dan Penataan Ruang (PUPR) Pemerintah Kabupaten Blora memiliki peran vital dalam pengelolaan infrastruktur dan penataan ruang yang mendukung pembangunan daerah. Dinas ini bertanggung jawab dalam merencanakan, melaksanakan, serta mengawasi pembangunan berbagai infrastruktur seperti jalan, jembatan, dan gedung, serta fasilitas publik lainnya. Selain itu, Dinas PUPR juga berperan dalam penataan ruang yang berfokus pada pengembangan wilayah yang terencana dan berkelanjutan. Struktur organisasi Dinas PUPR Kabupaten Blora dibangun dengan sistem yang terstruktur dan terorganisir guna memastikan efektivitas dalam pelaksanaan tugas-tugasnya.
                     </p>
 
-                    <p>
+                    <p style="text-align: justify">
                         Di bawah pimpinan Kepala Dinas, setiap unit kerja dalam Dinas PUPR memiliki peran yang jelas, mulai dari Sekretariat yang mendukung administrasi hingga berbagai bidang teknis seperti Bidang Bina Marga yang mengelola pembangunan dan pemeliharaan jalan serta jembatan, Bidang Cipta Karya yang berfokus pada pembangunan sarana prasarana publik, hingga Bidang Penataan Ruang yang bertanggung jawab atas pengelolaan tata ruang wilayah. Selain itu, Bidang Sumber Daya Air menangani pengelolaan sumber daya air dan sistem irigasi, sementara Unit Pelaksana Teknis (UPT) bertugas melaksanakan proyek-proyek terkait dan memastikan kelancaran operasional di lapangan. Dengan struktur yang jelas, Dinas PUPR Kabupaten Blora berkomitmen untuk mendukung pembangunan infrastruktur yang berkualitas dan memberikan manfaat maksimal bagi masyarakat.
-                    </p style="text-justify: center" >
+                    </p >
                 </div><!-- portfolio-details-content-text -->
 			</div><!-- portfolio-details-content -->
 		</div><!-- container -->
