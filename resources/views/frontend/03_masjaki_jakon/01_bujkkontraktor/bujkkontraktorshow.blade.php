bc011ea8c96018b60885fa4111a150de599ee8f9
sipjakiblora <sipjakiblora@gmail.com>
2025-02-20 20:09:22 +0700

    dfg

--- resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/bujkkontraktorshow.blade.php
+++ resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/bujkkontraktorshow.blade.php
@@ -35,7 +35,7 @@
 						{{-- <h4 class="department-details-title" style="font-size: 20px;">{{ $data->judul}}</h4> --}}
 					</div><!-- department-details-content-box -->
                     <div class="document-details-inner">
-						<h3>Download Documents</h3>
+						<h3>{{$data->namalengkap}}</h3>
 						<div class="document-details">
 							<div class="document-details-list">
 								<div class="document-details-icon">
