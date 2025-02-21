3cacc714a13dfb1a09a782e3223e3b3c41a5e63f
sipjakiblora <sipjakiblora@gmail.com>
2025-02-21 15:31:49 +0700

    dfg

--- app/Http/Controllers/BujkkonsultanController.php
+++ app/Http/Controllers/BujkkonsultanController.php
@@ -2,9 +2,60 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\bujkkonsultan;
+use App\Models\bujkkonsultansub;
+use App\Models\bujkkontraktor;
+use App\Models\bujkkontraktorsub;
 use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
+
+
 
 class BujkkonsultanController extends Controller
 {
     //
+    public function bujkkonsultan()
+    {
+        $data = bujkkonsultan::paginate(15);
+        $datasub = bujkkonsultansub::paginate(20);
+        $user = Auth::user();
+
+        return view('frontend.03_masjaki_jakon.02_bujkkonsultan.bujkkonsultan', [
+            'title' => 'BUJK Konsultan',
+            'user' => $user, // Mengirimkan data paginasi ke view
+            'data' => $data, // Mengirimkan data paginasi ke view
+            'datasub' => $datasub, // Mengirimkan data paginasi ke view
+        ]);
+    }
+
+    public function bujkkonsultanshow($namalengkap)
+    {
+        $databujkkonsultan = bujkkonsultan::where('namalengkap', $namalengkap)->first();
+
+        if (!$databujkkonsultan) {
+            // Tangani jika kegiatan tidak ditemukan
+            return redirect()->back()->with('error', 'Kegiatan tidak ditemukan.');
+        }
+
+        // Menggunakan paginate() untuk pagination
+        $subdata = bujkkonsultansub::where('bujkkonsultan_id', $databujkkonsultan->id)->paginate(10);
+
+          // Menghitung nomor urut mulai
+            $start = ($subdata->currentPage() - 1) * $subdata->perPage() + 1;
+
+
+    // Ambil data user saat ini
+    $user = Auth::user();
+
+    return view('frontend.03_masjaki_jakon.02_bujkkonsultan.bujkkonsultanshow', [
+        'title' => 'Data Bujk Kontraktor',
+        'data' => $databujkkonsultan,
+        'subData' => $subdata,  // Jika Anda ingin mengirimkan data sub kontraktor juga
+        'user' => $user,
+        'start' => $start,
+    ]);
+    }
+
+
 }
