33f9a958c8855111390db4a3087b81c231303db5
sipjakiblora <sipjakiblora@gmail.com>
2025-02-21 13:45:05 +0700

    dfg

--- resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/bujkkontraktorshow.blade.php
+++ resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/bujkkontraktorshow.blade.php
@@ -27,7 +27,7 @@
 
 			<div class="row row-gutter-30">
 				<div class="col-lg-12">
-                    <div class="col-lg-12" style="display: flex; justify-content: center;">
+                    <div class="col-lg-12" style="display: flex; justify-content: left; padding:20px">
                         <button class="btn"
                                 style="background-color: #000080;
                                        color: white;
@@ -38,7 +38,7 @@
                                        cursor: pointer;"
                                 onmouseover="this.style.backgroundColor='white'; this.style.color='black'; this.querySelector('i').style.color='black';"
                                 onmouseout="this.style.backgroundColor='#000080'; this.style.color='white'; this.querySelector('i').style.color='white';">
-                            <i class="fas fa-arrow-left" style="margin-right: px; color: white;"></i>Kembali
+                            <i class="fas fa-arrow-left" style="margin-right: 10px; color: white;"></i>Kembali
                         </button>
                     </div><!-- col-lg-12 -->
 				</div><!-- col-lg-12 -->
