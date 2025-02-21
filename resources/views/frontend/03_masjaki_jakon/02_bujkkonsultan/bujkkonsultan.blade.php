9b26f3d1f55e9078c10648ca6567000872426ce8
sipjakiblora <sipjakiblora@gmail.com>
2025-02-21 16:18:26 +0700

    dfg

--- resources/views/frontend/03_masjaki_jakon/02_bujkkonsultan/bujkkonsultan.blade.php
+++ resources/views/frontend/03_masjaki_jakon/02_bujkkonsultan/bujkkonsultan.blade.php
@@ -112,6 +112,19 @@
 </style>
 </head>
 
+<section class="document-section" style="margin-top: -100px;">
+    <div class="container" >
+      <ul class="nav nav-pills" id="pills-tab" role="tablist">
+        <a href="/datajakon/bujk">
+        <li class="nav-item" role="presentation">
+            <button class="nav-link" id="pills-health-tab" data-bs-toggle="pill" data-bs-target="#pills-health" type="button" role="tab" aria-controls="pills-health" aria-selected="false"><i class="flaticon-police-badge-1"></i>All Data Badan Usaha Jasa Konstruksi Kontraktor & Konsultan</button>
+        </li>
+        </a>
+        </ul>
+    </div>
+</section>
+
+
 <body>
 <div class="table-wrapper">
 <table class="fl-table">
