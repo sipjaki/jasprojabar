42d472b7f386b3f30afa26884e7db3434ac8762f
sipjakiblora <sipjakiblora@gmail.com>
2025-02-21 13:39:44 +0700

    dfg

--- resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/bujkkontraktorshow.blade.php
+++ resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/bujkkontraktorshow.blade.php
@@ -24,7 +24,7 @@
     <section class="donate-section">
 		<div class="container">
             <div class="col-lg-12">
-                <button class="btn btn-primary" style="display: flex; align-items: center;">
+                <button class="btn" style="background-color: #000080; color: white; display: flex; align-items: center;">
                     <i class="fas fa-arrow-left" style="margin-right: 8px;"></i>Kembali
                 </button>
             </div><!-- col-lg-12 -->
