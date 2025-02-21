cdcfb532794f1b6dfb3699fe52af73cd5a4634c9
sipjakiblora <sipjakiblora@gmail.com>
2025-02-21 11:29:03 +0700

    dfg

--- resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/bujkkontraktorshow.blade.php
+++ resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/bujkkontraktorshow.blade.php
@@ -21,6 +21,59 @@
 		</div><!-- container -->
 	</section><!--page-banner-->
 
+    <section class="donate-section">
+		<div class="container">
+			<div class="row row-gutter-30">
+				<div class="col-xxl-12 col-lg-12">
+
+					<div class="donate-box-inner">
+                        <div class="col-lg-12">
+                            <button class="btn btn-primary">{{$data->namalengkap}}</button>
+                        </div><!-- col-lg-12 -->
+
+						<div class="donate-amount">
+							<span>$</span>
+							<input class="give-amount" type="text" autocomplete="off" id="sd">
+						</div><!-- donate-amount -->
+
+						<form action="assets/inc/sendemail.php" class="contact-form donation" method="post">
+							<div class="row row-gutter-10">
+								<h3>Personal Info</h3>
+								<div class="col-lg-6">
+									<input type="text" id="fname"  class="input-text" placeholder="First Name*" name="fname" aria-required="true">
+								</div><!-- col-lg-6 -->
+								<div class="col-lg-6">
+									<input type="text" id="lname" class="input-text" placeholder="Last Name" name="lname" aria-required="true">
+								</div><!-- col-lg-6 -->
+								<div class="col-lg-12">
+									<input type="email" id="email" class="input-text" placeholder="Email Address*" name="email" aria-required="true">
+								</div><!-- col-lg-12 -->
+								<div class="col-lg-12">
+									<div class="donate-card">
+										<p>To make an offline donation toward this cause, follow these steps:: </p>
+										<ol>
+											<li>Write a check payable to "City Government & Municipal WordPress Theme"</li>
+											<li>On the memo line of the check, indicate that the donation is for "City Government & Municipal WordPress Theme"</li>
+											<li>Mail your check to:</li>
+										</ol><!-- ol -->
+										<p>City Government & Municipal WordPress Theme</p>
+										<p>111 Not A Real St.</p>
+										<p>Anytown, CA 12345 </p>
+										<span>Your tax-deductible donation is greatly appreciated!</span>
+									</div><!-- donate-card -->
+								</div><!-- col-lg-12 -->
+
+
+							</div><!-- row row-gutter-10 -->
+						</form><!-- contact-form -->
+					</div><!-- donate-box-inner -->
+				</div><!-- col-xl-8 col-lg-12 -->
+
+			</div><!-- row -->
+		</div><!-- container -->
+	</section><!-- donate-section -->
+
+
 	<section class="department-details-section">
 		<div class="container">
 			<div class="row">
