8bb9dee949e3d9cb9e58a72f2e266793f402d056
sipjakiblora <sipjakiblora@gmail.com>
2025-02-22 23:00:00 +0700

    g

--- resources/views/frontend/00_approve/01_cssterpisah/header.blade.php
+++ resources/views/frontend/00_approve/01_cssterpisah/header.blade.php
@@ -25,8 +25,6 @@
     {{-- <link rel="stylesheet" type="text/css" href="/assets/00_dokmasjaki/00_style/csstable.css"> --}}
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-        <!-- Import Flaticon CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flaticon/1.0.0/flaticon.css">
 
 
 	<!-- favicons Icons -->
