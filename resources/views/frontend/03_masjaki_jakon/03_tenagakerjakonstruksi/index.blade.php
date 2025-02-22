88b1793b0f8e1e1ed5729297156b7c00e1cc6008
sipjakiblora <sipjakiblora@gmail.com>
2025-02-22 23:48:56 +0700

    g

--- resources/views/frontend/03_masjaki_jakon/03_tenagakerjakonstruksi/index.blade.php
+++ resources/views/frontend/03_masjaki_jakon/03_tenagakerjakonstruksi/index.blade.php
@@ -29,7 +29,7 @@
                 <div class="service-two-card">
                     <div class="service-two-imgbox">
                         <div class="service-two-image" style="display: flex; justify-content: center;">
-                            <img src="/assets/03_datajakon/skktenagaahli.png" class="img-fluid" alt="img-28" loading="lazy" style="width: 70%  ">
+                            <img src="/assets/00_dokmasjaki/03_datajakon/jabatan.png" class="img-fluid" alt="img-28" loading="lazy" style="width: 70%; padding:30px;">
                             <a href="/datajakon/skktenagakerjablora"></a>
                         </div><!-- service-two-image -->
                     </div><!-- service-two-imgbox -->
@@ -47,14 +47,52 @@
                 <div class="service-two-card">
                     <div class="service-two-imgbox" >
                         <div class="service-two-image" style="display: flex; justify-content: center;">
-                            <img src="/assets/03_datajakon/datastatistikaskk.png"  class="img-fluid" alt="img-29"  loading="lazy" style="width:70% ">
+                            <img src="/assets/00_dokmasjaki/03_datajakon/statistika.png"  class="img-fluid" alt="img-29"  loading="lazy" style="width:70%; padding:30px; ">
                             <a href="/datajakon/statistikaskktenagakerjablora"></a>
                         </div><!-- service-two-image -->
                     </div><!-- service-two-imgbox -->
                     <div class="service-two-card-content">
-                        <h4>Data Statistika SKK Tenaga Ahli</h4>
+                        <h4>Data Statistik SKK Tenaga Ahli Berdasarkan Jabatan</h4>
                         <p>Dinas Pekerjaan Umum dan Penataan Ruang <br> Pemerintah Kabupaten Blora Provinsi Jawa Tengah</p>
                         <a href="/datajakon/statistikaskktenagakerjablora">
+                            <i class="fa-solid fa-arrow-right-long"></i>
+                            <span>Lihat Data</span>
+                        </a>
+                    </div><!--service-two-card-content-->
+                </div><!--service-two-card-->
+            </div><!--col-12 col-lg-4 col-xl-4-->
+
+            <div class="col-12 col-lg-3 col-xl-3">
+                <div class="service-two-card">
+                    <div class="service-two-imgbox" >
+                        <div class="service-two-image" style="display: flex; justify-content: center;">
+                            <img src="/assets/00_dokmasjaki/03_datajakon/daftar.png"  class="img-fluid" alt="img-29"  loading="lazy" style="width:70%; padding:30px; ">
+                            <a href="/datajakon/?"></a>
+                        </div><!-- service-two-image -->
+                    </div><!-- service-two-imgbox -->
+                    <div class="service-two-card-content">
+                        <h4>Daftar Tenaga Ahli Konstruksi</h4>
+                        <p>Dinas Pekerjaan Umum dan Penataan Ruang <br> Pemerintah Kabupaten Blora Provinsi Jawa Tengah</p>
+                        <a href="/datajakon/?">
+                            <i class="fa-solid fa-arrow-right-long"></i>
+                            <span>Lihat Data</span>
+                        </a>
+                    </div><!--service-two-card-content-->
+                </div><!--service-two-card-->
+            </div><!--col-12 col-lg-4 col-xl-4-->
+
+            <div class="col-12 col-lg-3 col-xl-3">
+                <div class="service-two-card">
+                    <div class="service-two-imgbox" >
+                        <div class="service-two-image" style="display: flex; justify-content: center;">
+                            <img src="/assets/00_dokmasjaki/03_datajakon/statistika1.png"  class="img-fluid" alt="img-29"  loading="lazy" style="width:70%; padding:30px; ">
+                            <a href="/datajakon/?"></a>
+                        </div><!-- service-two-image -->
+                    </div><!-- service-two-imgbox -->
+                    <div class="service-two-card-content">
+                        <h4>Data Statistik Tenaga Ahli Konstruksi</h4>
+                        <p>Dinas Pekerjaan Umum dan Penataan Ruang <br> Pemerintah Kabupaten Blora Provinsi Jawa Tengah</p>
+                        <a href="/datajakon/?">
                             <i class="fa-solid fa-arrow-right-long"></i>
                             <span>Lihat Data</span>
                         </a>
