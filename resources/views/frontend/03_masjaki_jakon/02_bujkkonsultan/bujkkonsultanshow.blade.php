de8e96e5a45d32cbc79445c8ded7a93d06296b3d
sipjakiblora <sipjakiblora@gmail.com>
2025-02-23 01:11:19 +0700

    g

--- resources/views/frontend/03_masjaki_jakon/02_bujkkonsultan/bujkkonsultanshow.blade.php
+++ resources/views/frontend/03_masjaki_jakon/02_bujkkonsultan/bujkkonsultanshow.blade.php
@@ -30,7 +30,7 @@
 
 
                                 <section class="donate-section">
-                                    <div class="container">
+                                    <div class="container" style="margin-top: -100px;">
 
                                         <div class="row row-gutter-30">
                                             <div class="col-lg-12">
