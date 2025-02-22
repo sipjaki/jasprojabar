e05f324728d5f3853a0205eca7c4d22b1d1b140f
sipjakiblora <sipjakiblora@gmail.com>
2025-02-22 23:44:10 +0700

    g

--- resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/index.blade.php
+++ resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/index.blade.php
@@ -70,7 +70,7 @@
                         </div><!-- service-two-image -->
                     </div><!-- service-two-imgbox -->
                     <div class="service-two-card-content">
-                        <h4>Data Statistika</h4>
+                        <h4>Data Statistik</h4>
                         <p>Dinas Pekerjaan Umum dan Penataan Ruang <br> Pemerintah Kabupaten Blora Provinsi Jawa Tengah</p>
                         <a href="/datajakon/statistikabujkblora">
                             <i class="fa-solid fa-arrow-right-long"></i>
