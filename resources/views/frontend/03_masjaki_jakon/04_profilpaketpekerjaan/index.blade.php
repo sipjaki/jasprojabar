e77d74e760fd46d7cbcde6710d19d69257e7514f
sipjakiblora <sipjakiblora@gmail.com>
2025-02-23 18:29:55 +0700

    g

--- resources/views/frontend/03_masjaki_jakon/04_profilpaketpekerjaan/index.blade.php
+++ resources/views/frontend/03_masjaki_jakon/04_profilpaketpekerjaan/index.blade.php
@@ -43,6 +43,7 @@
                     </div><!--service-two-card-content-->
                 </div><!--service-two-card-->
             </div><!--col-12 col-lg-4 col-xl-4-->
+
             <div class="col-12 col-lg-4 col-xl-4">
                 <div class="service-two-card">
                     <div class="service-two-imgbox" >
@@ -53,6 +54,44 @@
                     </div><!-- service-two-imgbox -->
                     <div class="service-two-card-content">
                         <h4>Pekerjaan Non Tender</h4>
+                        <p>Dinas Pekerjaan Umum dan Penataan Ruang <br> Pemerintah Kabupaten Blora Provinsi Jawa Tengah</p>
+                        <a href="/datajakon/statistikaskktenagakerjablora">
+                            <i class="fa-solid fa-arrow-right-long"></i>
+                            <span>Lihat Data</span>
+                        </a>
+                    </div><!--service-two-card-content-->
+                </div><!--service-two-card-->
+            </div><!--col-12 col-lg-4 col-xl-4-->
+
+            <div class="col-12 col-lg-4 col-xl-4">
+                <div class="service-two-card">
+                    <div class="service-two-imgbox" >
+                        <div class="service-two-image" style="display: flex; justify-content: center;">
+                            <img src="/assets/00_dokmasjaki/03_datajakon/konsultantender.png"  class="img-fluid" alt="img-29"  loading="lazy" style="width:70%; padding:30px; ">
+                            <a href="/404"></a>
+                        </div><!-- service-two-image -->
+                    </div><!-- service-two-imgbox -->
+                    <div class="service-two-card-content">
+                        <h4>Konsultan Tender</h4>
+                        <p>Dinas Pekerjaan Umum dan Penataan Ruang <br> Pemerintah Kabupaten Blora Provinsi Jawa Tengah</p>
+                        <a href="/datajakon/statistikaskktenagakerjablora">
+                            <i class="fa-solid fa-arrow-right-long"></i>
+                            <span>Lihat Data</span>
+                        </a>
+                    </div><!--service-two-card-content-->
+                </div><!--service-two-card-->
+            </div><!--col-12 col-lg-4 col-xl-4-->
+
+            <div class="col-12 col-lg-4 col-xl-4">
+                <div class="service-two-card">
+                    <div class="service-two-imgbox" >
+                        <div class="service-two-image" style="display: flex; justify-content: center;">
+                            <img src="/assets/00_dokmasjaki/03_datajakon/konsultannontender.png"  class="img-fluid" alt="img-29"  loading="lazy" style="width:70%; padding:30px; ">
+                            <a href="/404"></a>
+                        </div><!-- service-two-image -->
+                    </div><!-- service-two-imgbox -->
+                    <div class="service-two-card-content">
+                        <h4>Konsultan Non Tender</h4>
                         <p>Dinas Pekerjaan Umum dan Penataan Ruang <br> Pemerintah Kabupaten Blora Provinsi Jawa Tengah</p>
                         <a href="/datajakon/statistikaskktenagakerjablora">
                             <i class="fa-solid fa-arrow-right-long"></i>
