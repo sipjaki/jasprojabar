bca2a0f08b520bea99374c178c9fb6fc05381ea9
sipjakiblora <sipjakiblora@gmail.com>
2025-02-23 18:43:26 +0700

    g

--- resources/views/frontend/03_masjaki_jakon/04_profilpaketpekerjaan/index.blade.php
+++ resources/views/frontend/03_masjaki_jakon/04_profilpaketpekerjaan/index.blade.php
@@ -30,7 +30,7 @@
                     <div class="service-two-imgbox">
                         <div class="service-two-image" style="display: flex; justify-content: center;">
                             <img src="/assets/00_dokmasjaki/03_datajakon/pekerjaantender.png" class="img-fluid" alt="img-28" loading="lazy" style="width: 70%; padding:30px;">
-                            <a href="/paketpekerjaantender"></a>
+                            <a href="/datajakon/paketpekerjaantendergit "></a>
                         </div><!-- service-two-image -->
                     </div><!-- service-two-imgbox -->
                     <div class="service-two-card-content">
