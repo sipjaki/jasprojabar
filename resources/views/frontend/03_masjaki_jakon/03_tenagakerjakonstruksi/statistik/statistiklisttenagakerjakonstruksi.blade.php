cbee2f45b20eb2dbc66699aa4e9c1d46182425de
sipjakiblora <sipjakiblora@gmail.com>
2025-02-23 15:42:42 +0700

    g

--- resources/views/frontend/03_masjaki_jakon/03_tenagakerjakonstruksi/statistik/statistiklisttenagakerjakonstruksi.blade.php
+++ resources/views/frontend/03_masjaki_jakon/03_tenagakerjakonstruksi/statistik/statistiklisttenagakerjakonstruksi.blade.php
@@ -90,15 +90,15 @@
                         <div class="sidebar-widget-list-inner">
 
                                 <ul style="list-style: none; padding: 0; text-align: center;">
-                                    <li><a href="#"><i class="fa-solid fa-chart-pie"></i> Data Statistik Jenjang 1</a></li>
-                                    <li><a href="#"><i class="fa-solid fa-chart-pie"></i> Data Statistik Jenjang 2</a></li>
-                                    <li><a href="#"><i class="fa-solid fa-chart-pie"></i> Data Statistik Jenjang 3</a></li>
-                                    <li><a href="#"><i class="fa-solid fa-chart-pie"></i> Data Statistik Jenjang 4</a></li>
-                                    <li><a href="#"><i class="fa-solid fa-chart-pie"></i> Data Statistik Jenjang 5</a></li>
-                                    <li><a href="#"><i class="fa-solid fa-chart-pie"></i> Data Statistik Jenjang 6</a></li>
-                                    <li><a href="#"><i class="fa-solid fa-chart-pie"></i> Data Statistik Jenjang 7</a></li>
-                                    <li><a href="#"><i class="fa-solid fa-chart-pie"></i> Data Statistik Jenjang 8</a></li>
-                                    <li><a href="#"><i class="fa-solid fa-chart-pie"></i> Data Statistik Jenjang 9</a></li>
+                                    <li><a href="/datajakon/datajenjang1"><i class="fa-solid fa-chart-pie"></i> Data Statistik Jenjang 1</a></li>
+                                    <li><a href="/datajakon/datajenjang2"><i class="fa-solid fa-chart-pie"></i> Data Statistik Jenjang 2</a></li>
+                                    <li><a href="/datajakon/datajenjang3"><i class="fa-solid fa-chart-pie"></i> Data Statistik Jenjang 3</a></li>
+                                    <li><a href="/datajakon/datajenjang4"><i class="fa-solid fa-chart-pie"></i> Data Statistik Jenjang 4</a></li>
+                                    <li><a href="/datajakon/datajenjang5"><i class="fa-solid fa-chart-pie"></i> Data Statistik Jenjang 5</a></li>
+                                    <li><a href="/datajakon/datajenjang6"><i class="fa-solid fa-chart-pie"></i> Data Statistik Jenjang 6</a></li>
+                                    <li><a href="/datajakon/datajenjang7"><i class="fa-solid fa-chart-pie"></i> Data Statistik Jenjang 7</a></li>
+                                    <li><a href="/datajakon/datajenjang8"><i class="fa-solid fa-chart-pie"></i> Data Statistik Jenjang 8</a></li>
+                                    <li><a href="/datajakon/datajenjang9"><i class="fa-solid fa-chart-pie"></i> Data Statistik Jenjang 9</a></li>
                                     </ul>
 
                         </div><!-- sidebar-widget-list-inner -->
