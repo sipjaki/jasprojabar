2250c378a13634030a858a15dff42778bc72f22e
sipjakiblora <sipjakiblora@gmail.com>
2025-02-23 13:52:23 +0700

    g

--- resources/views/frontend/03_masjaki_jakon/03_tenagakerjakonstruksi/statistik/statistiklisttenagakerjakonstruksi.blade.php
+++ resources/views/frontend/03_masjaki_jakon/03_tenagakerjakonstruksi/statistik/statistiklisttenagakerjakonstruksi.blade.php
@@ -1,92 +1,160 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>{{ $title }}</title>
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-      google.charts.load("current", {packages:["corechart"]});
-      google.charts.setOnLoadCallback(drawCharts);
+@include('frontend.00_approve.01_cssterpisah.header')
 
-      function drawCharts() {
-          drawJenjangChart();
-          drawJabatanKerjaChart();
-      }
 
-      function drawJenjangChart() {
-          var data = google.visualization.arrayToDataTable([
-              ['Jenjang', 'Jumlah'],
-              @foreach($statistikJenjang as $item)
-                  ['{{ $item['jenjang'] }}', {{ $item['jumlah'] }}],
-              @endforeach
-          ]);
-
-          var options = {
-              title: 'Distribusi Jenjang',
-              is3D: true,
-          };
-
-          var chart = new google.visualization.PieChart(document.getElementById('chart_jenjang'));
-          chart.draw(data, options);
-      }
-
-      function drawJabatanKerjaChart() {
-          var data = google.visualization.arrayToDataTable([
-              ['Jabatan Kerja', 'Jumlah'],
-              @foreach($statistikJabatanKerja as $item)
-                  ['{{ $item['jabatankerja'] }}', {{ $item['jumlah'] }}],
-              @endforeach
-          ]);
-
-          var options = {
-              title: 'Distribusi Jabatan Kerja',
-              is3D: true,
-          };
-
-          var chart = new google.visualization.PieChart(document.getElementById('chart_jabatan_kerja'));
-          chart.draw(data, options);
-      }
-    </script>
-</head>
 <body>
 
-    <h2>Statistik SKK Tenaga Ahli Konstruksi</h2>
+    @include('frontend.00_approve.01_cssterpisah.loader')
+    @include('frontend.00_approve.01_cssterpisah.header1')
 
-    <div id="chart_jenjang" style="width: 600px; height: 400px;"></div>
-    <div id="chart_jabatan_kerja" style="width: 600px; height: 400px;"></div>
+    <div class="page-wrapper">
+	<section class="page-banner">
+		<div class="container">
+			<div class="page-banner-title">
+				<h3 style="font-size: 25px; color:black;">{{$title}}</h3>
+			</div><!-- page-banner-title -->
+		</div><!-- container -->
+	</section><!--page-banner-->
+	<section class="department-details-section">
+		<div class="container">
+			<div class="row">
+                <div class="col-12 col-lg-12 col-xl-12">
+                    <div class="department-details-imgbox">
+                        <h4 class="department-details-title" style="font-size: 20px;">Wilayah Administrasi Pemerintah Kabupaten Blora Provinsi Jawa Tengah</h4>
+						<img src="/assets/00_dokmasjaki/03_datajakon/petaindonesiabaru.png" alt="img-159">
+						<a href="#"></a>
+                    </div><!-- department-details-imgbox -->
+                </div>
+				<div class="col-12 col-lg-4 col-xl-4">
+					<div class="sidebar">
+						<div class="sidebar-widget-list-inner">
+							<ul>
+								<li><a href="/datajakon/skktenagakerja"><i class="fa-solid fa-arrow-left-long"></i>Kembali</a></li>
+								<li style="text-align: center"><a style="text-align: center;" href="#"> Tenaga Ahli Berdasarkan Jabatan Kerja<i class="fa-solid fa-database" style="margin-left: 10px;"></i></a></li>
+								<li style="text-align: center"><a style="text-align: center;" href="#">Total Tenaga Ahli : <i class="fa-solid fa-edit" style="margin-left: 10px;"></i></li>
+							</ul><!-- ul -->
+						</div><!-- sidebar-widget-list-inner -->
+						<div class="sidebar-widget sidebar-widget-card">
+							<div class="sidebar-widget-card-icon">
+								<i class="flaticon-question"></i>
+							</div><!-- sidebar-widget-card-icon -->
+							<div class="sidebar-widget-card-content">
+                                <h3><a href="https://wa.me/6281321455855" target="_blank">Butuh Bantuan? Klik </a></h3>
+								<p>
+                                    Kami siap membantu! Klik di atas  untuk terhubung langsung dengan tim kami.</p>
+							</div><!-- sidebar-widget-card-content -->
+						</div><!-- sidebar-widget sidebar-widget-card -->
+					</div><!--sidebar-->
+				</div><!--col-12 col-lg-4 col-xl-4-->
+				<div class="col-lg-8">
 
-</body>
-</html>
+					<div class="department-details-content-box">
+						<h4 style="font-size: 20px;" class="department-details-title">{{$title}}</h4>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>{{ $title }}</title>
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-      google.charts.load('current', {'packages':['table']});
-      google.charts.setOnLoadCallback(drawTable);
 
-      function drawTable() {
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Jenjang');
-        data.addColumn('number', 'Jumlah');
+                        {{-- ISIAN DATA --}}
 
-        data.addRows([
-            @foreach($statistikJenjang as $item)
-                ['{{ $item['jenjang'] }}', {{ $item['jumlah'] }}],
-            @endforeach
-        ]);
+                        <div id="chart_jenjang" style="width: 600px; height: 400px;"></div>
+                        <div id="chart_jabatan_kerja" style="width: 600px; height: 400px;"></div>
 
-        var table = new google.visualization.Table(document.getElementById('table_div'));
-        table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
-      }
-    </script>
-</head>
-<body>
+                        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+                        <script type="text/javascript">
+                          google.charts.load("current", {packages:["corechart"]});
+                          google.charts.setOnLoadCallback(drawCharts);
 
-    <h2>Statistik Jumlah Jenjang</h2>
+                          function drawCharts() {
+                              drawJenjangChart();
+                              drawJabatanKerjaChart();
+                          }
 
-    <div id="table_div"></div>
+                          function drawJenjangChart() {
+                              var data = google.visualization.arrayToDataTable([
+                                  ['Jenjang', 'Jumlah'],
+                                  @foreach($statistikJenjang as $item)
+                                      ['{{ $item['jenjang'] }}', {{ $item['jumlah'] }}],
+                                  @endforeach
+                              ]);
 
-</body>
-</html>
+                              var options = {
+                                  title: 'Distribusi Jenjang',
+                                  is3D: true,
+                              };
+
+                              var chart = new google.visualization.PieChart(document.getElementById('chart_jenjang'));
+                              chart.draw(data, options);
+                          }
+
+                          function drawJabatanKerjaChart() {
+                              var data = google.visualization.arrayToDataTable([
+                                  ['Jabatan Kerja', 'Jumlah'],
+                                  @foreach($statistikJabatanKerja as $item)
+                                      ['{{ $item['jabatankerja'] }}', {{ $item['jumlah'] }}],
+                                  @endforeach
+                              ]);
+
+                              var options = {
+                                  title: 'Distribusi Jabatan Kerja',
+                                  is3D: true,
+                              };
+
+                              var chart = new google.visualization.PieChart(document.getElementById('chart_jabatan_kerja'));
+                              chart.draw(data, options);
+                          }
+                        </script>
+
+
+                        </div><!-- department-details-content-box -->
+
+                </div><!-- col-lg-8 -->
+
+                <div class="col-lg-12" style="margin-top:25px;">
+					<div class="department-details-box">
+
+                        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+                        <script type="text/javascript">
+                          google.charts.load('current', {'packages':['table']});
+                          google.charts.setOnLoadCallback(drawTable);
+
+                          function drawTable() {
+                            var data = new google.visualization.DataTable();
+                            data.addColumn('string', 'Jenjang');
+                            data.addColumn('number', 'Jumlah');
+
+                            data.addRows([
+                                @foreach($statistikJenjang as $item)
+                                    ['{{ $item['jenjang'] }}', {{ $item['jumlah'] }}],
+                                @endforeach
+                            ]);
+
+                            var table = new google.visualization.Table(document.getElementById('table_div'));
+                            table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
+                          }
+                        </script>
+
+                    <div id="table_div"></div>
+
+
+                    </div><!-- department-details-box -->
+
+                </div>
+
+
+
+                <div class="col-lg-12" style="margin-top:25px;">
+					<div class="department-details-box">
+						<p style="text-align: justify;">
+
+                            Data ini menyajikan data mengenai Sertifikat Kompetensi Kerja (SKK) Tenaga Ahli Konstruksi berdasarkan jabatan kerja di lingkungan Pemerintah Kabupaten Blora, Provinsi Jawa Tengah. Data ini memberikan gambaran tentang kualifikasi, distribusi, dan kapasitas tenaga ahli yang berperan dalam mendukung pembangunan infrastruktur daerah. Dengan adanya informasi ini, diharapkan dapat menjadi referensi bagi pemerintah, pelaku industri konstruksi, serta masyarakat dalam perencanaan, pengelolaan, dan peningkatan kompetensi tenaga ahli konstruksi di Kabupaten Blora.
+                        </p>
+
+                        </div><!-- department-details-box -->
+
+                </div>
+			</div><!-- row -->
+		</div><!-- container -->
+	</section><!-- department-details-section -->
+</div><!--page-wrapper-->
+
+</div><!--page-wrapper-->
+@include('frontend.00_approve.01_cssterpisah.footer1')
+@include('frontend.00_approve.01_cssterpisah.footer')
+
