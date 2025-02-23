47d161c3493451bcbf27516aedd2075a12044358
sipjakiblora <sipjakiblora@gmail.com>
2025-02-23 19:07:22 +0700

    g

--- resources/views/frontend/03_masjaki_jakon/04_profilpaketpekerjaan/01_paketpekerjaantender.blade.php
+++ resources/views/frontend/03_masjaki_jakon/04_profilpaketpekerjaan/01_paketpekerjaantender.blade.php
@@ -126,7 +126,7 @@
                               <ul class="nav nav-pills" id="pills-tab" role="tablist">
                                 <a href="/datajakon/datapaketpekerjaanblora">
                                 <li class="nav-item" role="presentation">
-                                    <button class="nav-link" id="pills-health-tab" data-bs-toggle="pill" data-bs-target="#pills-health" type="button" role="tab" aria-controls="pills-health" aria-selected="false"><i class="flaticon-police-badge-1"></i>Menu Data Badan Usaha Konstruksi dan Konsultan</button>
+                                    <button class="nav-link" id="pills-health-tab" data-bs-toggle="pill" data-bs-target="#pills-health" type="button" role="tab" aria-controls="pills-health" aria-selected="false"><i class="flaticon-police-badge-1"></i>Menu Data Profil Paket Pekerjaan dan Konsultan</button>
                                 </li>
                                 </a>
                                 </ul>
