6e755bdf8e69c242a8d55e675ba1520d0909d59f
sipjakiblora <sipjakiblora@gmail.com>
2025-02-23 16:31:53 +0700

    g

--- resources/views/frontend/03_masjaki_jakon/03_tenagakerjakonstruksi/statistik/01_jenjang1.blade.php
+++ resources/views/frontend/03_masjaki_jakon/03_tenagakerjakonstruksi/statistik/01_jenjang1.blade.php
@@ -67,7 +67,7 @@
 
                                     document.querySelectorAll('#table_div th').forEach((th) => {
                                         th.style.backgroundColor = '#003D73';
-                                        th.style.color = '#000000';
+                                        th.style.color = 'white';
                                         th.style.textTransform = 'uppercase';
                                         th.style.padding = '12px';
                                         th.style.textAlign = 'left';
@@ -78,7 +78,7 @@
                                     });
 
                                     document.querySelectorAll('#table_div tr:hover').forEach((tr) => {
-                                        tr.style.backgroundColor = '#000000';
+                                        tr.style.backgroundColor = '#ffffff';
                                     });
 
                                     }, 500);
