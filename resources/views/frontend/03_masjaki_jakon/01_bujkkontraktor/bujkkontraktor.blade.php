bd2216269716e8578600184e66ca11ab60f5a1ac
sipjakiblora <sipjakiblora@gmail.com>
2025-02-24 18:20:35 +0700

    g

--- resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/bujkkontraktor.blade.php
+++ resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/bujkkontraktor.blade.php
@@ -88,7 +88,7 @@ color: #45a049;
 
 
             .badgekembali {
-                background: linear-gradient(to right, green, black);
+                background: linear-gradient(to right, green, green;
                 color: white;
                 padding: 10px 20px;
                 border-radius: 10px;
