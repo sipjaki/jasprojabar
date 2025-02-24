547f501ffdad420ad1ffe1c422833ccb26679509
sipjakiblora <sipjakiblora@gmail.com>
2025-02-24 18:49:15 +0700

    g

--- resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/bujkkontraktor.blade.php
+++ resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/bujkkontraktor.blade.php
@@ -128,7 +128,7 @@ color: #45a049;
 
     {{-- <section class="service-two-section" > --}}
         <section class="service-two-section" style="background-image: url('/assets/00_dokmasjaki/03_datajakon/banner.jpg');">
-        <div class="container" style="background: #ffae00;">
+        <div class="container">
             <div class="row row-gutter-y-40">
 
                 <div class="col-12 col-lg-12 col-xl-12">
