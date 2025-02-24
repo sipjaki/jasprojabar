5459d91e53b5177e3f3e14eb9096f051b82f85dc
sipjakiblora <sipjakiblora@gmail.com>
2025-02-24 17:57:06 +0700

    g

--- resources/views/frontend/00_approve/01_cssterpisah/header1.blade.php
+++ resources/views/frontend/00_approve/01_cssterpisah/header1.blade.php
@@ -16,7 +16,7 @@
 								<i class="fa-solid fa-database"></i>
 							</div><!-- topbar-icon -->
 							<div class="topbar-text" style="margin-right: 20px;">
-								<a href="/404">Sistem Informasi Pembina Jasa Konstruksi Pemerintah Kabupaten Blora Provinsi Jawa Tengah</a>
+								<a href="/404">Sistem Informasi Pembina Jasa Konstruksi Dinas Pekerjaan Umum dan Penataan Ruang Kabupaten Blora Provinsi Jawa Tengah</a>
 							</div><!-- topbar-text -->
                             <div class="topbar-icon">
 								<i class="fa-solid fa-envelope"></i>
