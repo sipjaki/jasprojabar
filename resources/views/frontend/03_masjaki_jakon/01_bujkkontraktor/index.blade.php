5e8118dcb4a9439b793efa522cc939950e9efb0e
sipjakiblora <sipjakiblora@gmail.com>
2025-02-24 23:46:36 +0700

    g

--- resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/index.blade.php
+++ resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/index.blade.php
@@ -16,8 +16,8 @@
   </section><!--page-banner-->
 
 
-  <section class="service-two-section">
-    <div class="container" style="background-image: url('/assets/00_dokmasjaki/03_datajakon/banner1.jpg');">
+  <section class="service-two-section" style="background-image: url('/assets/00_dokmasjaki/03_datajakon/banner1.jpg');">
+    <div class="container">
         <div class="row row-gutter-y-40">
             <div class="col-12 col-lg-4 col-xl-4">
                 <div class="service-two-card">
@@ -55,6 +55,26 @@
                     </div><!--service-two-card-content-->
                 </div><!--service-two-card-->
             </div><!--col-12 col-lg-4 col-xl-4-->
+
+            <div class="col-12 col-lg-4 col-xl-4">
+                <div class="service-two-card">
+                    <div class="service-two-imgbox" >
+                        <div class="service-two-image" style="display: flex; justify-content: center;">
+                            <img src="/assets/00_dokmasjaki/03_datajakon/konsultan.png"  class="img-fluid" alt="img-29"  loading="lazy" style="width:70%; padding:30px; ">
+                            <a href="/404"></a>
+                        </div><!-- service-two-image -->
+                    </div><!-- service-two-imgbox -->
+                    <div class="service-two-card-content">
+                        <h4>Asosiasi</h4>
+                        {{-- <p>Dinas Pekerjaan Umum dan Penataan Ruang <br> Pemerintah Kabupaten Blora Provinsi Jawa Tengah</p> --}}
+                        <a href="/404">
+                            <i class="fa-solid fa-arrow-right-long"></i>
+                            <span>Lihat Data</span>
+                        </a>
+                    </div><!--service-two-card-content-->
+                </div><!--service-two-card-->
+            </div><!--col-12 col-lg-4 col-xl-4-->
+
             <div class="col-12 col-lg-4 col-xl-4">
                 <div class="service-two-card">
                     <div class="service-two-imgbox">
