91bf7fcf42029a39e0de6990aa6f478cd12e000e
sipjakiblora <sipjakiblora@gmail.com>
2025-02-25 00:57:20 +0700

    g

--- resources/views/frontend/00_approve/01_cssterpisah/header1.blade.php
+++ resources/views/frontend/00_approve/01_cssterpisah/header1.blade.php
@@ -81,7 +81,7 @@
 								<li><a href="/datajakon/bujk">BUJK Kontraktor dan Konsultan</a></li>
 								{{-- <li><a href="/404">BUJK Konsultan</a></li> --}}
 								<li><a href="/datajakon/skktenagakerja">Tenaga Kerja Konstruksi</a></li>
-								<li><a href="/datajakon/datapaketpekerjaanblora">Profil Paket Pekerjaan</a></li>
+								<li><a href="/datajakon/datapaketpekerjaanblora">Profil Paket Pekerjaan Konstruksi & Konsultasi</a></li>
 							</ul><!-- list-unstyled -->
 						</li><!--has-dropdown-->
 
