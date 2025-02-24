b38e7c33459a1a629041d9aed7e7ec7a0d097597
sipjakiblora <sipjakiblora@gmail.com>
2025-02-25 03:41:41 +0700

    g

--- resources/views/frontend/03_masjaki_jakon/04_profilpaketpekerjaan/01_paketpekerjaantendershow.blade.php
+++ resources/views/frontend/03_masjaki_jakon/04_profilpaketpekerjaan/01_paketpekerjaantendershow.blade.php
@@ -266,10 +266,14 @@ color: #45a049;
                                             <div class="history-card-image" style="width: 70%">
                                                 <img src="/assets/00_dokmasjaki/03_datajakon/prosespaket.png" class="img-fluid" alt="img-25" width="250" style="margin-left: 50px;">
                                             </div><!-- history-card-image -->
+
                                             <div class="history-card-meta history-card-data-content">
-                                                <div class="history-card-years">{{$data->prosespaket->persiapan}}</div>
+                                                <div class="history-card-years {{ $data->prosespaket->pelaksanaan == 'BELUM' ? 'text-danger' : '' }}">
+                                                    {{$data->prosespaket->pelaksanaan}}
+                                                </div>
                                                 {{-- <div class="history-card-date">1 May</div> --}}
                                             </div><!-- history-card-meta -->
+
                                             <div class="history-card-detail"></div><!-- history-card-detail -->
                                             <div class="history-card-content">
                                                 <h3>Persiapan</h3>
