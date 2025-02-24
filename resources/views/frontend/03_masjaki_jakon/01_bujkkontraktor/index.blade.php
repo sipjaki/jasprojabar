c6935a344bbb2c34d6c8f3b502fd0d41e9b3b494
sipjakiblora <sipjakiblora@gmail.com>
2025-02-24 23:48:21 +0700

    g

--- resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/index.blade.php
+++ resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/index.blade.php
@@ -60,7 +60,7 @@
                 <div class="service-two-card">
                     <div class="service-two-imgbox" >
                         <div class="service-two-image" style="display: flex; justify-content: center;">
-                            <img src="/assets/00_dokmasjaki/03_datajakon/konsultan.png"  class="img-fluid" alt="img-29"  loading="lazy" style="width:70%; padding:30px; ">
+                            <img src="/assets/00_dokmasjaki/03_datajakon/asosiasi.png"  class="img-fluid" alt="img-29"  loading="lazy" style="width:70%; padding:30px; ">
                             <a href="/404"></a>
                         </div><!-- service-two-image -->
                     </div><!-- service-two-imgbox -->
