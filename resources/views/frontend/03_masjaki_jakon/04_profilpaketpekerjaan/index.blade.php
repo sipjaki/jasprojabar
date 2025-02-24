16a964bbf0d8aac78a12ba5c9b3ed2e6c820d375
sipjakiblora <sipjakiblora@gmail.com>
2025-02-25 00:59:08 +0700

    g

--- resources/views/frontend/03_masjaki_jakon/04_profilpaketpekerjaan/index.blade.php
+++ resources/views/frontend/03_masjaki_jakon/04_profilpaketpekerjaan/index.blade.php
@@ -72,7 +72,7 @@
                         </div><!-- service-two-image -->
                     </div><!-- service-two-imgbox -->
                     <div class="service-two-card-content">
-                        <h4>Konsultan Tender</h4>
+                        <h4>Konstruksi Konsultasi Tender</h4>
                         <p>Dinas Pekerjaan Umum dan Penataan Ruang <br> Pemerintah Kabupaten Blora Provinsi Jawa Tengah</p>
                         <a href="/datajakon/paketkonsultasitender">
                             <i class="fa-solid fa-arrow-right-long"></i>
@@ -91,7 +91,7 @@
                         </div><!-- service-two-image -->
                     </div><!-- service-two-imgbox -->
                     <div class="service-two-card-content">
-                        <h4>Konsultan Non Tender</h4>
+                        <h4>Konstruksi Konsultasi Non Tender</h4>
                         <p>Dinas Pekerjaan Umum dan Penataan Ruang <br> Pemerintah Kabupaten Blora Provinsi Jawa Tengah</p>
                         <a href="/datajakon/paketkonsultasinontender">
                             <i class="fa-solid fa-arrow-right-long"></i>
