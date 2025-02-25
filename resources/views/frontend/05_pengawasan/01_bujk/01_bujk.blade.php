67eb1d342e7e09599f8bd871b21bc9363fd93a7a
sipjakiblora <sipjakiblora@gmail.com>
2025-02-26 00:29:40 +0700

    g

--- resources/views/frontend/05_pengawasan/01_bujk/01_bujk.blade.php
+++ resources/views/frontend/05_pengawasan/01_bujk/01_bujk.blade.php
@@ -88,7 +88,7 @@ color: #45a049;
 
 
             .badgekembali {
-                background: linear-gradient(to right, green, green);
+                background: linear-gradient(to right, green, white);
                 color: white;
                 padding: 10px 20px;
                 border-radius: 10px;
