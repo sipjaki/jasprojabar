b9e9c43ef77aae993aeb007a9d31b49eb214bc77
sipjakiblora <sipjakiblora@gmail.com>
2025-02-26 01:12:13 +0700

    g

--- resources/views/frontend/05_pengawasan/02_kecelakaankerja/02_kecelakaankerja.blade.php
+++ resources/views/frontend/05_pengawasan/02_kecelakaankerja/02_kecelakaankerja.blade.php
@@ -25,7 +25,7 @@
   <section class="service-two-section" style="background-image: url('/assets/00_dokmasjaki/03_datajakon/banner1.jpg');">
     <div class="container">
         <div class="row row-gutter-y-40">
-            <div class="col-6 col-lg-3 col-xl-3">
+            <div class="col-6 col-lg-4 col-xl-4">
                 <div class="service-two-card">
                     <div class="service-two-imgbox">
                         <div class="service-two-image" style="display: flex; justify-content: center;">
@@ -34,7 +34,7 @@
                         </div><!-- service-two-image -->
                     </div><!-- service-two-imgbox -->
                     <div class="service-two-card-content">
-                        <h4>Profil Paket Pekerjaan Konstruksi dan Konsultasi</h4>
+                        <h4>Daftar Kecelakaan Kerja</h4>
                         {{-- <p>Dinas Pekerjaan Umum dan Penataan Ruang <br> Pemerintah Kabupaten Blora Provinsi Jawa Tengah</p> --}}
                         <a href="/datajakon/paketpekerjaantender">
                             <i class="fa-solid fa-arrow-right-long"></i>
@@ -101,7 +101,7 @@
                 </div><!--service-two-card-->
             </div><!--col-12 col-lg-4 col-xl-4--> --}}
 
-            <div class="col-6 col-lg-3 col-xl-3">
+            <div class="col-6 col-lg-4 col-xl-4">
                 <div class="service-two-card">
                     <div class="service-two-imgbox" >
                         <div class="service-two-image" style="display: flex; justify-content: center;">
@@ -110,7 +110,7 @@
                         </div><!-- service-two-image -->
                     </div><!-- service-two-imgbox -->
                     <div class="service-two-card-content">
-                        <h4>Data Statistik Profil Paket Pekerjaan</h4>
+                        <h4>Data Statistik Kecelakaan Kerja</h4>
                         {{-- <p>Dinas Pekerjaan Umum dan Penataan Ruang <br> Pemerintah Kabupaten Blora Provinsi Jawa Tengah</p> --}}
                         <a href="/datajakon/?">
                             <i class="fa-solid fa-arrow-right-long"></i>
