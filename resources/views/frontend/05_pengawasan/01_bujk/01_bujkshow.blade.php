cdb3ffeec2439531bd15608734b51d1667ad2212
sipjakiblora <sipjakiblora@gmail.com>
2025-02-25 23:08:13 +0700

    g

--- resources/views/frontend/05_pengawasan/01_bujk/01_bujkshow.blade.php
+++ resources/views/frontend/05_pengawasan/01_bujk/01_bujkshow.blade.php
@@ -275,22 +275,35 @@ color: #45a049;
 
                     <body>
                     <div class="table-wrapper">
-                        <table class="fl-table" style="width: 100%; border-collapse: collapse;">
-                            <thead>
-                                <tr>
-                                    <th colspan="12" style="text-align:center; color: black;">No</th>
-                                    <td colspan="12" style="text-align:center;">1</td>
-                                </tr>
-                                <tr>
-                                    <th colspan="12" style="text-align:center; color: black;">Nama Pengurus</th>
-                                    <td colspan="12" style="text-align:center;">{{$data->namaperusahaan}}</td>
-                                </tr>
-                                <tr>
-                                    <th colspan="12" style="text-align:center; color: black;">Nama Pengurus</th>
-                                    <td colspan="12" style="text-align:center;">{{$data->namaperusahaan}}</td>
-                                </tr>
-                            </thead>
-                        </table>
+                    <!-- Add this inside your HTML <head> for FontAwesome -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+
+<table class="fl-table" style="width: 100%; border-collapse: collapse;">
+    <thead>
+        <tr>
+            <th style="text-align:center; color: black;">
+                <!-- Add your icon here -->
+                <i class="fas fa-info-circle"></i>
+            </th>
+            <th colspan="11" style="text-align:center; color: black;">No</th>
+            <td colspan="12" style="text-align:center;">1</td>
+        </tr>
+        <tr>
+            <th style="text-align:center; color: black;">
+                <i class="fas fa-user"></i> <!-- Icon for 'Nama Pengurus' -->
+            </th>
+            <th colspan="11" style="text-align:center; color: black;">Nama Pengurus</th>
+            <td colspan="12" style="text-align:center;">{{$data->namaperusahaan}}</td>
+        </tr>
+        <tr>
+            <th style="text-align:center; color: black;">
+                <i class="fas fa-user"></i> <!-- Same icon as above -->
+            </th>
+            <th colspan="11" style="text-align:center; color: black;">Nama Pengurus</th>
+            <td colspan="12" style="text-align:center;">{{$data->namaperusahaan}}</td>
+        </tr>
+    </thead>
+</table>
 
                     </div>
                                                 </div><!-- document-details-list -->
