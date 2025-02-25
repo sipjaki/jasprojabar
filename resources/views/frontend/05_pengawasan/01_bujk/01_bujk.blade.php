5b7243db6e8b9f1d655499c4a909c98443a520e3
sipjakiblora <sipjakiblora@gmail.com>
2025-02-26 00:29:01 +0700

    g

--- resources/views/frontend/05_pengawasan/01_bujk/01_bujk.blade.php
+++ resources/views/frontend/05_pengawasan/01_bujk/01_bujk.blade.php
@@ -150,14 +150,14 @@ color: #45a049;
                         <section class="document-section" style="margin-top: -100px;">
                             <div class="container" >
                               <ul class="nav nav-pills" id="pills-tab" role="tablist">
-                                <img src="/assets/icon/logokabupatenblora.png" class="img-fluid" alt="img-25" width="50" height="50" style="margin-right: 20px;" loading="lazy">
-                                {{-- <img src="/assets/icon/logopupr.png" class="img-fluid" alt="img-25" width="50" style="margin-right: 20px;" loading="lazy"> --}}
+                                  {{-- <img src="/assets/icon/logopupr.png" class="img-fluid" alt="img-25" width="50" style="margin-right: 20px;" loading="lazy"> --}}
 
-                                <br><p style="font-size: 16px; color:black;">Dinas Pekerjaan Umum dan Penataan Ruang Kabupaten Blora</p>
-                                <div style="display: flex; justify-content: space-between; width: 100%; align-items: center; margin-top: 20px;">
-                                    <!-- Tombol di kiri -->
-                                    <a href="#" style="background: white;">
-                                        <button class="badgekembali" style="border: none; font-size: 12px; cursor: pointer;">
+                                  <br><p style="font-size: 16px; color:black;">Dinas Pekerjaan Umum dan Penataan Ruang Kabupaten Blora</p>
+                                  <div style="display: flex; justify-content: space-between; width: 100%; align-items: center; margin-top: 20px;">
+                                      <!-- Tombol di kiri -->
+                                      <a href="#" style="background: white;">
+                                          <button class="badgekembali" style="border: none; font-size: 12px; cursor: pointer;">
+                                            <img src="/assets/icon/logokabupatenblora.png" class="img-fluid" alt="img-25" width="50" height="50" style="margin-right: 20px;" loading="lazy">
                                             <i class="fas fa-list mr-2" style="margin-right: 10px;"></i> <span style="text-transform: uppercase">{{$title}}</span>
                                         </button>
                                     </a>
