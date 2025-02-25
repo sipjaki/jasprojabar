5b207f930217aad303da90a930bddf943e395153
sipjakiblora <sipjakiblora@gmail.com>
2025-02-25 22:03:46 +0700

    g

--- app/Http/Controllers/PengawasanbujkController.php
+++ app/Http/Controllers/PengawasanbujkController.php
@@ -24,33 +24,32 @@ class PengawasanbujkController extends Controller
         ]);
     }
 
-    // public function paketkonsultasishow($jenispekerjaan)
-    // {
-    //     $datapaketkonsultasi = profilpaketkonsultasi::where('jenispekerjaan', $jenispekerjaan)->first();
+    public function pengawasanbujkshow($kodeproyek)
+    {
+        $datashow = pengawasanbujk::where('kodeproyek', $kodeproyek)->first();
 
-    //     $subdata = prosespaketkonsultasi::all();
-    //     // if (!$databujkkontraktor) {
-    //     //     // Tangani jika kegiatan tidak ditemukan
-    //     //     return redirect()->back()->with('error', 'Kegiatan tidak ditemukan.');
-    //     // }
+        // $subdata = prosespaketkonsultasi::all();
+        // if (!$databujkkontraktor) {
+        //     // Tangani jika kegiatan tidak ditemukan
+        //     return redirect()->back()->with('error', 'Kegiatan tidak ditemukan.');
+        // }
 
-    //     // // Menggunakan paginate() untuk pagination
-    //     // $subdata = bujkkontraktorsub::where('bujkkontraktor_id', $databujkkontraktor->id)->paginate(10);
+        // Menggunakan paginate() untuk pagination
+        // $subdata = bujkkontraktorsub::where('bujkkontraktor_id', $databujkkontraktor->id)->paginate(10);
 
-    //     //   // Menghitung nomor urut mulai
-    //     //     $start = ($subdata->currentPage() - 1) * $subdata->perPage() + 1;
+          // Menghitung nomor urut mulai
+            // $start = ($subdata->currentPage() - 1) * $subdata->perPage() + 1;
 
 
-    // // Ambil data user saat ini
-    // $user = Auth::user();
+    // Ambil data user saat ini
+    $user = Auth::user();
 
-    // return view('frontend.03_masjaki_jakon.04_profilpaketpekerjaan.03_paketkonsultasitendershow', [
-    //     'title' => 'Profil Paket Pekerjaan Konstruksi & Konsultasi Kabupaten Blora',
-    //     'data' => $datapaketkonsultasi,
-    //     'subdata' => $subdata,
-    //     'user' => $user,
+    return view('frontend.05_pengawasan.01_bujk.01_bujkshow', [
+        'title' => 'Profil Paket Pekerjaan Konstruksi & Konsultasi Kabupaten Blora',
+        'data' => $datashow,
+        'user' => $user,
 
-    // ]);
-    // }
+    ]);
+    }
 
 }
