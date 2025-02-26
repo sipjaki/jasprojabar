98016306436956f5c2042c3a2cbffe71b1dfdc2f
sipjakiblora <sipjakiblora@gmail.com>
2025-02-26 10:14:09 +0700

    g

--- resources/views/frontend/00_approve/00_utama/index.blade.php
+++ resources/views/frontend/00_approve/00_utama/index.blade.php
@@ -24,7 +24,7 @@
 				</div><!-- container -->
 			</div><!--item-->
 			<div class="item">
-				<div class="item-slider-bg"  style="background-image: url(assets/00_dokmasjaki/01_beranda/masjaki1.jpg)"></div>
+				<div class="item-slider-bg"  style="background-image: url(assets/00_dokmasjaki/03_datajakon/petaindonesiabaru.png)"></div>
 				<div class="container">
 					<div class="row">
 						<div class="col-md-12">
