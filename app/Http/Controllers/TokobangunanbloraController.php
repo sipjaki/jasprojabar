2b63fb364882c94497dcddadece6133b76f808ab
sipjakiblora <sipjakiblora@gmail.com>
2025-02-28 01:31:34 +0700

    g

--- app/Http/Controllers/TokobangunanbloraController.php
+++ app/Http/Controllers/TokobangunanbloraController.php
@@ -26,32 +26,32 @@ class TokobangunanbloraController extends Controller
         ]);
     }
 
-    // public function bujkkonsultanshow($namalengkap)
-    // {
-    //     $databujkkonsultan = bujkkonsultan::where('namalengkap', $namalengkap)->first();
+    public function tokobangunanblorashow($kecamatanblora)
+    {
+        $datakecamatanblora = kecamatanblora::where('kecamatanblora', $kecamatanblora)->first();
 
-    //     if (!$databujkkonsultan) {
-    //         // Tangani jika kegiatan tidak ditemukan
-    //         return redirect()->back()->with('error', 'Kegiatan tidak ditemukan.');
-    //     }
+        if (!$datakecamatanblora) {
+            // Tangani jika kegiatan tidak ditemukan
+            return redirect()->back()->with('error', 'Kegiatan tidak ditemukan.');
+        }
 
-    //     // Menggunakan paginate() untuk pagination
-    //     $subdata = bujkkonsultansub::where('bujkkonsultan_id', $databujkkonsultan->id)->paginate(10);
+        // Menggunakan paginate() untuk pagination
+        $subdata = tokobangunanblora::where('kecamatanblora_id', $datakecamatanblora->id)->paginate(10);
 
-    //       // Menghitung nomor urut mulai
-    //         $start = ($subdata->currentPage() - 1) * $subdata->perPage() + 1;
+          // Menghitung nomor urut mulai
+            $start = ($subdata->currentPage() - 1) * $subdata->perPage() + 1;
 
 
-    // // Ambil data user saat ini
-    // $user = Auth::user();
+    // Ambil data user saat ini
+    $user = Auth::user();
 
-    // return view('frontend.03_masjaki_jakon.02_bujkkonsultan.bujkkonsultanshow', [
-    //     'title' => 'Data Bujk Konsultan',
-    //     'data' => $databujkkonsultan,
-    //     'subData' => $subdata,  // Jika Anda ingin mengirimkan data sub kontraktor juga
-    //     'user' => $user,
-    //     'start' => $start,
-    // ]);
-    // }
+    return view('frontend.06_rantaipasok.03_tokobangunanblora.tokobangunanblorashow', [
+        'title' => 'Data Bujk Konsultan',
+        'data' => $datakecamatanblora,
+        'subData' => $subdata,  // Jika Anda ingin mengirimkan data sub kontraktor juga
+        'user' => $user,
+        'start' => $start,
+    ]);
+    }
 
 }
