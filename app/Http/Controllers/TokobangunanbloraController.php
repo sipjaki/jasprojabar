f9be064fb40d078d541c355a6dd5c6d65d628584
sipjakiblora <sipjakiblora@gmail.com>
2025-02-28 02:21:47 +0700

    g

--- app/Http/Controllers/TokobangunanbloraController.php
+++ app/Http/Controllers/TokobangunanbloraController.php
@@ -54,4 +54,32 @@ class TokobangunanbloraController extends Controller
     ]);
     }
 
+    public function tokobangunanbloralistdetails($namatokobangunan)
+    {
+        $datatokobangunan = tokobangunanblora::where('namatokobangunan', $namatokobangunan)->first();
+
+        if (!$datatokobangunan) {
+            // Tangani jika kegiatan tidak ditemukan
+            return redirect()->back()->with('error', 'Kegiatan tidak ditemukan.');
+        }
+
+        // Menggunakan paginate() untuk pagination
+        $subdata = kecamatanblora::where('tokobangunanblora_id', $datatokobangunan->id)->paginate(10);
+
+          // Menghitung nomor urut mulai
+            $start = ($subdata->currentPage() - 1) * $subdata->perPage() + 1;
+
+
+    // Ambil data user saat ini
+    $user = Auth::user();
+
+    return view('frontend.06_rantaipasok.03_tokobangunanblora.tokobangunanbloralistshow', [
+        'title' => 'Toko Bangunan Cakupan Per Kecamatan',
+        'data' => $datatokobangunan,
+        'subData' => $subdata,  // Jika Anda ingin mengirimkan data sub kontraktor juga
+        'user' => $user,
+        'start' => $start,
+    ]);
+    }
+
 }
