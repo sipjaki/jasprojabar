f2bd460fb3e57a2e4b0d454107498aacda665fb3
sipjakiblora <sipjakiblora@gmail.com>
2025-02-28 11:53:35 +0700

    g

--- resources/views/frontend/07_ahsp/04_shst/shstindex.blade.php
+++ resources/views/frontend/07_ahsp/04_shst/shstindex.blade.php
@@ -239,7 +239,49 @@ color: #45a049;
                             </div>
                         </section>
 
-                    <section class="document-section" style="margin-top: -215px; margin-bottom:35px;" >
+                        <section class="document-section" style="margin-top: -215px; margin-bottom:35px;" >
+
+                        <div style="display: flex; justify-content: space-between; width: 100%; align-items: center; margin-top: 20px;">
+                            <!-- Tombol di kiri -->
+                            <a href="#" style="background: white;">
+                                <button class="badgepupr" style="border: none; font-size: 12px; cursor: pointer;">
+                                    <img src="/assets/icon/pupr.png" class="img-fluid" alt="img-25" width="50" height="50" style="margin-right: 20px;" loading="lazy">
+                                    <i class="fas fa-list mr-2" style="margin-right: 10px;"></i> <span style="text-transform: uppercase; font-weight:800; color:black">Bangunan Kantor dan Bangunan Lainya Per M2 (Meter Persegi)</span>
+                                </button>
+                            </a>
+                            <body>
+
+                            </section>
+
+                        <div class="table-wrapper" style="margin-top:-130px; margin-bottom:150px;">
+                        <table class="fl-table">
+                            <thead>
+                                <tr>
+                                    <th style="text-align:center"> No</th>
+                                    <th style="text-align:center"> Pekerjaan</th>
+                                    <th style="text-align:center"> Harga</th>
+                                    {{-- <th style="text-align:center" >View</th> --}}
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                                @php
+                                $start = ($data->currentPage() - 1) * $data->perPage() + 1;
+                                    @endphp
+
+                                @foreach ($data as $item )
+                                <tr>
+                                    <td style="text-align: center;">1</td>
+                                    <td>Bangunan Tidak Sederhana</td>
+                                    <td>{{ 'Rp ' . number_format($item->bangunankantortidaksederhana, 0, ',', '.') }}</td>
+                                </tr>
+
+                                @endforeach
+                            </tbody>
+                        </table>
+                        </div>
+
+                        <section class="document-section" style="margin-top: -215px; margin-bottom:35px;" >
 
                         <div style="display: flex; justify-content: space-between; width: 100%; align-items: center; margin-top: 20px;">
                             <!-- Tombol di kiri -->
