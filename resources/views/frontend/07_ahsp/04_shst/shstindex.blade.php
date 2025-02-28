6ec67fcb9cf49917c83324b73d11d89f13938d95
sipjakiblora <sipjakiblora@gmail.com>
2025-02-28 11:45:07 +0700

    g

--- resources/views/frontend/07_ahsp/04_shst/shstindex.blade.php
+++ resources/views/frontend/07_ahsp/04_shst/shstindex.blade.php
@@ -221,17 +221,19 @@ color: #45a049;
                             </div>
                         </section>
 
+                    <section>
 
-                        <body>
-                            <div style="display: flex; justify-content: space-between; width: 100%; align-items: center; margin-top: 20px;">
-                                <!-- Tombol di kiri -->
-                                <a href="#" style="background: white;">
-                                    <button class="badgekembali" style="border: none; font-size: 12px; cursor: pointer;">
-                                      <img src="/assets/icon/pupr.png" class="img-fluid" alt="img-25" width="50" height="50" style="margin-right: 20px;" loading="lazy">
-                                      <i class="fas fa-list mr-2" style="margin-right: 10px;"></i> <span style="text-transform: uppercase; font-weight:800; color:black">Bangunan Kantor dan Bangunan Lainya Per M2 (Meter Persegi)</span>
-                                  </button>
-                              </a>
+                        <div style="display: flex; justify-content: space-between; width: 100%; align-items: center; margin-top: 20px;">
+                            <!-- Tombol di kiri -->
+                            <a href="#" style="background: white;">
+                                <button class="badgekembali" style="border: none; font-size: 12px; cursor: pointer;">
+                                    <img src="/assets/icon/pupr.png" class="img-fluid" alt="img-25" width="50" height="50" style="margin-right: 20px;" loading="lazy">
+                                    <i class="fas fa-list mr-2" style="margin-right: 10px;"></i> <span style="text-transform: uppercase; font-weight:800; color:black">Bangunan Kantor dan Bangunan Lainya Per M2 (Meter Persegi)</span>
+                                </button>
+                            </a>
+                            <body>
 
+                            </section>
 
                         <div class="table-wrapper" style="margin-top:-130px; margin-bottom:150px;">
                         <table class="fl-table">
