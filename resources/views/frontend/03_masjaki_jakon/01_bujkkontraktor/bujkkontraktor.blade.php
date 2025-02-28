cce3fec336a575ef0926e7d7967c4019a2b59e04
sipjakiblora <sipjakiblora@gmail.com>
2025-03-01 01:23:19 +0700

    g

--- resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/bujkkontraktor.blade.php
+++ resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/bujkkontraktor.blade.php
@@ -188,8 +188,10 @@ color: #45a049;
 
                             <div class="table-wrapper" style="margin-top:-130px; position: relative;">
                                 <div style="display: flex; justify-content: space-between; margin-bottom: 10px; align-items: center;">
-                                    <div class="pagination-info" style="color: black; font-weight: 500;">
-                                        Data Ke {{ $data->firstItem() }} Sampai {{ $data->lastItem() }} Dari {{ $data->total() }} Jumlah {{$title}}
+                                    <div style="border: 1px solid #ccc; padding: 10px; border-radius: 5px; background-color: #f9f9f9; display: inline-block;">
+                                        <div class="pagination-info" style="color: black; font-weight: 500;">
+                                            Data Ke {{ $data->firstItem() }} Sampai {{ $data->lastItem() }} Dari {{ $data->total() }} Jumlah {{$title}}
+                                        </div>
                                     </div>
                                     <div>
                                         <label for="entries" style="margin-right: 10px; font-weight: bold;">Show:</label>
