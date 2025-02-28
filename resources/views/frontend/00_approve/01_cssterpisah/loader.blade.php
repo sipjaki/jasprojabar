611e0cfe44b51aab2e7d820c18dd56a930c60bcd
sipjakiblora <sipjakiblora@gmail.com>
2025-03-01 02:15:05 +0700

    g

--- resources/views/frontend/00_approve/01_cssterpisah/loader.blade.php
+++ resources/views/frontend/00_approve/01_cssterpisah/loader.blade.php
@@ -1,12 +1,7 @@
-<div class="container">
-        <div class="spinner"></div>
-        <p>Dinas PUPR Kab Blora</p>
-</div>
 
 <div id="pre-loader">
     <div id="loader-logo"></div><!-- loader-logo -->
     <div id="loader-circle"></div><!-- loader-circle -->
-    <p class="loader-text">Dinas PUPR Kab Blora<br>Provinsi Jawa Tengah</p>
     <div class="loader-section section-left"></div><!-- loader-section -->
     <div class="loader-section section-right"></div><!-- loader-section -->
 </div><!-- pre-loader -->
