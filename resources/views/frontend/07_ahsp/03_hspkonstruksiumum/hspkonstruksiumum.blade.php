583f970427bf864fc683a3b058a4600e110a7489
sipjakiblora <sipjakiblora@gmail.com>
2025-02-28 12:50:37 +0700

    g

--- resources/views/frontend/07_ahsp/03_hspkonstruksiumum/hspkonstruksiumum.blade.php
+++ resources/views/frontend/07_ahsp/03_hspkonstruksiumum/hspkonstruksiumum.blade.php
@@ -246,7 +246,7 @@ color: #45a049;
                             <a href="#" style="background: white;">
                                 <button class="badgepupr" style="border: none; font-size: 12px; cursor: pointer;">
                                     <img src="/assets/icon/pupr.png" class="img-fluid" alt="img-25" width="50" height="50" style="margin-right: 20px;" loading="lazy">
-                                    <i style="color: black" class="fas fa-building mr-2" style="margin-right: 10px;"></i> <span style="text-transform: uppercase; font-weight:800; color:black">Bangunan Kantor dan Bangunan Lainya Per M2 (Meter Persegi)</span>
+                                    <i style="color: black" class="fas fa-building mr-2" style="margin-right: 10px;"></i> <span style="text-transform: uppercase; font-weight:800; color:black">HSP Konstruksi UMUM</span>
                                 </button>
                             </a>
                             <body>
