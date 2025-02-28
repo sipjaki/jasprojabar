f98702f6dab7563e412b009b22754d5e6e644b5c
sipjakiblora <sipjakiblora@gmail.com>
2025-02-28 12:49:03 +0700

    g

--- app/Http/Controllers/HspkonstruksiumumController.php
+++ app/Http/Controllers/HspkonstruksiumumController.php
@@ -13,13 +13,13 @@ class HspkonstruksiumumController extends Controller
     //
     public function index()
     {
-        $data = hspkonstruksiumum::paginate(10);
+        // $data = hspkonstruksiumum::paginate(10);
         $user = Auth::user();
 
         return view('frontend.07_ahsp.03_hspkonstruksiumum.hspkonstruksiumum', [
             'title' => 'Standar Harga Satuan Tertinggi Kabupaten Blora Tahun 2025',
             'user' => $user, // Mengirimkan data paginasi ke view
-            'data' => $data, // Mengirimkan data paginasi ke view
+            // 'data' => $data, // Mengirimkan data paginasi ke view
         ]);
     }
 
