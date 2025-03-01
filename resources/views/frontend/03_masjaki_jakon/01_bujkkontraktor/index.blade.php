dbd9f08d3dd76e4c2073a4c0adedd5a2f8872cdb
sipjakiblora <sipjakiblora@gmail.com>
2025-03-01 14:12:19 +0700

    g

--- resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/index.blade.php
+++ resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/index.blade.php
@@ -46,7 +46,7 @@
                         </div><!-- service-two-image -->
                     </div><!-- service-two-imgbox -->
                     <div class="service-two-card-content">
-                        <h4>BUJK Konsultan</h4>
+                        <h4>BUJK Konsultasi Konstruksi</h4>
                         {{-- <p>Dinas Pekerjaan Umum dan Penataan Ruang <br> Pemerintah Kabupaten Blora Provinsi Jawa Tengah</p> --}}
                         <a href="/datajakon/bujkkonsultan">
                             <i class="fa-solid fa-arrow-right-long"></i>
