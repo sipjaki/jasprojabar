9f422fc3f3aef2eb156ade390c16fccea0d96183
sipjakiblora <sipjakiblora@gmail.com>
2025-03-01 15:55:57 +0700

    g

--- resources/views/frontend/03_masjaki_jakon/03_tenagakerjakonstruksi/tenagakerjakonstruksi.blade.php
+++ resources/views/frontend/03_masjaki_jakon/03_tenagakerjakonstruksi/tenagakerjakonstruksi.blade.php
@@ -88,7 +88,7 @@ color: #45a049;
 
 
             .badgekembali {
-                background: linear-gradient(to right, green, green);
+                background: linear-gradient(to right, white, green);
                 color: white;
                 padding: 10px 20px;
                 border-radius: 10px;
@@ -150,29 +150,29 @@ color: #45a049;
                         <section class="document-section" style="margin-top: -100px;">
                             <div class="container" >
                               <ul class="nav nav-pills" id="pills-tab" role="tablist">
-                                <img src="/assets/icon/logokabupatenblora.png" class="img-fluid" alt="img-25" width="50" height="50" style="margin-right: 20px;" loading="lazy">
-                                {{-- <img src="/assets/icon/logopupr.png" class="img-fluid" alt="img-25" width="50" style="margin-right: 20px;" loading="lazy"> --}}
+                                  {{-- <img src="/assets/icon/logopupr.png" class="img-fluid" alt="img-25" width="50" style="margin-right: 20px;" loading="lazy"> --}}
 
-                                <br><p style="font-size: 16px; color:black;">Dinas Pekerjaan Umum dan Penataan Ruang Kabupaten Blora</p>
-                                <div style="display: flex; justify-content: space-between; width: 100%; align-items: center; margin-top: 20px;">
-                                    <!-- Tombol di kiri -->
-                                    <a href="#" style="background: white;">
-                                        <button class="badgekembali" style="border: none; font-size: 12px; cursor: pointer;">
-                                            <i class="fas fa-list mr-2" style="margin-right: 10px;"></i> <span style="text-transform: uppercase">{{$title}}</span>
+                                  {{-- <br><p style="font-size: 16px; color:black;">Dinas Pekerjaan Umum dan Penataan Ruang Kabupaten Blora</p> --}}
+                                  <div style="display: flex; justify-content: space-between; width: 100%; align-items: center; margin-top: 20px;">
+                                      <!-- Tombol di kiri -->
+                                      <a href="#" style="background: white;">
+                                          <button class="badgekembali" style="border: none; font-size: 12px; cursor: pointer;">
+                                            <img src="/assets/icon/logokabupatenblora.png" class="img-fluid" alt="img-25" width="50" height="50" style="margin-right: 20px;" loading="lazy">
+                                            <i class="fas fa-edit mr-2" style="margin-right: 10px; color:#ffd100;"></i> <span style="text-transform: uppercase; color:black;">{{$title}}</span>
                                         </button>
                                     </a>
 
                                     <!-- Tombol dan kolom pencarian di kanan -->
                                     <div style="display: flex; align-items: center;">
-
+{{--
                                         <div style="position: relative; display: inline-block; margin-right:10px;">
                                             <input type="search" placeholder="Pencarian...." style="border: 1px solid #ccc; padding: 5px 10px; font-size: 14px; border-radius: 5px; width: 200px;">
                                             <i class="fas fa-search" style="position: absolute; right: 10px; top: 50%; transform: translateY(-50%); font-size: 16px; color: #888;"></i>
-                                        </div>
+                                        </div> --}}
 
-                                        <a href="/datajakon/skktenagakerja" style="background: white;">
+                                        <a href="/datajakon/bujk" style="background: white;">
                                             <button class="badgekembali" style="border: none; font-size: 12px; cursor: pointer; margin-right: 10px;">
-                                                <i class="fas fa-arrow-circle-left mr-2" style="margin-right: 15px;"></i> KEMBALI
+                                                <i class="fas fa-arrow-circle-left mr-2" style="margin-right: 20px; color:red;"></i><span style="color: black">KEMBALI</span>
                                             </button>
                                         </a>
 
@@ -185,62 +185,138 @@ color: #45a049;
 
 
                         <body>
-                        <div class="table-wrapper" style="margin-top:-130px;">
-                        <table class="fl-table">
-                            <thead>
-                                <tr>
-                                    <th style="text-align:center"> No</th>
-                                    <th style="text-align:center"> Nama Lengkap</th>
-                                    <th style="text-align:center" >Alamat</th>
-                                    {{-- <th style="text-align:center" >Sekolah/Universitas</th> --}}
-                                    {{-- <th style="text-align:center" >Pendidikan</th> --}}
-                                    {{-- <th style="text-align:center" >Jurusan</th> --}}
-                                    {{-- <th style="text-align:center" >Tahun Lulus</th> --}}
-                                    <th style="text-align:center" >Jabatan Kerja</th>
-                                    <th style="text-align:center" >Jenjang</th>
-                                    {{-- <th style="text-align:center" >LPS Penerbit</th>
-                                    <th style="text-align:center" >Terbit</th>
-                                    <th style="text-align:center" >Habis</th> --}}
-                                    <th style="text-align:center" >View</th>
-                                </tr>
-                            </thead>
-                            <tbody>
 
-                                @php
-                                $start = ($data->currentPage() - 1) * $data->perPage() + 1;
-                                    @endphp
+                            <div class="table-wrapper" style="margin-top:-130px; position: relative;">
+                                <div style="display: flex; justify-content: space-between; margin-bottom: 10px; align-items: center;">
+                                    <div class="pagination-info-box" style="margin-right:250px; padding: 10px; border: 1px solid black; background-color: #f9f9f9; border-radius: 5px;">
+                                        <div class="pagination-info" style="color: black; font-weight: 500;">
+                                            Data Ke {{ $data->firstItem() }} Sampai {{ $data->lastItem() }} Dari {{ $data->total() }} Jumlah {{$title}}
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <label for="entries" style="margin-right: 5px; font-weight: bold;">Show:</label>
+                                        <select id="entries" onchange="updateEntries()" style="padding: 5px; border: 1px solid black; background-color: white;">
+                                            <option value="10">10</option>
+                                            <option value="25">25</option>
+                                            <option value="50">50</option>
+                                            <option value="75">75</option>
+                                            <option value="100">100</option>
+                                            <option value="150">150</option>
+                                            <option value="200">200</option>
+                                        </select>
+                                    </div>
+                                    <div style="position: relative; display: inline-block; margin-right:10px;">
+                                        <input type="search" id="searchInput" placeholder="Badan Usaha ...." onkeyup="searchTable()" style="border: 1px solid #ccc; padding: 10px 20px; font-size: 14px; border-radius: 10px; width: 300px;">
+                                        <i class="fas fa-search" style="position: absolute; right: 10px; top: 50%; transform: translateY(-50%); font-size: 16px; color: #888;"></i>
+                                    </div>
+                                </div>
 
-                                @foreach ($data as $item )
-                                <tr>
-                                    <td>{{ $loop->iteration + $start - 1 }}</td>
-                                    <td>{{$item->nama}}</td>
-                                    <td>{{$item->alamat}}</td>
-                                    {{-- <td>{{$item->namasekolah->namasekolah}}</td>
-                                    <td>{{$item->jenjangpendidikan->jenjangpendidikan}}</td>
-                                    <td>{{$item->jurusan->jurusan}}</td>
-                                    <td>{{$item->tahunlulus}}</td> --}}
-                                    <td>{{$item->jabatankerja->jabatankerja}}</td>
-                                    <td>{{$item->jenjang->jenjang}}</td>
-                                    {{-- <td>{{$item->lpspenerbit->lpspenerbit}}</td>
-                                    <td>{{ \Carbon\Carbon::parse($item->tanggalterbit)->isoFormat('D MMMM YYYY') }}</td>
-                                    <td>{{ \Carbon\Carbon::parse($item->tanggalhabis)->isoFormat('D MMMM YYYY') }}</td>
-                                    <td>{{$item->statusterbit}}</td> --}}
-                                    <td style="text-align: center">
-                                        <a href="/datajakon/skktenagakerjablora/{{$item->nama}}">
-                                            <i class="fas fa-eye view-icon" onclick="alert('View clicked!')"></i>
-                                        </a>
-                                    </td>
-                                </tr>
+                                <table class="fl-table" id="sortableTable" style="margin-top: 15px; width: 100%; border-collapse: collapse;">
+                                    <thead>
+                                        <tr>
+                                            <th onclick="sortTable(0)" style="cursor:pointer; text-align:center"> No <span class="sort-icon">⇅</span></th>
+                                            <th onclick="sortTable(1)" style="cursor:pointer; text-align:center"> Nama Lengkap <span class="sort-icon">⇅</span></th>
+                                            <th onclick="sortTable(2)" style="cursor:pointer; text-align:center"> Sekolah/Universitas/Instansi<span class="sort-icon">⇅</span></th>
+                                            <th onclick="sortTable(3)" style="cursor:pointer; text-align:center"> Jenjang Pendidikan <span class="sort-icon">⇅</span></th>
+                                            <th onclick="sortTable(4)" style="cursor:pointer; text-align:center"> Jurusan <span class="sort-icon">⇅</span></th>
+                                            <th onclick="sortTable(5)" style="cursor:pointer; text-align:center"> Tahun Lulus <span class="sort-icon">⇅</span></th>
+                                            <th onclick="sortTable(6)" style="cursor:pointer; text-align:center"> Jabatan Kerja <span class="sort-icon">⇅</span></th>
+                                            <th onclick="sortTable(7)" style="cursor:pointer; text-align:center"> Jenjang <span class="sort-icon">⇅</span></th>
+                                            <th onclick="sortTable(8)" style="cursor:pointer; text-align:center"> LPS Penerbit<span class="sort-icon">⇅</span></th>
+                                            <th onclick="sortTable(9)" style="cursor:pointer; text-align:center"> Tanggal Terbit <span class="sort-icon">⇅</span></th>
+                                            <th onclick="sortTable(9)" style="cursor:pointer; text-align:center"> Tanggal Habis <span class="sort-icon">⇅</span></th>
+                                            <th onclick="sortTable(10)" style="cursor:pointer; text-align:center"> Status Terbit<span class="sort-icon">⇅</span></th>
+                                            <th style="text-align:center"> View </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody id="tableBody">
+                                        @php $start = ($data->currentPage() - 1) * $data->perPage() + 1; @endphp
+                                        @foreach ($data as $item )
+                                        <tr>
+                                            <td style="text-align: center;">{{ $loop->iteration + $start - 1 }}</td>
+                                            <td class="namalengkap">{{$item->nama}}</td>
+                                            <td>{{$item->alamat}}</td>
+                                            <td>{{$item->namasekolah_id->namasekolah}}</td>
+                                            <td>{{$item->jenjangpendidikan_id->jenjangpendidikan}}</td>
+                                            <td>{{$item->jurusan_id->jurusan}}</td>
+                                            <td>{{$item->tahunlulus}}</td>
+                                            <td>{{$item->jabatankerja_id->jabatankerja}}</td>
+                                            <td>{{$item->jenjang_id->jenjang}}</td>
+                                            <td>{{$item->lpspenerbit_id->lpspenerbit}}</td>
+                                            <td>{{ \Carbon\Carbon::parse($item->tanggalterbit)->isoFormat('D MMMM YYYY') }}</td>
+                                            <td>{{ \Carbon\Carbon::parse($item->tanggalhabis)->isoFormat('D MMMM YYYY') }}</td>
+                                            <td>{{$item->statusterbit}}</td>
+                                            <td style="text-align: center">
+                                                <a href="/datajakon/skktenagakerjablora/{{$item->nama}}">
+                                                    <i class="fas fa-eye view-icon" onclick="alert('View clicked!')"></i>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
 
-                                @endforeach
-                            </tbody>
-                        </table>
-                        </div>
-                                            </div><!-- donate-box-inner -->
+                                <div class="pagination-container" style="margin-top: 50px; display: flex; flex-direction: column; align-items: center;">
+                                    <ul class="pagination-paginate" style="display: flex; padding-left: 0; list-style: none; margin-top: 10px;">
+                                        <li class="page-item {{ $data->onFirstPage() ? 'disabled' : '' }}" style="margin-right: 5px;">
+                                            <a class="page-link" href="{{ $data->previousPageUrl() }}">
+                                                <i class="fas fa-arrow-left" style="margin-right: 10px;"></i>Previous
+                                            </a>
+                                        </li>
+                                        <li class="page-item {{ $data->hasMorePages() ? '' : 'disabled' }}" style="margin-right: 5px;">
+                                            <a class="page-link" href="{{ $data->nextPageUrl() }}">
+                                                Next <i class="fas fa-arrow-right" style="margin-left: 10px;"></i>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <style>
+                                .sort-icon {
+                                    font-size: 16px;
+                                    color: gray;
+                                    margin-left: 5px;
+                                }
+                                th {
+                                    padding: 10px;
+                                    border-bottom: 2px solid black;
+                                }
+                                th:hover {
+                                    background-color: #f2f2f2;
+                                }
+                            </style>
+
+                            <script>
+                            function updateEntries() {
+                                let selectedValue = document.getElementById("entries").value;
+                                let url = new URL(window.location.href);
+                                url.searchParams.set("perPage", selectedValue);
+                                window.location.href = url.toString();
+                            }
+                            function searchTable() {
+                            let input = document.getElementById("searchInput").value;
+
+                            fetch(`/datajakon/skktenagakerjablora?search=${input}`)
+                                .then(response => response.text())
+                                .then(html => {
+                                    let parser = new DOMParser();
+                                    let doc = parser.parseFromString(html, "text/html");
+                                    let newTableBody = doc.querySelector("#tableBody").innerHTML;
+                                    document.querySelector("#tableBody").innerHTML = newTableBody;
+                                })
+                                .catch(error => console.error("Error fetching search results:", error));
+                        }
+
+                                </script>
+
+
+
+                        </div><!-- donate-box-inner -->
                                         </div><!-- col-xl-8 col-lg-12 -->
                                     </div><!-- row -->
                                 </div><!-- container -->
-                                @include('frontend.00_approve.01_cssterpisah.paginator')
+                                {{-- @include('frontend.00_approve.01_cssterpisah.paginator') --}}
                             </section><!-- donate-section -->
 
 
