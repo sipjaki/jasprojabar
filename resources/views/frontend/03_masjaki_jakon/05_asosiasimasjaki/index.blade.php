5d79cb5b380398fa7d712559b48775c08f3ec897
sipjakiblora <sipjakiblora@gmail.com>
2025-03-01 15:03:22 +0700

    g

--- resources/views/frontend/03_masjaki_jakon/05_asosiasimasjaki/index.blade.php
+++ resources/views/frontend/03_masjaki_jakon/05_asosiasimasjaki/index.blade.php
@@ -309,6 +309,131 @@ color: #45a049;
 
                                 </script>
 
+                        <body>
+                            <div class="table-wrapper" style="margin-top:-130px; position: relative;">
+                                <div style="display: flex; justify-content: space-between; margin-bottom: 10px; align-items: center;">
+                                    <div class="pagination-info-box" style="margin-right:150px; padding: 10px; border: 1px solid black; background-color: #f9f9f9; border-radius: 5px;">
+                                        <div class="pagination-info" style="color: black; font-weight: 500;">
+                                            Data Ke {{ $data->firstItem() }} Sampai {{ $data->lastItem() }} Dari {{ $data->total() }} Jumlah {{$title}}
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <label for="entries" style="margin-right: 5px; font-weight: bold;">Show:</label>
+                                        <select id="entries" onchange="updateEntries()" style="padding: 5px; border: 1px solid black; background-color: white;">
+                                            <option value="10">10</option>
+                                            <option value="25">25</option>
+                                            <option value="50">50</option>
+                                            <option value="75">75</option>
+                                            <option value="100">100</option>
+                                            <option value="150">150</option>
+                                            <option value="200">200</option>
+                                        </select>
+                                    </div>
+                                    {{-- <div style="position: relative; display: inline-block; margin-right:10px;">
+                                        <input type="search" id="searchInput" placeholder="Badan Usaha ...." onkeyup="searchTable()" style="border: 1px solid #ccc; padding: 10px 20px; font-size: 14px; border-radius: 10px; width: 300px;">
+                                        <i class="fas fa-search" style="position: absolute; right: 10px; top: 50%; transform: translateY(-50%); font-size: 16px; color: #888;"></i>
+                                    </div> --}}
+                                </div>
+
+                                <table class="fl-table" id="sortableTable" style="margin-top: 15px; width: 100%; border-collapse: collapse;">
+                                    <thead>
+                                        <tr>
+                                            <th onclick="sortTable(0)" style="cursor:pointer; text-align:center; width:100px;"> No <span class="sort-icon">⇅</span></th>
+                                            <th onclick="sortTable(1)" style="cursor:pointer; text-align:center"> Nama Asosiasi <span class="sort-icon">⇅</span></th>
+                                            <th onclick="sortTable(2)" style="cursor:pointer; text-align:center"> Jumlah <span class="sort-icon">⇅</span></th>
+                                            {{-- <th onclick="sortTable(3)" style="cursor:pointer; text-align:center"> No Telepon <span class="sort-icon">⇅</span></th> --}}
+                                            {{-- <th onclick="sortTable(4)" style="cursor:pointer; text-align:center"> Email <span class="sort-icon">⇅</span></th>
+                                            <th onclick="sortTable(5)" style="cursor:pointer; text-align:center"> NIB <span class="sort-icon">⇅</span></th>
+                                            <th onclick="sortTable(6)" style="cursor:pointer; text-align:center"> PJU <span class="sort-icon">⇅</span></th>
+                                            <th onclick="sortTable(7)" style="cursor:pointer; text-align:center"> Akte <span class="sort-icon">⇅</span></th>
+                                            <th onclick="sortTable(8)" style="cursor:pointer; text-align:center"> Tanggal <span class="sort-icon">⇅</span></th>
+                                            <th onclick="sortTable(9)" style="cursor:pointer; text-align:center"> Notaris <span class="sort-icon">⇅</span></th>
+                                            <th onclick="sortTable(10)" style="cursor:pointer; text-align:center"> Pengesahan <span class="sort-icon">⇅</span></th> --}}
+                                            {{-- <th style="text-align:center"> View </th> --}}
+                                        </tr>
+                                    </thead>
+                                    <tbody id="tableBody">
+                                        @php
+                                                $nomor = $start ?? 1; // Pastikan $start memiliki nilai default
+                                        @endphp
+
+                                        @foreach ($databujkkonsultan as $item )
+
+                                        <tr>
+                                            <td style="text-align: center;">{{ $nomor++ }}</td>
+                                            <td>{{$item->asosiasimasjaki->namaasosiasi}}</td>
+                                            <td style="text-align: center;">{{$item->jumlah}}</td>
+                                            {{-- <td>{{$item->email}}</td>
+                                            <td>{{$item->nib}}</td>
+                                            <td>{{$item->pju}}</td>
+                                            <td>{{$item->no_akte}}</td>
+                                            <td>{{ \Carbon\Carbon::parse($item->tanggal)->isoFormat('D MMMM YYYY') }}</td>
+                                            <td>{{$item->nama_notaris}}</td>
+                                            <td>{{$item->no_pengesahan}}</td> --}}
+                                            {{-- <td style="text-align: center">
+                                                <a href="/datajakon/bujkkontraktor/{{$item->namalengkap}}">
+                                                    <i class="fas fa-eye view-icon" onclick="alert('View clicked!')"></i>
+                                                </a>
+                                            </td> --}}
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+
+                                {{-- <div class="pagination-container" style="margin-top: 50px; display: flex; flex-direction: column; align-items: center;">
+                                    <ul class="pagination-paginate" style="display: flex; padding-left: 0; list-style: none; margin-top: 10px;">
+                                        <li class="page-item {{ $data->onFirstPage() ? 'disabled' : '' }}" style="margin-right: 5px;">
+                                            <a class="page-link" href="{{ $data->previousPageUrl() }}">
+                                                <i class="fas fa-arrow-left" style="margin-right: 10px;"></i>Previous
+                                            </a>
+                                        </li>
+                                        <li class="page-item {{ $data->hasMorePages() ? '' : 'disabled' }}" style="margin-right: 5px;">
+                                            <a class="page-link" href="{{ $data->nextPageUrl() }}">
+                                                Next <i class="fas fa-arrow-right" style="margin-left: 10px;"></i>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div> --}}
+                            </div>
+
+                            <style>
+                                .sort-icon {
+                                    font-size: 16px;
+                                    color: gray;
+                                    margin-left: 5px;
+                                }
+                                th {
+                                    padding: 10px;
+                                    border-bottom: 2px solid black;
+                                }
+                                th:hover {
+                                    background-color: #f2f2f2;
+                                }
+                            </style>
+
+                            <script>
+                            function updateEntries() {
+                                let selectedValue = document.getElementById("entries").value;
+                                let url = new URL(window.location.href);
+                                url.searchParams.set("perPage", selectedValue);
+                                window.location.href = url.toString();
+                            }
+                            function searchTable() {
+                            let input = document.getElementById("searchInput").value;
+
+                            fetch(`/datajakon/asosiasimasjaki?search=${input}`)
+                                .then(response => response.text())
+                                .then(html => {
+                                    let parser = new DOMParser();
+                                    let doc = parser.parseFromString(html, "text/html");
+                                    let newTableBody = doc.querySelector("#tableBody").innerHTML;
+                                    document.querySelector("#tableBody").innerHTML = newTableBody;
+                                })
+                                .catch(error => console.error("Error fetching search results:", error));
+                        }
+
+                                </script>
+
 
 
                         </div><!-- donate-box-inner -->
