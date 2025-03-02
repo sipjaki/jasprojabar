0169f51a251f612cf3f0001608368a4fbad8bd50
sipjakiblora <sipjakiblora@gmail.com>
2025-03-02 13:35:56 +0700

    g

--- resources/views/frontend/05_pengawasan/01_bujk/01_bujkshow.blade.php
+++ resources/views/frontend/05_pengawasan/01_bujk/01_bujkshow.blade.php
@@ -169,7 +169,7 @@ color: #45a049;
                         <div class="document-details">
 							<div class="document-details-list">
 
-                                <div class="centered-button" style="display: flex; justify-content: space-between; align-items: center; margin-top: -15px;">
+                                <div class="centered-button" style="display: flex; justify-content: space-between; align-items: center; margin-top: -25px;">
                                     <!-- Tombol di kiri -->
                                     <button class="green-button">
                                         <img src="/assets/icon/logokabupatenblora.png" class="img-fluid" alt="img-25" width="50" height="50" style="margin-right: 10px;" loading="lazy">
@@ -191,7 +191,7 @@ color: #45a049;
                                         </head>
 
                     <body>
-                    <div class="table-wrapper">
+                    <div class="table-wrapper" style="margin-top: 25px;">
                     <!-- Add this inside your HTML <head> for FontAwesome -->
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
 
