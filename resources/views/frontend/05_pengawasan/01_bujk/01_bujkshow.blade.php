ef0aee52097aae2af292ac9855836ab4e12e1a6c
sipjakiblora <sipjakiblora@gmail.com>
2025-03-02 13:37:13 +0700

    g

--- resources/views/frontend/05_pengawasan/01_bujk/01_bujkshow.blade.php
+++ resources/views/frontend/05_pengawasan/01_bujk/01_bujkshow.blade.php
@@ -191,7 +191,7 @@ color: #45a049;
                                         </head>
 
                     <body>
-                    <div class="table-wrapper" style="margin-top: 25px;">
+                    <div class="table-wrapper" style="margin-top: 15px;">
                     <!-- Add this inside your HTML <head> for FontAwesome -->
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
 
