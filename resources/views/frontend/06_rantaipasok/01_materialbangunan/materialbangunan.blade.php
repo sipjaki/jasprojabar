f545667d38ed317a3b604ee45fd08293d98f6dd3
sipjakiblora <sipjakiblora@gmail.com>
2025-03-02 23:46:39 +0700

    g

--- resources/views/frontend/06_rantaipasok/01_materialbangunan/materialbangunan.blade.php
+++ resources/views/frontend/06_rantaipasok/01_materialbangunan/materialbangunan.blade.php
@@ -235,7 +235,7 @@ color: #45a049;
                                             <td style="text-align: center">
                                                 <img src="{{ asset('storage/' . $item->foto) }}" alt="Foto Toko Bangunan" width="100">
                                             </td>
-                                            <td>{{$item->alamat}}</td>
+                                            {{-- <td>{{$item->alamat}}</td> --}}
                                             <td>{{$item->notelepon}}</td>
                                             <td>{{$item->materialproduk}}</td>
                                             {{-- <td>{{$item->submaterialproduk}}</td>
