cd60f8f142bc512f533ad9c75d031114d5a24ccb
sipjakiblora <sipjakiblora@gmail.com>
2025-03-02 14:48:47 +0700

    g

--- resources/views/frontend/05_pengawasan/03_tertibjakon/01_tertibusaha/list.blade.php
+++ resources/views/frontend/05_pengawasan/03_tertibjakon/01_tertibusaha/list.blade.php
@@ -286,7 +286,7 @@ color: #45a049;
                             function searchTable() {
                             let input = document.getElementById("searchInput").value;
 
-                            fetch(`/datajakon/bujkkontraktor?search=${input}`)
+                            fetch(`/tertibjasakonstruksilist?search=${input}`)
                                 .then(response => response.text())
                                 .then(html => {
                                     let parser = new DOMParser();
