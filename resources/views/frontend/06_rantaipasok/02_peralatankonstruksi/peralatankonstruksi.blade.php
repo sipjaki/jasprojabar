12a707b57cc52526a5f694dbc6920ddb4d50d781
sipjakiblora <sipjakiblora@gmail.com>
2025-03-02 21:39:50 +0700

    g

--- resources/views/frontend/06_rantaipasok/02_peralatankonstruksi/peralatankonstruksi.blade.php
+++ resources/views/frontend/06_rantaipasok/02_peralatankonstruksi/peralatankonstruksi.blade.php
@@ -170,11 +170,11 @@ color: #45a049;
                                             <i class="fas fa-search" style="position: absolute; right: 10px; top: 50%; transform: translateY(-50%); font-size: 16px; color: #888;"></i>
                                         </div> --}}
 
-                                        <a href="/datajakon/bujk" style="background: white;">
+                                        {{-- <a href="/peralatankonstruksi" style="background: white;">
                                             <button class="badgekembali" style="border: none; font-size: 12px; cursor: pointer; margin-right: 10px;">
                                                 <i class="fas fa-arrow-circle-left mr-2" style="margin-right: 20px; color:red;"></i><span style="color: black">KEMBALI</span>
                                             </button>
-                                        </a>
+                                        </a> --}}
 
                                     </div>
                                 </div>
