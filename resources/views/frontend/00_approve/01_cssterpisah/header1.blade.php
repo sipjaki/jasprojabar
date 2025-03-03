70ef5ff095c459d17a132e97fb8e0a97c80692b4
sipjakiblora <sipjakiblora@gmail.com>
2025-03-03 10:57:01 +0700

    fdgfdgfd

--- resources/views/frontend/00_approve/01_cssterpisah/header1.blade.php
+++ resources/views/frontend/00_approve/01_cssterpisah/header1.blade.php
@@ -120,9 +120,9 @@
 						<li class="active has-dropdown">
 							<a href="#">KELEMBAGAAN</a>
 							<ul class="list-unstyled">
-								<li><a href="/profil/struktur">Struktur DPUPR Kab Blora</a></li>
-								<li><a href="/profil/rencanastrategis">Profil Jakon DPUPR Blora</a></li>
-								<li><a href="/profil/tupoksi">Tupoksi Jakon DPUPR Kab Blora</a></li>
+								<li><a href="/profil/struktur">Struktur DPUPR</a></li>
+								<li><a href="/profil/rencanastrategis">Profil Jakon</a></li>
+								<li><a href="/profil/tupoksi">Tupoksi Jakon</a></li>
 							</ul><!-- list-unstyled -->
 						</li><!--has-dropdown-->
 
