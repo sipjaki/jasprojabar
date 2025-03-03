7f25df39e5427580d6be239b948ab6d4e6c3ae48
sipjakiblora <sipjakiblora@gmail.com>
2025-03-03 22:40:57 +0700

    g

--- database/migrations/2025_02_21_065853_create_bujkkonsultans_table.php
+++ database/migrations/2025_02_21_065853_create_bujkkonsultans_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
             $table->id();
             $table->foreignId('bujkkonsultansub_id')->nullable();
             // $table->foreignId('asosiasimasjaki_id')->nullable();
-            $table->foreignId('asosiasimasjaki_id')->nullable()->constrained(false);
+            // $table->foreignId('asosiasimasjaki_id')->nullable()->constrained(false);
+            $table->foreignId('asosiasimasjaki_id')->nullable()->constrained()->nullOnDelete();
             $table->string('namalengkap')->nullable();
             $table->text('alamat')->nullable();
             $table->string('no_telepon')->nullable();
