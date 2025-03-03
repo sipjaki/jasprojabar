6ff3f6a7dc805d2aaadbbd7b3ec93f0246bb8908
sipjakiblora <sipjakiblora@gmail.com>
2025-03-04 00:48:52 +0700

    g

--- resources/views/frontend/03_masjaki_jakon/02_bujkkonsultan/bujkkonsultan.blade.php
+++ resources/views/frontend/03_masjaki_jakon/02_bujkkonsultan/bujkkonsultan.blade.php
@@ -187,7 +187,7 @@ color: #45a049;
                         <body>
                             <div class="table-wrapper" style="margin-top:-130px; position: relative;">
                                 <div style="display: flex; justify-content: space-between; margin-bottom: 10px; align-items: center;">
-                                    <div class="pagination-info-box" style="margin-right:300px; padding: 10px; border: 1px solid black; background-color: #f9f9f9; border-radius: 5px;">
+                                    <div class="pagination-info-box" style="margin-right:250px; padding: 10px; border: 1px solid black; background-color: #f9f9f9; border-radius: 5px;">
                                         <div class="pagination-info" style="color: black; font-weight: 500; font-size:14px;">
                                             Data Ke {{ $data->firstItem() }} Sampai {{ $data->lastItem() }} Dari {{ $data->total() }} Jumlah {{$title}}
                                         </div>
