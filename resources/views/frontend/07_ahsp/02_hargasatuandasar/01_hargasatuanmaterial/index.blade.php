fd8916151e63b32a914382a062ad7876961e49cb
sipjakiblora <sipjakiblora@gmail.com>
2025-03-04 01:34:46 +0700

    dfg

--- resources/views/frontend/07_ahsp/02_hargasatuandasar/01_hargasatuanmaterial/index.blade.php
+++ resources/views/frontend/07_ahsp/02_hargasatuandasar/01_hargasatuanmaterial/index.blade.php
@@ -218,6 +218,7 @@ color: #45a049;
                                             <th onclick="sortTable(2)" style="cursor:pointer; text-align:center; width:100px;"> Satuan <span class="sort-icon">⇅</span></th>
                                             <th onclick="sortTable(3)" style="cursor:pointer; text-align:center; width:50px;"> Rp. <span class="sort-icon">⇅</span></th>
                                             <th onclick="sortTable(4)" style="cursor:pointer; text-align:center; width:200px;"> Besaran <span class="sort-icon">⇅</span></th>
+                                            <th onclick="sortTable(5)" style="cursor:pointer; text-align:center; width:200px;"> Keterangan <span class="sort-icon">⇅</span></th>
                                             {{-- <th style="text-align:center"> View </th> --}}
                                         </tr>
                                     </thead>
