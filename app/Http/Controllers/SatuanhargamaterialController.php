292fa471c591c3523b97e13702fca0c94dd43619
sipjakiblora <sipjakiblora@gmail.com>
2025-03-04 01:28:24 +0700

    g

--- app/Http/Controllers/SatuanhargamaterialController.php
+++ app/Http/Controllers/SatuanhargamaterialController.php
@@ -14,18 +14,49 @@ use Illuminate\Support\Facades\DB;
 
 class SatuanhargamaterialController extends Controller
 {
-    //
-    public function index()
+    // //
+    // public function index()
+    // {
+    //     $user = Auth::user();
+    //     $data = satuanhargamaterial::paginate();
+
+    //     return view('frontend.07_ahsp.02_hargasatuandasar.01_hargasatuanmaterial.index', [
+    //         'title' => 'Satuan Harga Dasar',
+    //         'user' => $user, // Mengirimkan data paginasi ke view
+    //         // 'data' => $data, // Mengirimkan data paginasi ke view
+    //     ]);
+    // }
+
+    public function index(Request $request)
     {
-        $user = Auth::user();
-        // $data = satuanhargamaterial::all();
+        $perPage = $request->input('perPage', 10);
+        $search = $request->input('search');
+
+        $query = satuanhargamaterial::query();
+
+        if ($search) {
+            $query->where('uraian', 'LIKE', "%{$search}%")
+                  ->orWhere('satuan', 'LIKE', "%{$search}%")
+                  ->orWhere('besaran', 'LIKE', "%{$search}%")
+                  ->orWhere('keterangan', 'LIKE', "%{$search}%");
+        }
+
+        $data = $query->paginate($perPage);
+
+        if ($request->ajax()) {
+            return response()->json([
+                'html' => view('frontend.07_ahsp.02_hargasatuandasar.01_hargasatuanmaterial.partials.table', compact('data'))->render()
+            ]);
+        }
 
         return view('frontend.07_ahsp.02_hargasatuandasar.01_hargasatuanmaterial.index', [
-            'title' => 'Satuan Harga Dasar',
-            'user' => $user, // Mengirimkan data paginasi ke view
-            // 'data' => $data, // Mengirimkan data paginasi ke view
+            'title' => 'Satuan Harga Dasar Material',
+            'data' => $data,
+            'perPage' => $perPage,
+            'search' => $search
         ]);
     }
+
 
 
 }
