86c12062fdab3e7b81144ce0f25f91d15af6d8b0
sipjakiblora <sipjakiblora@gmail.com>
2025-03-03 13:13:35 +0700

    g

--- resources/views/frontend/00_approve/01_cssterpisah/header1.blade.php
+++ resources/views/frontend/00_approve/01_cssterpisah/header1.blade.php
@@ -192,14 +192,6 @@
 							</ul><!-- list-unstyled -->
 						</li><!--has-dropdown-->
 
-                        <li class="active has-dropdown">
-							<a href="#">RANTAI PASOK</a>
-							<ul class="list-unstyled">
-								<li><a href="/rantaipasokmaterial">Material Bangunan Gedung </a></li>
-								<li><a href="/peralatankonstruksi">Peralatan Konstruksi</a></li>
-								<li><a href="/tokobangunanblora">Toko Bangunan Kab Blora</a></li>
-							</ul><!-- list-unstyled -->
-						</li><!--has-dropdown-->
 
                         <li class="active has-dropdown">
 							<a href="#">AHSP</a>
@@ -235,6 +227,16 @@
 								{{-- <li><a href="/hspkonstruksiumum">HSP Konstruksi Umum </a></li>
 								<li><a href="/shstblora">SHST Konstruksi</a></li> --}}
 								{{-- <li><a href="/404">Daftar Material</a></li> --}}
+							</ul><!-- list-unstyled -->
+						</li><!--has-dropdown-->
+
+
+                        <li class="active has-dropdown">
+							<a href="#">RANTAI PASOK</a>
+							<ul class="list-unstyled">
+								<li><a href="/rantaipasokmaterial">Material Bangunan Gedung </a></li>
+								<li><a href="/peralatankonstruksi">Peralatan Konstruksi</a></li>
+								<li><a href="/tokobangunanblora">Toko Bangunan Kab Blora</a></li>
 							</ul><!-- list-unstyled -->
 						</li><!--has-dropdown-->
 
