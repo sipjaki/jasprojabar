928536a14dc1f7903053a21009afa86b66ca659f
sipjakiblora <sipjakiblora@gmail.com>
2025-03-05 15:52:11 +0700

    fd

--- resources/views/frontend/00_approve/00_utama/index.blade.php
+++ resources/views/frontend/00_approve/00_utama/index.blade.php
@@ -83,7 +83,7 @@
 						<div class="event-card">
 							<div class="event-card-image">
 								<div class="event-card-image-inner">
-									<a href="event-details.html"><img src="/assets/00_dokmasjaki/03_datajakon/tokobangunanblora.jpg" class="img-fluid" alt="img-21" width="5%"></a>
+									<a href="event-details.html"><img src="/assets/icon/.jpg" class="img-fluid" alt="img-21"></a>
 									<div class="event-card-meta">
 										<div class="event-meta-number">
 											<span>-</span>
