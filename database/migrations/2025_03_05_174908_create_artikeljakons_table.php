fdc27df337accaeb37e372d0e8af89b74ab0310e
sipjakiblora <sipjakiblora@gmail.com>
2025-03-06 00:54:04 +0700

    g

--- database/migrations/2025_03_05_174908_create_artikeljakons_table.php
+++ database/migrations/2025_03_05_174908_create_artikeljakons_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('artikeljakons', function (Blueprint $table) {
             $table->id();
+            $table->string('judul')->nullable();
+            $table->string('berkas')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
