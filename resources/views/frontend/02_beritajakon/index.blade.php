4200b7e7513029cb3e56db2ec02219cada837ff9
sipjakiblora <sipjakiblora@gmail.com>
2025-03-05 23:39:12 +0700

    g

--- resources/views/frontend/02_beritajakon/index.blade.php
+++ resources/views/frontend/02_beritajakon/index.blade.php
@@ -15,7 +15,18 @@
 		</div><!-- container -->
 	</section><!--page-banner-->
 	<section class="news-details-section">
-		<div class="container">
+		<div class="container"
+        style="
+        background: white;
+        max-width: 95%;
+        margin: 30px auto;
+        padding: 20px;
+        height: auto;
+        border-radius: 20px;
+        box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+        position: relative;
+        z-index: 10;"
+        >
 			<div class="row">
 				<div class="col-lg-8">
 					<div class="news-details-box-image">
