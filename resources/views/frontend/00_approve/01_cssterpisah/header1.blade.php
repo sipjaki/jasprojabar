4ed4709927a2e3897ac0e57d0b893a1ed72cd9f3
sipjakiblora <sipjakiblora@gmail.com>
2025-03-05 11:33:23 +0700

    df

--- resources/views/frontend/00_approve/01_cssterpisah/header1.blade.php
+++ resources/views/frontend/00_approve/01_cssterpisah/header1.blade.php
@@ -105,6 +105,14 @@
 		</div><!-- topbar-inner -->
 	</div><!--topbar-->
 	<div class="main-menu sticky-header" style="background: black">
+        <div style="display: flex; align-items: center; ">
+            {{-- <img src="/assets/icon/pupr.png" alt="Logo SIPJAKIKBB" style="width: 50px; height: 50px; object-fit: cover; padding: 0;"> --}}
+            <img src="/assets/icon/logokabupatenblora.png" alt="Logo SIPJAKIKBB" style="width: 50px; height: 50px; object-fit: cover; padding: 0;">
+            <h1 style="margin-left: 5px;">
+                <span>MASJAKI</span>BLORA
+                <p style="margin: 0; font-size: 16px; color: white" >Dinas Pekerjaan Umum & Penataan Ruang</p>
+            </h1>
+        </div>
 		<div class="main-menu-inner">
 			<div class="main-menu-left">
 				<div class="main-menu-logo">
