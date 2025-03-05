ea1b03e9b27ad74a29eaf614d5017c737f56a3c2
sipjakiblora <sipjakiblora@gmail.com>
2025-03-05 13:58:26 +0700

    g

--- resources/views/frontend/00_approve/01_cssterpisah/footer1.blade.php
+++ resources/views/frontend/00_approve/01_cssterpisah/footer1.blade.php
@@ -17,7 +17,7 @@
 					</div><!-- footer-widget-text -->
 					<div class="footer-widget-text">
               			<p style="text-align: justify;">
-                            Mas Jaki DPUPR Blora adalah **Sistem Informasi Pembina Jasa Konstruksi** Kabupaten Blora yang mendigitalisasi pendataan **Sertifikat Kompetensi Kerja (SKK) tenaga kerja konstruksi**, serta pengawasan **Badan Usaha Jasa Konstruksi (BUJK) Konstruksi dan Konsultasi**. Sistem ini memastikan kepatuhan regulasi, memantau progres proyek secara real-time, dan menilai kinerja kontraktor serta tenaga ahli. Selain itu, platform ini memfasilitasi komunikasi antar pemangku kepentingan untuk menciptakan ekosistem konstruksi yang profesional, transparan, dan berkelanjutan.
+                            Mas Jaki DPUPR Blora adalah Sistem Informasi Pembina Jasa Konstruksi Kabupaten Blora yang mendigitalisasi pendataan SKK Tenaga Kerja Konstruksi, serta pengawasan Badan Usaha Jasa Konstruksi (BUJK) Konstruksi dan Konsultasi. Sistem ini memastikan kepatuhan regulasi, memantau progres proyek secara real-time, dan menilai kinerja kontraktor serta tenaga ahli. Selain itu, platform ini memfasilitasi komunikasi antar pemangku kepentingan untuk menciptakan ekosistem konstruksi yang profesional, transparan, dan berkelanjutan.
                         </p>
                     </div><!-- footer-widget-text -->
 
