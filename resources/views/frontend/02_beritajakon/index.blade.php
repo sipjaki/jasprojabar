de21348bf66e2f36825fdb91b8de38d06a682bfe
sipjakiblora <sipjakiblora@gmail.com>
2025-03-05 23:46:25 +0700

    g

--- resources/views/frontend/02_beritajakon/index.blade.php
+++ resources/views/frontend/02_beritajakon/index.blade.php
@@ -48,7 +48,7 @@
 						</div><!-- news-details-meta-box-inner -->
 					</div><!-- news-details-meta-box --> --}}
 
-                    <div class="news-details-content-box" style="margin-left: 100px;">
+                    <div class="news-details-content-box" style="margin-left: 25px;">
 						<h4>Supporting local business to bounce back</h4>
 						<p>Tliq is notm hendr erit a augue insu image pellen tes que id erat quis sollicitud. Lorem ipsum dolor sit amet, consectetur adipiscing ullam blandit hendrerit faucibus suspendisse. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. </p>
 						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum ley of type and scrambled it to make a type specimen book.</p>
