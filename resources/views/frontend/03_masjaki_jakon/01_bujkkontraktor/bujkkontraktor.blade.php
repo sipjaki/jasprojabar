018008c1255a187b70ff533c445137123bf466ad
sipjakiblora <sipjakiblora@gmail.com>
2025-03-06 03:18:40 +0700

    g

--- resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/bujkkontraktor.blade.php
+++ resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/bujkkontraktor.blade.php
@@ -177,7 +177,7 @@ color: #45a049;
                                       <a href="#" style="background: white;">
                                           <button class="badgekembali" style="border: none; font-size: 12px; cursor: pointer;">
                                             <img src="/assets/icon/logokabupatenblora.png" class="img-fluid" alt="img-25" width="50" height="50" style="margin-right: 20px;" loading="lazy">
-                                            <i class="fas fa-edit mr-2" style="margin-right: 10px; color:#ffd100;"></i> <span style="text-transform: uppercase; color:black;">{{$title}}</span>
+                                            <i class="fas fa-edit mr-2" style="margin-right: 10px; color:#ffd100;"></i> <span style="text-transform: uppercase; color:black; font-size:20px;">{{$title}}</span>
                                         </button>
                                     </a>
 
@@ -207,7 +207,7 @@ color: #45a049;
                             <div class="table-wrapper" style="margin-top:-130px; position: relative;">
                                 <div style="display: flex; justify-content: space-between; margin-bottom: 10px; align-items: center;">
                                     <div class="pagination-info-box" style="margin-right:300px; padding: 10px; border: 1px solid black; background-color: #f9f9f9; border-radius: 5px;">
-                                        <div class="pagination-info" style="color: black; font-weight: 500; font-size:14px;">
+                                        <div class="pagination-info" style="color: black; font-weight: 500; font-size:20px;">
                                             Data Ke {{ $data->firstItem() }} Sampai {{ $data->lastItem() }} Dari {{ $data->total() }} Jumlah {{$title}}
                                         </div>
                                     </div>
