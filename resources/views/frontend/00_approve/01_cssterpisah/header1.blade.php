4066320cab018d78dc5c30308c9de2b4a9f3912b
sipjakiblora <sipjakiblora@gmail.com>
2025-03-05 12:27:03 +0700

    g

--- resources/views/frontend/00_approve/01_cssterpisah/header1.blade.php
+++ resources/views/frontend/00_approve/01_cssterpisah/header1.blade.php
@@ -4,14 +4,14 @@
 	<div class="topbar" style="background: #2ECC71;>
 		<div class="topbar-inner" style="background: #2ECC71 " >
 			<div class="topbar-left" style="background: #2ECC71 ">
-				<div class="topbar-socials" style="height: 10vh;">
+				<div class="topbar-socials" style="height: 5vh;">
                     <a href="#"><i class="fa-brands fa-tiktok" style="font-size: 28px;"></i></a>
                     {{-- <a href="#"><i class="fa-solid fa-envelope" style="font-size: 25px;"></i></a> --}}
                     <a href="#"><i class="fa-brands fa-instagram" style="font-size: 28px;"></i></a>
                     <a href="#"><i class="fa-brands fa-whatsapp" style="font-size: 28px;"></i></a>
                     <a href="#"><i class="fa-brands fa-facebook" style="font-size: 28px;"></i></a>
 				</div><!--topbar-socials-->
-				<div class="topbar-info" style="height: 10vh;">
+				<div class="topbar-info" style="height: 5vh;">
 					<ul>
 						<li>
 							{{-- <div class="topbar-a">
