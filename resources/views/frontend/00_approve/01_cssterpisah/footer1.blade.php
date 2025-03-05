d71f29f80ef4f97997915cfe712bc876ae7497bc
sipjakiblora <sipjakiblora@gmail.com>
2025-03-05 14:12:01 +0700

    g

--- resources/views/frontend/00_approve/01_cssterpisah/footer1.blade.php
+++ resources/views/frontend/00_approve/01_cssterpisah/footer1.blade.php
@@ -22,10 +22,13 @@
                     </div><!-- footer-widget-text -->
 
                     <div class="footer-widget-socials">
-						<a href="/404"><i class="fa-brands fa-twitter"></i></a>
-						<a href="/404"><i class="fa-brands fa-facebook"></i></a>
-						{{-- <a href="/404"><i class="fa-brands fa-pinterest-p"></i></a> --}}
-						<a href="/404"><i class="fa-brands fa-instagram"></i></a>
+                        <li><a href="#"><i class="fa-solid fa-envelope"></i></a></li>
+                        <li><a href="#"><i class="fa-brands fa-tiktok"></i></a></li>
+                        <li><a href="https://wa.me/6281321455855"><i class="fa-brands fa-whatsapp"></i></a></li>
+                        <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+                        <li><a href="#"><i class="fa-brands fa-youtube"></i></a></li>
+                        <li><a href="#"><i class="fa-solid fa-phone"></i></a></li>
 					</div><!-- footer-widget-socials -->
 				</div><!--col-lg-4-->
 				<div class="col-lg-3">
