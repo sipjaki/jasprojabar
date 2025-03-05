225a0da67b68743c8be98e4da8893681a13d4858
sipjakiblora <sipjakiblora@gmail.com>
2025-03-06 03:16:01 +0700

    g

--- resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/bujkkontraktor.blade.php
+++ resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/bujkkontraktor.blade.php
@@ -253,7 +253,7 @@ color: #45a049;
                                             <td style="text-align: center;">{{ $loop->iteration + $start - 1 }}</td>
                                             <td style="text-transform: capitalize;">{{ ucwords(strtolower($item->namalengkap)) }}</td>
                                             <td>{{$item->alamat}}</td>
-                                            <td>{{$item->no_telepon}}</td>
+                                            <td style="text-align: center;">{{$item->no_telepon}}</td>
                                             {{-- <td>{{$item->email}}</td>
                                             <td>{{$item->nib}}</td>
                                             <td>{{$item->pju}}</td>
