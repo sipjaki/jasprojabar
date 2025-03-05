00d2b3e8bab5855e6bb7fd5028bced238d261f84
sipjakiblora <sipjakiblora@gmail.com>
2025-03-05 20:13:07 +0700

    dfg

--- resources/views/frontend/00_approve/01_cssterpisah/footer1.blade.php
+++ resources/views/frontend/00_approve/01_cssterpisah/footer1.blade.php
@@ -31,27 +31,6 @@
                         <a href="tel:+621234567890"><i class="fa-solid fa-phone"></i></a>
 					</div><!-- footer-widget-socials -->
 
-                    <div class="footer-widget" style="margin-top: -0px;">
-						<div class="footer-widget-contact">
-							<h4 class="footer-widget-title">
-                                <i class="fas fa-phone-alt" style="margin-right: 5px;"></i> Kontak Kami
-                            </h4>
-                            <p>Jl. Nusantara No.62, Jetis, Kauman, Kec. Blora<br>Kabupaten Blora 58214 Provinsi Jawa Tengah </p>
-						</div><!-- footer-widget-contact -->
-						<div class="footer-widget-contact-list">
-							<i class="fa-solid fa-envelope"></i>
-							<div class="footer-widget-contact-item">
-								<a href="mailto:needhelp@company.com">kontraktor@masjakidpuprblora.co.id</a>
-							</div><!-- footer-widget-contact-item -->
-						</div><!-- footer-widget-contact-list -->
-						{{-- <div class="footer-widget-contact-list">
-							<i class="fa-solid fa-phone"></i>
-							<div class="footer-widget-contact-item">
-								<a href="tel:+92-003-68-090">0813 2145 5855</a>
-							</div><!-- footer-widget-contact-item -->
-						</div><!-- footer-widget-contact-list --> --}}
-					</div><!--footer-widget-->
-
 
 				</div><!--col-lg-4-->
 				<div class="col-lg-3">
@@ -70,7 +49,7 @@
 								<li><a href="/">Badan Usaha Jasa Konsultasi Konstruksi</a></li>
 								<li><a href="/">Tenaga Kerja Konstruksi DPUPR</a></li>
 								<li><a href="/">Tenaga Kerja Kab Blora</a></li>
-								<li><a href="/">Profil Paket Pekerjaan</a></li>
+								{{-- <li><a href="/">Profil Paket Pekerjaan</a></li> --}}
 								{{-- <li><a href="/">Agenda Pelatihan</a></li>
 								<li><a href="/">Tertib Jasa Konstruksi</a></li>
 								<li><a href="/">Kecelakaan Kerja</a></li> --}}
@@ -102,32 +81,15 @@
 							</ul><!-- list-unstyled -->
 						</div><!-- footer-widget-department -->
 
-                        <div class="footer-widget-explore" style="margin-top: 50px;">
-                            <div class="footer-widget-contact">
-                                <h4 class="footer-widget-title">
-                                    <i class="fas fa-map-marker-alt" style="margin-right: 5px;"></i> Lokasi Kami
-                                </h4>
 
-                                {{-- <p>Jl. Merdeka No.10, Kecamatan Pusat, Kota Contoh<br>Provinsi Jawa Barat 12345</p> --}}
-                                <iframe
-                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3958.523764685145!2d111.4188524747965!3d-7.179069792829058!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7712b6c5e5f9d7%3A0x98b939fa9f2c6b88!2sJl.%20Nusantara%20No.62%2C%20Jetis%2C%20Kauman%2C%20Kec.%20Blora%2C%20Kabupaten%20Blora%2C%20Jawa%20Tengah%2058214!5e0!3m2!1sen!2sid!4v1710000000000"
-                                    width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy"
-                                    referrerpolicy="no-referrer-when-downgrade">
-                                </iframe>
-                            </div><!-- footer-widget-contact -->
-                        </div><!--footer-widget-->
 
 					</div><!--footer-widget-->
 				</div><!--col-lg-3-->
 
 				<div class="col-lg-3">
 					<div class="footer-widget">
+                        <div class="footer-widget-department">
 
-					</div><!--footer-widget-->
-				</div><!--col-lg-2-->
-				<div class="col-lg-2">
-					<div class="footer-widget">
-						<div class="footer-widget-department">
                             <h4 class="footer-widget-title">
                                 <i class="fas fa-book" style="margin-right: 5px;"></i> AHSP Kab Blora 2025
                             </h4>
@@ -147,6 +109,47 @@
 								<li><a href="/404">HSP Divisi VIII Pekerjaan Jaringan Pipa</a></li>
 							</ul><!-- list-unstyled -->
 						</div><!-- footer-widget-department -->
+					</div><!--footer-widget-->
+				</div><!--col-lg-2-->
+				<div class="col-lg-2">
+					<div class="footer-widget">
+
+                    <div class="footer-widget" style="margin-top: -0px;">
+						<div class="footer-widget-contact">
+							<h4 class="footer-widget-title">
+                                <i class="fas fa-phone-alt" style="margin-right: 5px;"></i> Kontak Kami
+                            </h4>
+                            <p>Jl. Nusantara No.62, Jetis, Kauman, Kec. Blora<br>Kabupaten Blora 58214 Provinsi Jawa Tengah </p>
+						</div><!-- footer-widget-contact -->
+						<div class="footer-widget-contact-list">
+							<i class="fa-solid fa-envelope"></i>
+							<div class="footer-widget-contact-item">
+								<a href="mailto:needhelp@company.com">kontraktor@masjakidpuprblora.co.id</a>
+							</div><!-- footer-widget-contact-item -->
+						</div><!-- footer-widget-contact-list -->
+						{{-- <div class="footer-widget-contact-list">
+							<i class="fa-solid fa-phone"></i>
+							<div class="footer-widget-contact-item">
+								<a href="tel:+92-003-68-090">0813 2145 5855</a>
+							</div><!-- footer-widget-contact-item -->
+						</div><!-- footer-widget-contact-list --> --}}
+					</div><!--footer-widget-->
+
+
+                        <div class="footer-widget-explore" style="margin-top: 50px;">
+                            <div class="footer-widget-contact">
+                                <h4 class="footer-widget-title">
+                                    <i class="fas fa-map-marker-alt" style="margin-right: 5px;"></i> Lokasi Kami
+                                </h4>
+
+                                {{-- <p>Jl. Merdeka No.10, Kecamatan Pusat, Kota Contoh<br>Provinsi Jawa Barat 12345</p> --}}
+                                <iframe
+                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3958.523764685145!2d111.4188524747965!3d-7.179069792829058!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7712b6c5e5f9d7%3A0x98b939fa9f2c6b88!2sJl.%20Nusantara%20No.62%2C%20Jetis%2C%20Kauman%2C%20Kec.%20Blora%2C%20Kabupaten%20Blora%2C%20Jawa%20Tengah%2058214!5e0!3m2!1sen!2sid!4v1710000000000"
+                                    width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy"
+                                    referrerpolicy="no-referrer-when-downgrade">
+                                </iframe>
+                            </div><!-- footer-widget-contact -->
+                        </div><!--footer-widget-->
 					</div><!--footer-widget-->
 
 				</div><!--col-lg-2-->
