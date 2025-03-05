782508f7627fd6d824bfef166b74e7e6b354fea8
sipjakiblora <sipjakiblora@gmail.com>
2025-03-05 13:52:09 +0700

    g

--- resources/views/frontend/00_approve/01_cssterpisah/footer1.blade.php
+++ resources/views/frontend/00_approve/01_cssterpisah/footer1.blade.php
@@ -15,14 +15,11 @@
                         </h1>
 						<p>Dinas Pekerjaan Umum dan Penataan Ruang Kabupaten Blora</p>
 					</div><!-- footer-widget-text -->
-					<div>
-                        <div style="font-family: 'Poppins', sans-serif; color: white; text-align: justify; padding: 20px; border-radius: 10px;">
-                            <h5>
-                                Mas Jaki DPUPR Blora adalah Sistem Informasi Pembina Jasa Konstruksi Kabupaten Blora Provinsi Jawa Tengah, sebuah platform digital untuk meningkatkan efisiensi dan transparansi dalam pendataan jasa konstruksi. Sistem ini memantau pelaksanaan proyek paket pekerjaan, memastikan kepatuhan terhadap standar, dan memfasilitasi komunikasi antara pemerintah, kontraktor, pengawas, tenaga ahli, dan masyarakat. Hal ini diharapkan dapat meningkatkan kualitas infrastruktur dan mempercepat proses administrasi.
-                            </h5>
-                        </div>
-
-                    </div><!-- footer-widget-socials -->
+					<div class="footer-widget-text">
+              			<p>
+                            Mas Jaki DPUPR Blora adalah Sistem Informasi Pembina Jasa Konstruksi Kabupaten Blora Provinsi Jawa Tengah, sebuah platform digital untuk meningkatkan efisiensi dan transparansi dalam pendataan jasa konstruksi. Sistem ini memantau pelaksanaan proyek paket pekerjaan, memastikan kepatuhan terhadap standar, dan memfasilitasi komunikasi antara pemerintah, kontraktor, pengawas, tenaga ahli, dan masyarakat. Hal ini diharapkan dapat meningkatkan kualitas infrastruktur dan mempercepat proses administrasi.
+                        </p>
+                    </div><!-- footer-widget-text -->
 
                     <div class="footer-widget-socials">
 						<a href="/404"><i class="fa-brands fa-twitter"></i></a>
