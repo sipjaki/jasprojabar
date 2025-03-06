c2677d5f8adaafe0ef56eb20a42642d2005abda3
sipjakiblora <sipjakiblora@gmail.com>
2025-03-06 23:09:40 +0700

    g

--- resources/views/frontend/00_full/01_kritikdansaran/index.blade.php
+++ resources/views/frontend/00_full/01_kritikdansaran/index.blade.php
@@ -78,7 +78,7 @@
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3952.628066692034!2d111.4085298!3d-6.977182!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e77411bc797c1ed%3A0x5ddba5997190e35c!2sDinas%20Pekerjaan%20Umum%20dan%20Penataan%20Ruang%20(DPUPR)%20Kabupaten%20Blora!5e0!3m2!1sen!2sid!4v1710156789012"
                     width="100%"
-                    height="400"
+                    height="200"
                     style="border:0;"
                     allowfullscreen=""
                     loading="lazy"
@@ -97,8 +97,8 @@
     position: absolute;
     top: 0;
     left: 0;
-    width: 80%;
-    height: 80%;
+    width: 100%;
+    height: 100%;
     border: 0;
 }
 
