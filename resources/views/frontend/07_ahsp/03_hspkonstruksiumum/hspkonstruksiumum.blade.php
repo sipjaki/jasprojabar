a054074428031d2f3729e5c909ead79721b52b3d
sipjakiblora <sipjakiblora@gmail.com>
2025-03-06 13:22:05 +0700

    df

--- resources/views/frontend/07_ahsp/03_hspkonstruksiumum/hspkonstruksiumum.blade.php
+++ resources/views/frontend/07_ahsp/03_hspkonstruksiumum/hspkonstruksiumum.blade.php
@@ -245,7 +245,7 @@ color: #45a049;
                                     <thead>
                                         <tr>
                                             <th onclick="sortTable(0)" style="cursor:pointer; text-align:center; width:100px;"> No <span class="sort-icon">⇅</span></th>
-                                            <th onclick="sortTable(1)" style="cursor:pointer; text-align:center; width:100px;"> Kode AHSP<span class="sort-icon">⇅</span></th>
+                                            <th onclick="sortTable(1)" style="cursor:pointer; text-align:center; width:300px;"> Kode AHSP<span class="sort-icon">⇅</span></th>
                                             {{-- <th onclick="sortTable(2)" style="cursor:pointer; text-align:center; width:300px;"> Divisi <span class="sort-icon">⇅</span></th> --}}
                                             {{-- <th onclick="sortTable(3)" style="cursor:pointer; text-align:center; width:300px;"> Paket <span class="sort-icon">⇅</span></th> --}}
                                             <th onclick="sortTable(4)" style="cursor:pointer; text-align:center; width:300px;"> Kode Paket <span class="sort-icon">⇅</span></th>
