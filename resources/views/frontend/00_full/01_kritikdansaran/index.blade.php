2dc150d2e8ceb1c3853233bcf4131f84a84a5f85
sipjakiblora <sipjakiblora@gmail.com>
2025-03-06 23:12:22 +0700

    dfg

--- resources/views/frontend/00_full/01_kritikdansaran/index.blade.php
+++ resources/views/frontend/00_full/01_kritikdansaran/index.blade.php
@@ -140,10 +140,13 @@
 					<div class="col-12 col-lg-6 col-xl-2">
 						<div class="cta-four-content">
 							<div class="cta-four-widget-socials">
-								<a href="#"><i class="fa-brands fa-twitter"></i></a>
-								<a href="#"><i class="fa-brands fa-facebook"></i></a>
-								<a href="#"><i class="fa-brands fa-pinterest-p"></i></a>
-								<a href="#"><i class="fa-brands fa-instagram"></i></a>
+                                <li><a href="#"><i class="fa-solid fa-envelope"></i></a></li>
+                                <li><a href="#"><i class="fa-brands fa-tiktok"></i></a></li>
+                                <li><a href="https://wa.me/6281321455855"><i class="fa-brands fa-whatsapp"></i></a></li>
+                                <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+                                <li><a href="#"><i class="fa-brands fa-youtube"></i></a></li>
+                                <li><a href="#"><i class="fa-solid fa-phone"></i></a></li>
 							</div><!-- cta-four-widget-socials -->
 						</div><!-- cta-four-content -->
 					</div><!-- col-12 col-lg-6 col-xl-2 -->
