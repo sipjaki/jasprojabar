84545dbaf8586559e70c5e5a763bf6b75252e1c8
sipjakiblora <sipjakiblora@gmail.com>
2025-03-07 13:06:57 +0700

    fd

--- resources/views/frontend/04_pembinaan/02_skktenagakerja/show.blade.php
+++ resources/views/frontend/04_pembinaan/02_skktenagakerja/show.blade.php
@@ -384,9 +384,10 @@
 
     </table>
 
-    <button id="downloadPDF">Download PDF</button>
 
 </div>
+
+<button id="downloadPDF">Download PDF</button>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
@@ -395,24 +396,27 @@
     document.getElementById("downloadPDF").addEventListener("click", function () {
         const { jsPDF } = window.jspdf;
         const pdf = new jsPDF("p", "mm", "a4");
-        const element = document.querySelector(".container-surat"); // Sesuaikan dengan elemen yang ingin didownload
+        const element = document.querySelector(".container-surat"); // Target elemen yang akan dikonversi
+
+        const margin = 20; // 2 cm (20 mm)
+        const pdfWidth = 210 - 2 * margin; // Lebar A4 dikurangi margin kiri & kanan
+        const pdfHeight = 297 - 2 * margin; // Tinggi A4 dikurangi margin atas & bawah
 
         html2canvas(element, { scale: 2 }).then((canvas) => {
             const imgData = canvas.toDataURL("image/png");
-            const imgWidth = 210; // A4 width in mm
-            const pageHeight = 297; // A4 height in mm
+            const imgWidth = pdfWidth;
             const imgHeight = (canvas.height * imgWidth) / canvas.width;
             let heightLeft = imgHeight;
-            let position = 0;
+            let position = margin;
 
-            pdf.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
-            heightLeft -= pageHeight;
+            pdf.addImage(imgData, "PNG", margin, position, imgWidth, imgHeight);
+            heightLeft -= pdfHeight;
 
             while (heightLeft > 0) {
-                position -= pageHeight;
+                position = margin - heightLeft;
                 pdf.addPage();
-                pdf.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
-                heightLeft -= pageHeight;
+                pdf.addImage(imgData, "PNG", margin, position, imgWidth, imgHeight);
+                heightLeft -= pdfHeight;
             }
 
             pdf.save("Dokumen_Peserta.pdf");
