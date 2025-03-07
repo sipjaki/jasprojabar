08e5c9a615c79ae4586810792ba60f348c7ce820
sipjakiblora <sipjakiblora@gmail.com>
2025-03-07 13:17:48 +0700

    dsf

--- resources/views/frontend/00_approve/01_cssterpisah/footer1.blade.php
+++ resources/views/frontend/00_approve/01_cssterpisah/footer1.blade.php
@@ -1,5 +1,5 @@
 
-<section class="footer">
+<section class="footer" style="margin-bottom: -100px;">
 	<div class="footer-inner">
 		{{-- <div class="container" style="max-width: calc(100% - 300px); margin: 0 auto;"> --}}
 		<div class="container" style="
