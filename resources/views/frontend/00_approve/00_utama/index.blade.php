a317465bb68345cb4368d1058c690520677e1e07
sipjakiblora <sipjakiblora@gmail.com>
2025-03-08 02:04:18 +0700

    g

--- resources/views/frontend/00_approve/00_utama/index.blade.php
+++ resources/views/frontend/00_approve/00_utama/index.blade.php
@@ -102,18 +102,7 @@
 
 
     <section class="blog-section" style="background-image: url(assets/00_dokmasjaki/03_datajakon/bannerbetter.jpg);">
-        <div class="container"
-        style="
-        background: white;
-        max-width: 95%;
-        margin: 30px auto;
-        padding: 20px;
-        height: auto;
-        border-radius: 20px;
-        box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
-        position: relative;
-        z-index: 10;"
-        >
+        <div class="container">
             <div class="blog-box">
                 <div class="section-title-box text-center" style="margin-top: 20px;">
                     <h2 class="section-title" style="font-size: 32px;">Berita Jasa Konstruksi</h2>
