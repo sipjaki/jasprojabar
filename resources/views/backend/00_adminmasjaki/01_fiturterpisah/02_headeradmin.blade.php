55d71036795630c49bf87d14b9fbe0135e4c3917
abgblora <abgblorabangunangedung@gmail.com>
2025-03-10 01:12:35 +0700

    g

--- resources/views/backend/00_adminmasjaki/01_fiturterpisah/02_headeradmin.blade.php
+++ resources/views/backend/00_adminmasjaki/01_fiturterpisah/02_headeradmin.blade.php
@@ -15,8 +15,6 @@
 
       <title>{{$title}}</title>
 
-
-
       <!-- Canonical SEO -->
       <meta name="description" content="Sneat is the best bootstrap 5 dashboard for responsive web apps. Streamline your app development process with ease." />
 
@@ -54,7 +52,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
 
-    <link rel="stylesheet" href="../../assets/00_adminmasjaki/vendor/fonts/iconify-icons.css" />
+    {{-- <link rel="stylesheet" href="../../assets/00_adminmasjaki/vendor/fonts/iconify-icons.css" /> --}}
 
     <!-- Core CSS -->
     <!-- build:css assets/vendor/css/theme.css  -->
@@ -78,6 +76,7 @@
   <link rel="stylesheet" href="../../assets/00_adminmasjaki/vendor/libs/apex-charts/apex-charts.css" />
 
     <!-- Page CSS -->
+    <link rel="icon" href="/assets/icon/logokabupatenblora.png" type="image/x-icon">
 
 
     <!-- Helpers -->
