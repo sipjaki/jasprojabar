8d41b2e10af45418f900a5edf90a804da938e2db
abgblora <abgblorabangunangedung@gmail.com>
2025-03-09 21:09:44 +0700

    g

--- resources/views/backend/00_dashboard/part/menufooter.blade.php
+++ resources/views/backend/00_dashboard/part/menufooter.blade.php
@@ -6,7 +6,7 @@
                     <span style="font-size: 16px;">&copy; Dinas Pekerjaan Umum Dan Penataan Ruang Kabupaten Blora Provinsi Jawa Tengah 2025 </span>
                 </div>
                 <div class="col-sm-4 text-right">
-                    <a href="https://wa.me/081321455855" target="_blank">
+                    <a href="https://wa.me/081321455855" target="_blank" style="background: inherit;">
                         <button class="ml-2" style="font-size:12px; background-color: #1e8714; border-radius:0px; border:none; padding: 5px 20px; cursor:pointer; color: white;">
                             <i class="fas fa-phone"></i>
                             Contact
