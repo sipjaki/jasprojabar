069d4f465c42d0a627d1fcd5117bf2c2e5c91445
abgblora <abgblorabangunangedung@gmail.com>
2025-03-10 01:38:26 +0700

    g

--- resources/views/backend/00_adminmasjaki/01_fiturterpisah/01_dashboard.blade.php
+++ resources/views/backend/00_adminmasjaki/01_fiturterpisah/01_dashboard.blade.php
@@ -1362,31 +1362,6 @@
 
       <!-- Language -->
       <li class="nav-item dropdown-language dropdown me-2 me-xl-0">
-        <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
-          <i class="icon-base bx bx-globe icon-md"></i>
-        </a>
-        <ul class="dropdown-menu dropdown-menu-end">
-          <li>
-            <a class="dropdown-item" href="javascript:void(0);" data-language="en" data-text-direction="ltr">
-              <span>English</span>
-            </a>
-          </li>
-          <li>
-            <a class="dropdown-item" href="javascript:void(0);" data-language="fr" data-text-direction="ltr">
-              <span>French</span>
-            </a>
-          </li>
-          <li>
-            <a class="dropdown-item" href="javascript:void(0);" data-language="ar" data-text-direction="rtl">
-              <span>Arabic</span>
-            </a>
-          </li>
-          <li>
-            <a class="dropdown-item" href="javascript:void(0);" data-language="de" data-text-direction="ltr">
-              <span>German</span>
-            </a>
-          </li>
-        </ul>
       </li>
       <!--/ Language -->
 
