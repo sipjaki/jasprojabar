457ccda5623550559f31490ceca3b361a630066f
abgblora <abgblorabangunangedung@gmail.com>
2025-03-10 00:55:28 +0700

    g

--- resources/views/backend/00_adminmasjaki/01_fiturterpisah/01_dashboard.blade.php
+++ resources/views/backend/00_adminmasjaki/01_fiturterpisah/01_dashboard.blade.php
@@ -1267,37 +1267,6 @@
 
 
 
-      <!-- Language -->
-      <li class="nav-item dropdown-language dropdown me-2 me-xl-0">
-        <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
-          <i class="icon-base bx bx-globe icon-md"></i>
-        </a>
-        <ul class="dropdown-menu dropdown-menu-end">
-          <li>
-            <a class="dropdown-item" href="javascript:void(0);" data-language="en" data-text-direction="ltr">
-              <span>English</span>
-            </a>
-          </li>
-          <li>
-            <a class="dropdown-item" href="javascript:void(0);" data-language="fr" data-text-direction="ltr">
-              <span>French</span>
-            </a>
-          </li>
-          <li>
-            <a class="dropdown-item" href="javascript:void(0);" data-language="ar" data-text-direction="rtl">
-              <span>Arabic</span>
-            </a>
-          </li>
-          <li>
-            <a class="dropdown-item" href="javascript:void(0);" data-language="de" data-text-direction="ltr">
-              <span>German</span>
-            </a>
-          </li>
-        </ul>
-      </li>
-      <!--/ Language -->
-
-
         <!-- Style Switcher -->
         <li class="nav-item dropdown me-2 me-xl-0">
           <a class="nav-link dropdown-toggle hide-arrow" id="nav-theme" href="javascript:void(0);" data-bs-toggle="dropdown">
