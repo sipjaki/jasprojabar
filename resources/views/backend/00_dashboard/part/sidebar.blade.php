973db22c373448c1d2b11a9dc949b74cc8a7274c
abgblora <abgblorabangunangedung@gmail.com>
2025-03-09 21:22:11 +0700

    g

--- resources/views/backend/00_dashboard/part/sidebar.blade.php
+++ resources/views/backend/00_dashboard/part/sidebar.blade.php
@@ -6,7 +6,7 @@
             <div class="col-sm-3 col-xs-6 sidebar pl-0">
                 <div class="inner-sidebar mr-3">
                     <!--Image Avatar-->
-                    <div class="avatar text-center" style="background:white; ">
+                    <div class="avatar text-center" style="background:black; ">
 
                         <div class="div">
                         {{-- @foreach ($user as $item ) --}}
