18a1ec2b76dc18a2987db8ab1389a7a9bd22a1e0
abgblora <abgblorabangunangedung@gmail.com>
2025-03-09 21:05:23 +0700

    g

--- resources/views/backend/00_dashboard/part/menufooter.blade.php
+++ resources/views/backend/00_dashboard/part/menufooter.blade.php
@@ -6,11 +6,11 @@
                     <span style="font-size: 16px;">&copy; Dinas Pekerjaan Umum Dan Penataan Ruang Kabupaten Blora Provinsi Jawa Tengah 2025 </span>
                 </div>
                 <div class="col-sm-4 text-right">
-                    <button class="ml-2" style="font-size:12px; background-color: #FFCB0F; border-radius:20px; border:none; padding: 5px 20px; cursor:pointer;">
+                    <button class="ml-2" style="font-size:12px; background-color: #1e8714; border-radius:20px; border:none; padding: 5px 20px; cursor:pointer;">
                         <i class="fas fa-phone"></i>
                         Contact
                     </button>
-                    <button class="ml-2" style="font-size:12px; background-color: #FFCB0F; border-radius:20px; border:none; padding: 5px 20px; cursor:pointer;">
+                    <button class="ml-2" style="font-size:12px; background-color: #1e8714; border-radius:20px; border:none; padding: 5px 20px; cursor:pointer;">
                         <i class="fas fa-life-ring"></i> <!-- Ini adalah ikon support -->
                         Support
                     </button>
