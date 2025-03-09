f7218f42fc22832d6553ae8ae5330dbcfdcc792f
abgblora <abgblorabangunangedung@gmail.com>
2025-03-09 19:21:03 +0700

    f

--- resources/views/backend/00_loginakun/01_login/index.blade.php
+++ resources/views/backend/00_loginakun/01_login/index.blade.php
@@ -113,7 +113,7 @@
     <div class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
       <!-- Copyright -->
       <div class="text-white mb-3 mb-md-0">
-        Copyright © 2020. All rights reserved.
+            © Dinas Pekerjaan Umum Dan Penataan Ruang Kabupaten Blora Provinsi Jawa Tengah
       </div>
       <!-- Copyright -->
 
