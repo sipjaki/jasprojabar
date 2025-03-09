6a4e036363f9b84d8083501ce2f2e53f77c790f3
abgblora <abgblorabangunangedung@gmail.com>
2025-03-09 21:02:50 +0700

    g

--- resources/views/backend/00_dashboard/part/menufooter.blade.php
+++ resources/views/backend/00_dashboard/part/menufooter.blade.php
@@ -3,7 +3,7 @@
             <hr>
             <div class="row mt-0 mb-2 footer">
                 <div class="col-sm-8" >
-                    <span style="font-size: 12px;">&copy; Dinas Pekerjaan Umum Dan Penataan Ruang Kabupaten Blora Provinsi Jawa Tengah Designed By <a href="https://wa.me/6281321455855" target="_blank" style="background: #1d1d1b; border-radius:10px; padding: 10px 10px;"><span style="color: black; font-size:12px;">CV. Angkasa Raya Teknik</span> </a></span>
+                    <span style="font-size: 16px;">&copy; Dinas Pekerjaan Umum Dan Penataan Ruang Kabupaten Blora Provinsi Jawa Tengah Designed By <a href="https://wa.me/6281321455855" target="_blank" style="background: #1d1d1b; border-radius:10px; padding: 5px 10px;"><span style="color: white; font-size:10px;">CV. Angkasa Raya Teknik</span> </a></span>
                 </div>
                 <div class="col-sm-4 text-right">
                     <button class="ml-2" style="font-size:12px; background-color: #FFCB0F; border-radius:20px; border:none; padding: 5px 20px; cursor:pointer;">
