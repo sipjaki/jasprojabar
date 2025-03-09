e0034a34eb0c1c0acba3ea007fa12abf63aade60
abgblora <abgblorabangunangedung@gmail.com>
2025-03-10 01:02:18 +0700

    g

---
+++ resources/views/backend/00_adminmasjaki/01_fiturterpisah/03_footer.bladeadmin.php
@@ -0,0 +1,103 @@
+
+
+
+<!-- Footer -->
+<footer class="content-footer footer bg-footer-theme">
+    <div class="container-xxl">
+      <div class="footer-container d-flex align-items-center justify-content-between py-4 flex-md-row flex-column">
+        <div class="mb-2 mb-md-0">
+          ©
+          <script>
+          document.write(new Date().getFullYear());
+          </script>, made with ❤️ by <a href="https://themeselection.com" target="_blank" class="footer-link">ThemeSelection</a>
+        </div>
+        <div class="d-none d-lg-inline-block">
+
+          <a href="https://themeselection.com/license/" class="footer-link me-4" target="_blank">License</a>
+          <a href="https://themeselection.com/" target="_blank" class="footer-link me-4">More Themes</a>
+
+          <a href="https://demos.themeselection.com/sneat-bootstrap-html-admin-template/documentation/" target="_blank" class="footer-link me-4">Documentation</a>
+
+
+          <a href="https://themeselection.com/support/" target="_blank" class="footer-link d-none d-sm-inline-block">Support</a>
+
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- / Footer -->
+
+
+          <div class="content-backdrop fade"></div>
+        </div>
+        <!-- Content wrapper -->
+      </div>
+      <!-- / Layout page -->
+    </div>
+
+
+
+    <!-- Overlay -->
+    <div class="layout-overlay layout-menu-toggle"></div>
+
+
+    <!-- Drag Target Area To SlideIn Menu On Small Screens -->
+    <div class="drag-target"></div>
+
+  </div>
+  <!-- / Layout wrapper -->
+
+
+        <div class="buy-now">
+          <a href="https://themeselection.com/item/sneat-dashboard-pro-bootstrap/" target="_blank" class="btn btn-danger btn-buy-now">Buy Now</a>
+        </div>
+
+
+
+
+      <!-- Core JS -->
+      <!-- build:js assets/vendor/js/theme.js  -->
+
+
+        <script src="../../assets/00_adminmasjaki/vendor/libs/jquery/jquery.js"></script>
+
+      <script src="../../assets/00_adminmasjaki/vendor/libs/popper/popper.js"></script>
+      <script src="../../assets/00_adminmasjaki/vendor/js/bootstrap.js"></script>
+      <script src="../../assets/00_adminmasjaki/vendor/libs/@algolia/autocomplete-js.js"></script>
+
+
+
+        <script src="../../assets/00_adminmasjaki/vendor/libs/pickr/pickr.js"></script>
+
+
+
+        <script src="../../assets/00_adminmasjaki/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+
+          <script src="../../assets/00_adminmasjaki/vendor/libs/hammer/hammer.js"></script>
+
+            <script src="../../assets/00_adminmasjaki/vendor/libs/i18n/i18n.js"></script>
+
+
+        <script src="../../assets/00_adminmasjaki/vendor/js/menu.js"></script>
+
+      <!-- endbuild -->
+
+      <!-- Vendors JS -->
+      <script src="../../assets/00_adminmasjaki/vendor/libs/moment/moment.js"></script>
+    <script src="../../assets/00_adminmasjaki/vendor/libs/datatables-bs5/datatables-bootstrap5.js"></script>
+    <script src="../../assets/00_adminmasjaki/vendor/libs/apex-charts/apexcharts.js"></script>
+
+      <!-- Main JS -->
+
+        <script src="../../assets/00_adminmasjaki/js/main.js"></script>
+
+
+      <!-- Page JS -->
+      <script src="../../assets/00_adminmasjaki/js/app-academy-dashboard.js"></script>
+
+    </body>
+  </html>
+
+    <!-- beautify ignore:end -->
+
