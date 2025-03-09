9de0543ee0b21f301d58252cc39c5ba9f9380d8a
abgblora <abgblorabangunangedung@gmail.com>
2025-03-09 15:39:44 +0700

    g

--- resources/views/frontend/00_approve/00_utama/index.blade.php
+++ resources/views/frontend/00_approve/00_utama/index.blade.php
@@ -132,24 +132,18 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-8">
-					<div class="news-details-box-image">
+
+                    @foreach ($data->skip(0)->take(0) as $item)
+
+                    <div class="news-details-box-image">
 						<div class="news-details-box-image-inner">
-							<img src="/assets/00_dokmasjaki/01_berita/berita1.JPG" class="img-fluid" alt="img-193">
-							<a href="news-details.html" class="news-details-box-date">MARET 2025</a>
+							<img src="{{ asset('storage/' . $item->foto) }}" class="img-fluid" alt="img-193">
+
+                            <a href="news-details.html" class="news-details-box-date">MARET 2025</a>
 						</div><!-- news-details-box-image-inner -->
 					</div><!-- news-details-box-image -->
 
-					{{-- <div class="news-details-meta-box">
-						<div class="news-details-meta-box-inner">
-							<span class="author">
-								by<a href="news-details.html">Admin</a>
-							</span><!-- author -->
-							<span class="comment">
-								<a href="news-details.html">02 Comments</a>
-							</span><!-- comment -->
-						</div><!-- news-details-meta-box-inner -->
-					</div><!-- news-details-meta-box --> --}}
-<br><br>
+                    <br><br>
                     <div class="news-details-content-box" style="margin-left: 25px;">
 						<h4>Pelatihan dan Uji Kompetensi SKK Jenjang 4 bagi Tenaga Kerja Konstruksi di Kabupaten Blora</h4>
 
@@ -166,28 +160,7 @@
 
                     </div><!-- /.news-details-content-box -->
 
-
-
-                    {{-- <div class="news-details-comment-form">
-						<h3>Leave a Comment</h3>
-						<form class="contact-form contact-form-validated" action="assets/inc/sendemail.php" method="post">
-							<div class="row row-gutter-10">
-								<div class="col-12 col-lg-6">
-									<input type="text" class="input-text" placeholder="Your name" name="name" aria-required="true">
-								</div><!-- col-12 col-lg-6 -->
-								<div class="col-12 col-lg-6">
-									<input type="email"  class="input-text" placeholder="Email address" name="email" aria-required="true">
-								</div><!-- col-12 col-lg-6 -->
-								<div class="col-12 col-lg-12">
-									<textarea name="message" placeholder="Write a message" class="input-text " aria-required="true"></textarea>
-								</div><!-- col-12 col-lg-12 -->
-								<div class="col-12 col-lg-12">
-									<button class="btn btn-primary">Submit Comment</button>
-								</div><!-- col-12 col-lg-12 -->
-							</div><!-- row -->
-						</form><!-- contact-form -->
-					</div><!-- news-details-comment-form --> --}}
-
+                    @endforeach
 
                 </div><!-- col-lg-8 -->
 
