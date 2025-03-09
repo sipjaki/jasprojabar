bf9775ecb5ffe4c458e11701956eb0207a035f90
abgblora <abgblorabangunangedung@gmail.com>
2025-03-09 21:09:07 +0700

    g

--- resources/views/backend/00_dashboard/part/menufooter.blade.php
+++ resources/views/backend/00_dashboard/part/menufooter.blade.php
@@ -6,17 +6,14 @@
                     <span style="font-size: 16px;">&copy; Dinas Pekerjaan Umum Dan Penataan Ruang Kabupaten Blora Provinsi Jawa Tengah 2025 </span>
                 </div>
                 <div class="col-sm-4 text-right">
-                    <button class="ml-2" style="font-size:12px; background-color: #1e8714; border-radius:10px; border:none; padding: 5px 20px; cursor:pointer;">
-                        <i class="fas fa-phone"></i>
-                        Contact
-                    </button>
-                    <p style="font-size:12px; margin-top:5px;">
-                        Designed By <a href="https://wa.me/6281321455855" style="color:#1e8714; text-decoration:none; font-weight:bold;">
-                            Angkasa Raya Teknik
-                        </a>
-                    </p>
+                    <a href="https://wa.me/081321455855" target="_blank">
+                        <button class="ml-2" style="font-size:12px; background-color: #1e8714; border-radius:0px; border:none; padding: 5px 20px; cursor:pointer; color: white;">
+                            <i class="fas fa-phone"></i>
+                            Contact
+                        </button>
+                    </a>
 
-                    {{-- <button class="ml-2" style="font-size:12px; background-color: #1e8714; border-radius:10px; border:none; padding: 5px 20px; cursor:pointer;">
+                    {{-- <button class="ml-2" style="font-size:12px; background-color: #1e8714; border-radius:20px; border:none; padding: 5px 20px; cursor:pointer;">
                         <i class="fas fa-life-ring"></i> <!-- Ini adalah ikon support -->
                         Support
                     </button> --}}
