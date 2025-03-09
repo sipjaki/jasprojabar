76e10e6f7d6661f9d0797daafc2461445de532b8
abgblora <abgblorabangunangedung@gmail.com>
2025-03-09 20:33:57 +0700

    g

--- resources/views/backend/00_dashboard/part/header.blade.php
+++ resources/views/backend/00_dashboard/part/header.blade.php
@@ -12,10 +12,6 @@
     <!--Meta Responsive tag-->
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!--Favicon Icon-->
-    {{-- <link rel="icon" href="favicon.ico" type="image/x-icon"> --}}
-    <link rel="icon" type="image/png" href="/assets/css/fe_css/images/icons/menuandroid/logohaiufull.jpg">
-
     <!--Bootstrap CSS-->
     <link rel="stylesheet" href="/assets/backend/css/bootstrap.min.css">
     <!--Custom style.css-->
@@ -52,7 +48,7 @@
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
-  
+
     <!-- jsPDF -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
 
@@ -64,7 +60,7 @@
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <link rel="icon" href="/assets/icon/sipjakikbb.png" type="image/x-icon">
+    <link rel="icon" href="/assets/icon/logokabupatenblora.png" type="image/x-icon">
     <title>{{ $title }}</title>
   </head>
   <body>
