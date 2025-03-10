16f44856dedd9a0f68e59ebfc24f71117bc1f61a
abgblora <abgblorabangunangedung@gmail.com>
2025-03-10 08:00:47 +0700

    dg

--- resources/views/backend/00_adminmasjaki/01_fiturterpisah/04_sidebaradmin.blade.php
+++ resources/views/backend/00_adminmasjaki/01_fiturterpisah/04_sidebaradmin.blade.php
@@ -44,7 +44,7 @@
     </svg>
   </span>
   </span>
-        <span class="app-brand-text demo menu-text fw-bold ms-2">Sneat</span>
+        <span class="app-brand-text demo menu-text fw-bold ms-2">MASJAKIBLORA</span>
       </a>
 
       <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto">
