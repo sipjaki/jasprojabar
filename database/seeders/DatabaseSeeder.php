39151d6af561f6831a31449853aaf42e05445445
abgblora <abgblorabangunangedung@gmail.com>
2025-03-11 05:21:22 +0700

    g

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7396,6 +7396,12 @@ hspkodepekerjaan::create([
     'namapekerjaan' => 'Pengukuran dan pasangan bouwplank',
 ]);
 
+hspkodepekerjaan::create([
+    'id' => 4,
+    'hsppaket_id' => 1,
+    'namapekerjaan' => 'Pengukuran dan pasangan bouwplank',
+]);
+
 
 //======================== ===================================================================================
 // HSP PAKET HARGA MAS JAKI BLORA
