cfbc5ad1ca8d8267e227014f78580f426a49dd01
abgblora <abgblorabangunangedung@gmail.com>
2025-03-10 07:56:15 +0700

    g

--- resources/views/backend/00_adminmasjaki/01_fiturterpisah/01_dashboard.blade.php
+++ resources/views/backend/00_adminmasjaki/01_fiturterpisah/01_dashboard.blade.php
@@ -463,7 +463,7 @@
       <li class="nav-item navbar-dropdown dropdown-user dropdown">
         <a class="nav-link dropdown-toggle hide-arrow p-0" href="javascript:void(0);" data-bs-toggle="dropdown">
           <div class="avatar avatar-online">
-            <img src="../../assets/00_adminmasjaki/img/avatars/1.png" alt class="rounded-circle" />
+            <img src="{{ asset('storage/' . $user->avatar) }}" alt class="rounded-circle" />
           </div>
         </a>
         <ul class="dropdown-menu dropdown-menu-end">
