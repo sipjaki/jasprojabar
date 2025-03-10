b6816ac51d8162a3d78e4ac1bd78bc400e535142
abgblora <abgblorabangunangedung@gmail.com>
2025-03-11 02:09:44 +0700

    g

--- resources/views/frontend/07_ahsp/02_hargasatuandasar/02_hargasatuanupah/index.blade.php
+++ resources/views/frontend/07_ahsp/02_hargasatuandasar/02_hargasatuanupah/index.blade.php
@@ -264,7 +264,7 @@ color: #45a049;
                                             <td style="text-align: center;">{{$item->kode}}</td>
                                             <td style="text-align: center;">{{$item->satuan}}</td>
                                             <td style="text-align: center;">Rp.</td>
-                                            <td style="text-align: right;">{{ number_format($item->besaran, 0, ',', '.') }}</td>
+                                            <td style="text-align: right;">{{$item->besaran}}</td>
                                             <td style="text-align: center;">Rp.</td>
                                             <td style="text-align: right;">{{$item->besaranperjam}}</td>
                                         </tr>
