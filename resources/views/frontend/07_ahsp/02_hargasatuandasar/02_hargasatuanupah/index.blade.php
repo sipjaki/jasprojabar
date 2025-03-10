560f702ebf6fef1eee6f362e29e042b606bbc223
abgblora <abgblorabangunangedung@gmail.com>
2025-03-11 01:44:16 +0700

    g

--- resources/views/frontend/07_ahsp/02_hargasatuandasar/02_hargasatuanupah/index.blade.php
+++ resources/views/frontend/07_ahsp/02_hargasatuandasar/02_hargasatuanupah/index.blade.php
@@ -128,7 +128,16 @@ color: #45a049;
 
     {{-- <section class="service-two-section" > --}}
         <section class="service-two-section" style="background-image: url('/assets/00_dokmasjaki/03_datajakon/banner1.jpg');">
-        <div class="container" style="background: #FFD100; border-radius:20px;">
+        <div class="container" style="
+        background: white;
+        max-width: 95%;
+        margin: 30px auto;
+        padding: 20px;
+        height: auto;
+        border-radius: 20px;
+        box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+        position: relative;
+        z-index: 10;">
             <div class="row row-gutter-y-40">
 
                 <div class="col-12 col-lg-12 col-xl-12">
@@ -136,16 +145,7 @@ color: #45a049;
                         <div class="service-two-imgbox">
 
                             <section class="donate-section" style="background: linear-gradient(to bottom, green, #FFD100, white);">
-                                <div class="container" style="
-                                background: white;
-                                max-width: 95%;
-                                margin: 30px auto;
-                                padding: 20px;
-                                height: auto;
-                                border-radius: 20px;
-                                box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
-                                position: relative;
-                                z-index: 10;">
+                                <div class="container" style="margin-top: -100px;">
                                     <div class="row row-gutter-30">
 
                                         <div class="col-xxl-12 col-lg-12">
