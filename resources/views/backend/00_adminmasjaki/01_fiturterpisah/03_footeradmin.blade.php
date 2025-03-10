f7c186227bc6a83b14b89219d9bd951440bf09fe
abgblora <abgblorabangunangedung@gmail.com>
2025-03-10 07:54:02 +0700

    dfg

--- resources/views/backend/00_adminmasjaki/01_fiturterpisah/03_footeradmin.blade.php
+++ resources/views/backend/00_adminmasjaki/01_fiturterpisah/03_footeradmin.blade.php
@@ -11,13 +11,13 @@
         </div>
         <div class="d-none d-lg-inline-block">
 
-          <a href="https://themeselection.com/license/" class="footer-link me-4" target="_blank">License</a>
+          {{-- <a href="https://themeselection.com/license/" class="footer-link me-4" target="_blank">License</a>
           <a href="https://themeselection.com/" target="_blank" class="footer-link me-4">More Themes</a>
 
           <a href="https://demos.themeselection.com/sneat-bootstrap-html-admin-template/documentation/" target="_blank" class="footer-link me-4">Documentation</a>
 
 
-          <a href="https://themeselection.com/support/" target="_blank" class="footer-link d-none d-sm-inline-block">Support</a>
+          <a href="https://themeselection.com/support/" target="_blank" class="footer-link d-none d-sm-inline-block">Support</a> --}}
 
         </div>
       </div>
