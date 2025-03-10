689517ffe1bc98b0012ef52b48fd91de011f35bd
abgblora <abgblorabangunangedung@gmail.com>
2025-03-10 07:53:31 +0700

    g

--- resources/views/backend/00_adminmasjaki/01_fiturterpisah/03_footeradmin.blade.php
+++ resources/views/backend/00_adminmasjaki/01_fiturterpisah/03_footeradmin.blade.php
@@ -46,9 +46,9 @@
   <!-- / Layout wrapper -->
 
 
-        <div class="buy-now">
+        {{-- <div class="buy-now">
           <a href="https://themeselection.com/item/sneat-dashboard-pro-bootstrap/" target="_blank" class="btn btn-danger btn-buy-now">Buy Now</a>
-        </div>
+        </div> --}}
 
 
 
