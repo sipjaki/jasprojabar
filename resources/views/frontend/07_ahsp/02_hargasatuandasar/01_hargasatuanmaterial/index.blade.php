7841f863a4356df911105587c00a1acdc24a151b
abgblora <abgblorabangunangedung@gmail.com>
2025-03-11 05:08:03 +0700

    g

--- resources/views/frontend/07_ahsp/02_hargasatuandasar/01_hargasatuanmaterial/index.blade.php
+++ resources/views/frontend/07_ahsp/02_hargasatuandasar/01_hargasatuanmaterial/index.blade.php
@@ -214,7 +214,7 @@ color: #45a049;
                                     <thead>
                                         <tr>
                                             <th onclick="sortTable(0)" style="cursor:pointer; text-align:center; width:75px;"> No <span class="sort-icon">⇅</span></th>
-                                            <th onclick="sortTable(1)" style="cursor:pointer; text-align:center; width:275px;"> Uraian <span class="sort-icon">⇅</span></th>
+                                            <th onclick="sortTable(1)" style="cursor:pointer; text-align:center; width:275px;"> Bahan Material <span class="sort-icon">⇅</span></th>
                                             <th onclick="sortTable(2)" style="cursor:pointer; text-align:center; width:100px;"> Satuan <span class="sort-icon">⇅</span></th>
                                             <th onclick="sortTable(3)" style="cursor:pointer; text-align:center; width:100px;"> Rp. <span class="sort-icon">⇅</span></th>
                                             <th onclick="sortTable(4)" style="cursor:pointer; text-align:center; width:150px;"> Besaran <span class="sort-icon">⇅</span></th>
