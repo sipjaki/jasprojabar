7ca63369668097a6d0e2f14989dea5e613670972
abgblora <abgblorabangunangedung@gmail.com>
2025-03-10 17:05:16 +0700

    fd

--- resources/views/frontend/00_approve/01_cssterpisah/header1.blade.php
+++ resources/views/frontend/00_approve/01_cssterpisah/header1.blade.php
@@ -105,8 +105,8 @@
 		</div><!-- topbar-inner -->
 	</div><!--topbar-->
 
-	<div class="main-menu sticky-header" style="background: #1d1d1b">
-                <div class="header-container" style="margin-top:5px;">
+	<div class="main-menu sticky-header" style="background: #1d1d1b" style="margin-bottom:10px;">
+                <div class="header-container">
                     <img src="/assets/icon/pupr.png" alt="Logo PUPR" class="header-logo">
                     <div class="header-text">
                         <h1 class="header-title">
