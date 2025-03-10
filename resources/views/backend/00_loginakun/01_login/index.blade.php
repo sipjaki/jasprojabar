d6203b0dcfe1ebbb335ee50f339822fa059026ef
abgblora <abgblorabangunangedung@gmail.com>
2025-03-10 17:12:32 +0700

    dgf

--- resources/views/backend/00_loginakun/01_login/index.blade.php
+++ resources/views/backend/00_loginakun/01_login/index.blade.php
@@ -43,9 +43,9 @@
       }
     }
   </style>
-  <section>
+  <section class="vh-95">
     <div class="container-fluid h-custom">
-      <div class="row d-flex justify-content-center align-items-center">
+      <div class="row d-flex justify-content-center align-items-center h-100">
         <div class="col-md-9 col-lg-6 col-xl-5 d-flex justify-content-center align-items-center"
         style="gap: 15px;">
        <img src="/assets/icon/logokabupatenblora.png" class="img-fluid" alt="Logo Kabupaten Blora" width="125" loading="lazy">
