e33cd54a37d3b7196270619024872afa19302489
abgblora <abgblorabangunangedung@gmail.com>
2025-03-11 01:38:53 +0700

    g

--- resources/views/frontend/00_approve/01_cssterpisah/header1.blade.php
+++ resources/views/frontend/00_approve/01_cssterpisah/header1.blade.php
@@ -5,11 +5,11 @@
 		<div class="topbar-inner" style="background: #2ECC71 " >
 			<div class="topbar-left" style="background: #2ECC71 ">
 				<div class="topbar-socials" style="height: 8vh;">
-                    <a href="#"><i class="fa-brands fa-tiktok" style="font-size: 28px; margin-top:15px;"></i></a>
+                    <a href="#"><i class="fa-brands fa-tiktok" style="font-size: 24px;"></i></a>
                     {{-- <a href="#"><i class="fa-solid fa-envelope" style="font-size: 25px;"></i></a> --}}
-                    <a href="#"><i class="fa-brands fa-instagram" style="font-size: 20px; "></i></a>
-                    <a href="#"><i class="fa-brands fa-whatsapp" style="font-size: 20px; "></i></a>
-                    <a href="#"><i class="fa-brands fa-facebook" style="font-size: 20px; "></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram" style="font-size: 24px; "></i></a>
+                    <a href="#"><i class="fa-brands fa-whatsapp" style="font-size: 24px; "></i></a>
+                    <a href="#"><i class="fa-brands fa-facebook" style="font-size: 24px; "></i></a>
 				</div><!--topbar-socials-->
 				<div class="topbar-info" style="height: 8vh;">
 					<ul>
