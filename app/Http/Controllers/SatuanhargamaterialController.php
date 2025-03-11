229c90f723473a7bfa59e1117373b6cd32ec5b38
abgblora <abgblorabangunangedung@gmail.com>
2025-03-12 04:23:07 +0700

    df

--- app/Http/Controllers/SatuanhargamaterialController.php
+++ app/Http/Controllers/SatuanhargamaterialController.php
@@ -295,5 +295,59 @@ class SatuanhargamaterialController extends Controller
         ]);
     }
 
+// DIVISI 4
+
+
+    public function hspdivisi4(Request $request)
+    {
+        $perPage = $request->input('perPage', 25);
+        $search = $request->input('search');
+
+        $query = hspkonstruksiumum4::query();
+
+        if ($search) {
+            $query->where('kode', 'LIKE', "%{$search}%")
+                  ->orWhere('jenispekerjaan', 'LIKE', "%{$search}%")
+                  ->orWhere('hargasatuan', 'LIKE', "%{$search}%")
+
+                  ->orWhereHas('hspdivisi', function ($q) use ($search) {
+                      $q->where('hspdivisi', 'LIKE', "%{$search}%"); // 'jabatankerja' = nama kolom di tabel jabatankerja
+                  })
+
+                  ->orWhereHas('hsppaket4', function ($q) use ($search) {
+                      $q->where('hsppaket4', 'LIKE', "%{$search}%"); // 'jenjang' = nama kolom di tabel jenjang
+                  })
+
+                  ->orWhereHas('hspkodepekerjaan4', function ($q) use ($search) {
+                      $q->where('namapekerjaan', 'LIKE', "%{$search}%"); // 'jenjang' = nama kolom di tabel jenjang
+                  });
+        }
+
+        $data = $query->paginate($perPage);
+
+        if ($request->ajax()) {
+            return response()->json([
+                'html' => view('frontend.07_ahsp.03_hspkonstruksiumum.04_divisi4.partials.table', compact('data'))->render()
+            ]);
+        }
+
+        // $subdata = HspKonstruksiUmum::with('subhargadiv1')
+        // ->whereHas('jenispekerjaan', function ($query) use ($jenispekerjaan) {
+        //     $query->where('jenispekerjaan', $jenispekerjaan);
+        // })
+        // ->get();
+
+        // if ($request->ajax()) {
+        //     return response()->json($subdata);
+        // }
+        return view('frontend.07_ahsp.03_hspkonstruksiumum.04_divisi4.04_divisi4', [
+            'title' => 'HSP Divisi 4 Pekerjaan Lansekap',
+            'data' => $data,
+            // 'subdata' => $subdata,
+            'perPage' => $perPage,
+            'search' => $search
+        ]);
+    }
+
 
 }
