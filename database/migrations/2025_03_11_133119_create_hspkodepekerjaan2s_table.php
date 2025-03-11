e0cf0ae571c6f1f547c42f4f2847c7defbe98b38
abgblora <abgblorabangunangedung@gmail.com>
2025-03-12 00:55:08 +0700

    f

--- database/migrations/2025_03_11_133119_create_hspkodepekerjaan2s_table.php
+++ database/migrations/2025_03_11_133119_create_hspkodepekerjaan2s_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('hspkonstruksiumum2_id')->nullable();
             $table->foreignId('hsppaket2_id')->nullable();
+            $table->string('kodepekerjaan')->nullable();
             $table->string('namapekerjaan')->nullable();
             $table->softDeletes();
             $table->timestamps();
