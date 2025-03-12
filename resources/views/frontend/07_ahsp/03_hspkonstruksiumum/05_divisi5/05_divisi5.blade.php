ec2531478a8135a98e289f7b8862326f710a8a80
abgblora <abgblorabangunangedung@gmail.com>
2025-03-12 20:21:02 +0700

    g

--- resources/views/frontend/07_ahsp/03_hspkonstruksiumum/05_divisi5/05_divisi5.blade.php
+++ resources/views/frontend/07_ahsp/03_hspkonstruksiumum/05_divisi5/05_divisi5.blade.php
@@ -236,7 +236,7 @@ color: #45a049;
                                             <tr>
                                                 <th onclick="sortTable(0)" style="cursor:pointer; text-align:center; width:100px;"> No <span class="sort-icon">⇅</span></th>
                                                 <th onclick="sortTable(1)" style="cursor:pointer; text-align:center; width:150px;"> Kode AHSP <span class="sort-icon">⇅</span></th>
-                                                <th onclick="sortTable(1)" style="cursor:pointer; text-align:center; width:150px;"> HSP Divisi <span class="sort-icon">⇅</span></th>
+                                                {{-- <th onclick="sortTable(1)" style="cursor:pointer; text-align:center; width:150px;"> HSP Divisi <span class="sort-icon">⇅</span></th> --}}
                                                 <th onclick="sortTable(1)" style="cursor:pointer; text-align:center; width:150px;"> Paket  <span class="sort-icon">⇅</span></th>
                                                 <th onclick="sortTable(4)" style="cursor:pointer; text-align:center; width:250px;"> Item Pekerjaan <span class="sort-icon">⇅</span></th>
                                                 <th onclick="sortTable(7)" style="cursor:pointer; text-align:center; width:600px;"> Jenis Pekerjaan <span class="sort-icon">⇅</span></th>
@@ -256,7 +256,7 @@ color: #45a049;
                                                     {{$item->kode}}
                                                     {{-- {{ optional($item->kode->id) }} --}}
                                                 </td>
-                                                <td style="text-align: center;">{{$item->hspdivisi->hspdivisi}}</td>
+                                                {{-- <td style="text-align: center;">{{$item->hspdivisi->hspdivisi}}</td> --}}
                                                 <td style="text-align: center;">{{$item->hsppaket5->hsppaket5}}</td>
                                                 <td style="text-align: center;">{{$item->hspkodepekerjaan5->namapekerjaan}}</td>
                                                 <td style="cursor: pointer; color: blue; text-decoration: underline;" onclick="showModal('{{ $item->jenispekerjaan }}')">{{$item->jenispekerjaan}}</td>
