940c085dca9c3d19c82687c8b5e56c23bd091c15
abgblora <abgblorabangunangedung@gmail.com>
2025-03-12 21:30:54 +0700

    g

--- resources/views/backend/00_adminmasjaki/01_fiturterpisah/04_sidebaradmin.blade.php
+++ resources/views/backend/00_adminmasjaki/01_fiturterpisah/04_sidebaradmin.blade.php
@@ -74,23 +74,93 @@
             class="sidebar-item  has-sub">
             <a href="#" class='sidebar-link'>
                 <i class="bi bi-stack"></i>
-                <span>Kelembagaan</span>
+                <span>Components</span>
             </a>
 
             <ul class="submenu ">
 
                 <li class="submenu-item  ">
-                    <a href="/404" class="submenu-link">Struktur Organisasi</a>
+                    <a href="component-accordion.html" class="submenu-link">Accordion</a>
 
                 </li>
 
                 <li class="submenu-item  ">
-                    <a href="/404" class="submenu-link">Profil Jakon DPUPR Blora</a>
+                    <a href="component-alert.html" class="submenu-link">Alert</a>
 
                 </li>
 
                 <li class="submenu-item  ">
-                    <a href="/404" class="submenu-link">Tupoksi Jakon DPUPR Blora</a>
+                    <a href="component-badge.html" class="submenu-link">Badge</a>
+
+                </li>
+
+                <li class="submenu-item  ">
+                    <a href="component-breadcrumb.html" class="submenu-link">Breadcrumb</a>
+
+                </li>
+
+                <li class="submenu-item  ">
+                    <a href="component-button.html" class="submenu-link">Button</a>
+
+                </li>
+
+                <li class="submenu-item  ">
+                    <a href="component-card.html" class="submenu-link">Card</a>
+
+                </li>
+
+                <li class="submenu-item  ">
+                    <a href="component-carousel.html" class="submenu-link">Carousel</a>
+
+                </li>
+
+                <li class="submenu-item  ">
+                    <a href="component-collapse.html" class="submenu-link">Collapse</a>
+
+                </li>
+
+                <li class="submenu-item  ">
+                    <a href="component-dropdown.html" class="submenu-link">Dropdown</a>
+
+                </li>
+
+                <li class="submenu-item  ">
+                    <a href="component-list-group.html" class="submenu-link">List Group</a>
+
+                </li>
+
+                <li class="submenu-item  ">
+                    <a href="component-modal.html" class="submenu-link">Modal</a>
+
+                </li>
+
+                <li class="submenu-item  ">
+                    <a href="component-navs.html" class="submenu-link">Navs</a>
+
+                </li>
+
+                <li class="submenu-item  ">
+                    <a href="component-pagination.html" class="submenu-link">Pagination</a>
+
+                </li>
+
+                <li class="submenu-item  ">
+                    <a href="component-progress.html" class="submenu-link">Progress</a>
+
+                </li>
+
+                <li class="submenu-item  ">
+                    <a href="component-spinner.html" class="submenu-link">Spinner</a>
+
+                </li>
+
+                <li class="submenu-item  ">
+                    <a href="component-toasts.html" class="submenu-link">Toasts</a>
+
+                </li>
+
+                <li class="submenu-item  ">
+                    <a href="component-tooltip.html" class="submenu-link">Tooltip</a>
 
                 </li>
 
@@ -102,74 +172,39 @@
         <li
             class="sidebar-item  has-sub">
             <a href="#" class='sidebar-link'>
-                <i class="bi bi-stack"></i>
-                <span>Peraturan</span>
+                <i class="bi bi-collection-fill"></i>
+                <span>Extra Components</span>
             </a>
 
             <ul class="submenu ">
 
                 <li class="submenu-item  ">
-                    <a href="/404" class="submenu-link">Standar Biaya Umum</a>
+                    <a href="extra-component-avatar.html" class="submenu-link">Avatar</a>
 
                 </li>
 
                 <li class="submenu-item  ">
-                    <a href="/404" class="submenu-link">Undang-Undang</a>
+                    <a href="extra-component-divider.html" class="submenu-link">Divider</a>
 
                 </li>
 
                 <li class="submenu-item  ">
-                    <a href="/404" class="submenu-link">Undang-Undang</a>
+                    <a href="extra-component-date-picker.html" class="submenu-link">Date Picker</a>
 
                 </li>
 
                 <li class="submenu-item  ">
-                    <a href="/404" class="submenu-link">Peraturan Pemerintah</a>
+                    <a href="extra-component-sweetalert.html" class="submenu-link">Sweet Alert</a>
 
                 </li>
 
                 <li class="submenu-item  ">
-                    <a href="/404" class="submenu-link">Peraturan Presiden</a>
+                    <a href="extra-component-toastify.html" class="submenu-link">Toastify</a>
 
                 </li>
 
                 <li class="submenu-item  ">
-                    <a href="/404" class="submenu-link">Peraturan Menteri</a>
-
-                </li>
-
-                <li class="submenu-item  ">
-                    <a href="/404" class="submenu-link">Keputusan Menteri</a>
-
-                </li>
-
-                <li class="submenu-item  ">
-                    <a href="/404" class="submenu-link">Surat Edaran Menteri</a>
-
-                </li>
-
-                <li class="submenu-item  ">
-                    <a href="/404" class="submenu-link">Surat Referensi</a>
-
-                </li>
-
-                <li class="submenu-item  ">
-                    <a href="/404" class="submenu-link">Peraturan Daerah</a>
-
-                </li>
-
-                <li class="submenu-item  ">
-                    <a href="/404" class="submenu-link">Peraturan Gubernur</a>
-
-                </li>
-
-                <li class="submenu-item  ">
-                    <a href="/404" class="submenu-link">Peraturan Walikota/Bupati</a>
-
-                </li>
-
-                <li class="submenu-item  ">
-                    <a href="/404" class="submenu-link">Surat Keputusan</a>
+                    <a href="extra-component-rating.html" class="submenu-link">Rating</a>
 
                 </li>
 
@@ -177,77 +212,38 @@
 
 
         </li>
+
         <li
             class="sidebar-item  has-sub">
             <a href="#" class='sidebar-link'>
-                <i class="bi bi-stack"></i>
-                <span>Peraturan</span>
+                <i class="bi bi-grid-1x2-fill"></i>
+                <span>Layouts</span>
             </a>
 
             <ul class="submenu ">
 
                 <li class="submenu-item  ">
-                    <a href="/404" class="submenu-link">Standar Biaya Umum</a>
+                    <a href="layout-default.html" class="submenu-link">Default Layout</a>
 
                 </li>
 
                 <li class="submenu-item  ">
-                    <a href="/404" class="submenu-link">Undang-Undang</a>
+                    <a href="layout-vertical-1-column.html" class="submenu-link">1 Column</a>
 
                 </li>
 
                 <li class="submenu-item  ">
-                    <a href="/404" class="submenu-link">Undang-Undang</a>
+                    <a href="layout-vertical-navbar.html" class="submenu-link">Vertical Navbar</a>
 
                 </li>
 
                 <li class="submenu-item  ">
-                    <a href="/404" class="submenu-link">Peraturan Pemerintah</a>
+                    <a href="layout-rtl.html" class="submenu-link">RTL Layout</a>
 
                 </li>
 
                 <li class="submenu-item  ">
-                    <a href="/404" class="submenu-link">Peraturan Presiden</a>
-
-                </li>
-
-                <li class="submenu-item  ">
-                    <a href="/404" class="submenu-link">Peraturan Menteri</a>
-
-                </li>
-
-                <li class="submenu-item  ">
-                    <a href="/404" class="submenu-link">Keputusan Menteri</a>
-
-                </li>
-
-                <li class="submenu-item  ">
-                    <a href="/404" class="submenu-link">Surat Edaran Menteri</a>
-
-                </li>
-
-                <li class="submenu-item  ">
-                    <a href="/404" class="submenu-link">Surat Referensi</a>
-
-                </li>
-
-                <li class="submenu-item  ">
-                    <a href="/404" class="submenu-link">Peraturan Daerah</a>
-
-                </li>
-
-                <li class="submenu-item  ">
-                    <a href="/404" class="submenu-link">Peraturan Gubernur</a>
-
-                </li>
-
-                <li class="submenu-item  ">
-                    <a href="/404" class="submenu-link">Peraturan Walikota/Bupati</a>
-
-                </li>
-
-                <li class="submenu-item  ">
-                    <a href="/404" class="submenu-link">Surat Keputusan</a>
+                    <a href="layout-horizontal.html" class="submenu-link">Horizontal Menu</a>
 
                 </li>
 
