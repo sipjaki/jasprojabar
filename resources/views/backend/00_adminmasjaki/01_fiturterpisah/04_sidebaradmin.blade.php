6f41987eeca9e5d043ab7c73bbbcd2a1b792fd65
abgblora <abgblorabangunangedung@gmail.com>
2025-03-12 21:14:09 +0700

    g

--- resources/views/backend/00_adminmasjaki/01_fiturterpisah/04_sidebaradmin.blade.php
+++ resources/views/backend/00_adminmasjaki/01_fiturterpisah/04_sidebaradmin.blade.php
@@ -5,7 +5,10 @@
 <div class="sidebar-header position-relative">
     <div class="d-flex justify-content-between align-items-center">
         <div class="logo">
-            <a href="index.html"><img src="./assets/icon/logokabupatenblora.png" alt="Logo" srcset="" width="100px;"></a>
+            <a href="#">
+                <img src="./assets/icon/logokabupatenblora.png" alt="Logo" width="100%" style="max-width: 100px; height: auto;">
+            </a>
+
         </div>
         <div class="theme-toggle d-flex gap-2  align-items-center mt-2">
             <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true"
