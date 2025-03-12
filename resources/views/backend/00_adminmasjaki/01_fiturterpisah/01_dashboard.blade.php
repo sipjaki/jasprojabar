2c6a05d2932d4f3a1aa94df1c96f64d3f994700f
abgblora <abgblorabangunangedung@gmail.com>
2025-03-12 20:50:32 +0700

    g

--- resources/views/backend/00_adminmasjaki/01_fiturterpisah/01_dashboard.blade.php
+++ resources/views/backend/00_adminmasjaki/01_fiturterpisah/01_dashboard.blade.php
@@ -281,7 +281,10 @@
             <footer>
     <div class="footer clearfix mb-0 text-muted">
         <div class="float-start">
-            <p>2023 &copy; Mazer</p>
+            <script>
+                document.write(new Date().getFullYear());
+            </script>
+            {{-- <p>2025 &copy; Mazer</p> --}}
         </div>
         <div class="float-end">
             <p>Crafted with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
