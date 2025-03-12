cab40358faa098125313962b2871b930b54d6e02
abgblora <abgblorabangunangedung@gmail.com>
2025-03-13 00:35:10 +0700

    g

--- resources/views/backend/00_adminmasjaki/01_fiturterpisah/01_dashboard.blade.php
+++ resources/views/backend/00_adminmasjaki/01_fiturterpisah/01_dashboard.blade.php
@@ -22,7 +22,7 @@
 </div>
 <div class="page-content">
     <section class="row">
-        <div class="col-12 col-lg-9">
+        <div class="col-12 col-lg-12">
             <div class="row">
                 <div class="col-6 col-lg-3 col-md-6">
                     <div class="card">
