e878d8bfe71d3c4457fa752a1c12bcaf99c03963
abgblora <abgblorabangunangedung@gmail.com>
2025-03-13 19:32:59 +0700

    g

--- resources/views/frontend/00_approve/00_utama/index.blade.php
+++ resources/views/frontend/00_approve/00_utama/index.blade.php
@@ -251,7 +251,7 @@
                                         <div class="sidebar-meta-item">
                                             <div class="sidebar-meta-icon">
 												<span class="author">
-													by<a href="news-details.html">Mas Zaki</a>
+													Penulis : <a href="news-details.html">Mas Zaki</a>
 												</span><!-- author -->
 											</div><!-- sidebar-meta-icon -->
 										</div><!-- sidebar-meta-item -->
