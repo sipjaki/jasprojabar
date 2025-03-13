77c7832363feac729159cc85a15cfde0cfad74dd
abgblora <abgblorabangunangedung@gmail.com>
2025-03-14 01:35:19 +0700

    g

--- app/Http/Controllers/FedashboardController.php
+++ app/Http/Controllers/FedashboardController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\View;
 use Illuminate\Http\Request;
 
 use App\Models\berita; // Pastikan namespace model sesuai dengan struktur direktori
