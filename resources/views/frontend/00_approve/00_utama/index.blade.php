cc338f7e3d46df068aea16c5b055a330822f34be
abgblora <abgblorabangunangedung@gmail.com>
2025-03-13 17:36:55 +0700

    g

--- resources/views/frontend/00_approve/00_utama/index.blade.php
+++ resources/views/frontend/00_approve/00_utama/index.blade.php
@@ -288,7 +288,7 @@
                                         <div class="event-card-image">
                                             <div class="event-card-image-inner">
                                                 <a href="#">
-                                                    <img src="{{ asset('storage/' . $item->foto1) }}" class="img-fluid" alt="Gambar Artikel" width="100%">
+                                                    <img src="{{ asset('storage/' . $item->foto1) }}" class="img-fluid" alt="Gambar Artikel" width="40%">
                                                 </a>
                                                 <div class="event-card-meta">
                                                     <div class="event-meta-date">
@@ -313,7 +313,7 @@
                         <div class="event-card">
                             <div class="event-card-image">
                                 <div class="event-card-image-inner">
-                                    <iframe width="100%" height="315"
+                                    <iframe width="100%" height="800"
                                         src="https://www.youtube.com/embed/IP3whQNdlZM"
                                         title="Dinas Pekerjaan Umum dan Penataan Ruang Kab Blora"
                                         frameborder="0"
