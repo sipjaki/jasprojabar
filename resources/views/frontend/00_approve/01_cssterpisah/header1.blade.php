5c3a88c9149e403ba820ac9d15a3dddb1c4c78af
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-15 01:35:31 +0700

    g

--- resources/views/frontend/00_approve/01_cssterpisah/header1.blade.php
+++ resources/views/frontend/00_approve/01_cssterpisah/header1.blade.php
@@ -108,6 +108,7 @@
 	<div class="main-menu sticky-header" style="background: #1d1d1b">
     <br>
                 <div class="header-container">
+                    <img src="/assets/icon/logokabupatenblora.png" alt="Logo PUPR" class="header-logo">
                     <img src="/assets/icon/pupr.png" alt="Logo PUPR" class="header-logo">
                     <div class="header-text">
                         <h1 class="header-title">
