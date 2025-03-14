686cd6d23315dc234ddc3bfe9582d4a33d0bc633
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-15 01:23:17 +0700

    g

--- resources/views/frontend/03_masjaki_jakon/05_asosiasimasjaki/showasosiasikontraktor.blade.php
+++ resources/views/frontend/03_masjaki_jakon/05_asosiasimasjaki/showasosiasikontraktor.blade.php
@@ -191,7 +191,7 @@ color: #45a049;
 
                                         <a href="/datajakon/asosiasimasjaki" style="background: white;">
                                             <button class="badgekembali" style="border: none; font-size: 12px; cursor: pointer; margin-right: 10px;">
-                                                <i class="fas fa-arrow-circle-left mr-2" style="margin-right: 20px; color:red;"></i><span style="color: black; font-size:20px;">KEMBALI</span>
+                                                <i class="fas fa-arrow-circle-left mr-2" style="margin-right: 20px; color:red;"></i><span style="color: black; font-size:16px;">Kembali</span>
                                             </button>
                                         </a>
 
