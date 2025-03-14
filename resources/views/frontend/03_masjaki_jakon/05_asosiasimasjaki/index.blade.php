abcf8a785607a3cee58b89db3963abe2ee14adb7
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-15 00:47:26 +0700

    g

--- resources/views/frontend/03_masjaki_jakon/05_asosiasimasjaki/index.blade.php
+++ resources/views/frontend/03_masjaki_jakon/05_asosiasimasjaki/index.blade.php
@@ -307,7 +307,7 @@ color: #45a049;
                             <div class="table-wrapper" style="margin-top:-130px; position: relative;">
                                 <div style="display: flex; justify-content: space-between; margin-bottom: 10px; align-items: center;">
                                     <div class="pagination-info-box" style="margin-right:150px; padding: 10px; border: 1px solid black; background-color: #f9f9f9; border-radius: 5px;">
-                                        <div class="pagination-info" style="color: black; font-weight: 500; font-size:14px;">
+                                        <div class="pagination-info" style="color: black; font-weight: 500; font-size:18px;">
                                             Data Ke {{ $databujkkonsultanpaginate->firstItem() }} Sampai {{ $databujkkonsultanpaginate->lastItem() }} Dari {{ $databujkkonsultanpaginate->total() }} Jumlah : BUJK Konsultasi Konstruksi
                                              {{-- {{$title}} --}}
                                         </div>
