c5e3d070448fdd5f732c6f6ad4935df93d0c976e
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-16 06:26:45 +0700

    g

--- resources/views/frontend/04_pembinaan/02_agendaskk/03_daftaragendaskk.blade.php
+++ resources/views/frontend/04_pembinaan/02_agendaskk/03_daftaragendaskk.blade.php
@@ -225,14 +225,9 @@ color: #adb6ad;
                                         </select>
                                     </div>
 
-                                    <div style="position: relative; display: inline-block; margin-right:10px;">
-                                        <label>Masukan NIK</label>
-                                        <input type="search" id="searchsertifikat" placeholder="Download Sertifikat" onkeyup="searchTable()" style="border: 1px solid #ccc; padding: 10px 20px; font-size: 14px; border-radius: 10px; width: 300px;">
-                                        {{-- <i class="fas fa-search" style="position: absolute; right: 10px; top: 50%; transform: translateY(-50%); font-size: 16px; color: #888;"></i> --}}
-                                    </div>
 
                                     <div style="position: relative; display: inline-block; margin-right:10px;">
-                                        <label for="">Cari Kegiatan</label>
+                                        {{-- <label for="">Cari Kegiatan</label> --}}
                                         <input type="search" id="searchInput" placeholder="Cari Kegiatan ...." onkeyup="searchTable()" style="border: 1px solid #ccc; padding: 10px 20px; font-size: 14px; border-radius: 10px; width: 300px;">
                                         <i class="fas fa-search" style="position: absolute; right: 10px; top: 50%; transform: translateY(-50%); font-size: 16px; color: #888;"></i>
                                     </div>
