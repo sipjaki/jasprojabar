d9a8d136598e4502126cb5b482a40a37ea61cda9
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-16 00:31:37 +0700

    g

--- resources/views/frontend/05_pengawasan/03_tertibjakon/01_tertibusaha/show.blade.php
+++ resources/views/frontend/05_pengawasan/03_tertibjakon/01_tertibusaha/show.blade.php
@@ -225,6 +225,19 @@ color: #45a049;
     </td>
 </tr>
 
+        <tr>
+    <th style="text-align:center; color: black; width:50px; background-color: white;">
+        <i class="fas fa-hammer" style="font-size:20px; margin-right:8px;"></i> <!-- Ikon untuk Status Penyedia -->
+    </th>
+    <th colspan="11" style="text-align:left; color: black; background-color: white; padding-left: 10px; width:300px;">
+        Nama Pekerjaan
+    </th>
+    <th colspan="1" style="text-align:center; color: black; background-color: white; width:25px;">:</th>
+    <td colspan="12" style="text-align:left; background-color: white; padding-left: 10px; color: black; width:1000px;">
+        {{$data->namapekerjaan}}
+    </td>
+</tr>
+
 <tr>
     <th style="text-align:center; color: black; width:50px; background-color: white;">
         <i class="fas fa-id-card" style="font-size:20px; margin-right:8px;"></i> <!-- Ikon untuk Nomor Induk Berusaha -->
