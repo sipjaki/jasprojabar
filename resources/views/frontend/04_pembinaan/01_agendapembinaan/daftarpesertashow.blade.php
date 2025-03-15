56aae2a62635023363733a2bdcd80038a5174bc7
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-15 23:31:52 +0700

    g

--- resources/views/frontend/04_pembinaan/01_agendapembinaan/daftarpesertashow.blade.php
+++ resources/views/frontend/04_pembinaan/01_agendapembinaan/daftarpesertashow.blade.php
@@ -258,7 +258,8 @@ color: #45a049;
                                             <td style="text-align: center;">{{ $loop->index + 1 }}</td>
 
                                             <td style="text-transform: capitalize;">
-                                                {{ dd($item->user) }}
+                                         {{ dd($item->user) }}
+
                                             </td>
                                             {{-- <td style="text-transform: capitalize;">{{ ucwords(strtolower($item->user->name)) }}</td> --}}
                                             {{-- <td>{{$item->user->name}}</td> --}}
