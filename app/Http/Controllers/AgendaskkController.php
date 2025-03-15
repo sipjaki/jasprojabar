6e1734ff21e934faf1e9bd8d4b4c1779a2b1cfc3
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-16 06:39:10 +0700

    g

--- app/Http/Controllers/AgendaskkController.php
+++ app/Http/Controllers/AgendaskkController.php
@@ -141,7 +141,7 @@ public function daftarpesertaskkshow(Request $request, $namakegiatan)
     $user = Auth::user();
 
         $datapesertaskk = allskktenagakerjablora::where('agendaskk_id', $agendaskk->id)
-                    ->select(['id', 'user_id', 'jeniskelamin', 'namaasosiasi', 'jabatankerja'])
+                    ->select(['id', 'user_id', 'jeniskelamin', 'namaasosiasi', 'tahunpilihan_id'])
                     ->paginate(25);
 
     $dataagendaskk = agendaskk::where('namakegiatan', $namakegiatan)->first();
