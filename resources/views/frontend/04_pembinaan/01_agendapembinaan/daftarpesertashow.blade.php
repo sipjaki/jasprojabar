fd9036856f549f7ed674be62e7cf1581e8b83f13
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-15 23:38:22 +0700

    g

--- resources/views/frontend/04_pembinaan/01_agendapembinaan/daftarpesertashow.blade.php
+++ resources/views/frontend/04_pembinaan/01_agendapembinaan/daftarpesertashow.blade.php
@@ -246,9 +246,9 @@ color: #45a049;
                                         <tr>
                                             <th onclick="sortTable(0)" style="cursor:pointer; text-align:center; width:100px;"> No <span class="sort-icon">⇅</span></th>
                                             <th onclick="sortTable(1)" style="cursor:pointer; text-align:center; width:300px;"> Nama Lengkap <span class="sort-icon">⇅</span></th>
-                                            <th onclick="sortTable(2)" style="cursor:pointer; text-align:center; width:400px;"> Gender <span class="sort-icon">⇅</span></th>
-                                            <th onclick="sortTable(3)" style="cursor:pointer; text-align:center; width:150px;"> Instansi <span class="sort-icon">⇅</span></th>
-                                            <th style="text-align:center; width:100px;"> View </th>
+                                            <th onclick="sortTable(2)" style="cursor:pointer; text-align:center; width:100;"> Gender <span class="sort-icon">⇅</span></th>
+                                            <th onclick="sortTable(3)" style="cursor:pointer; text-align:center; width:400px;"> Instansi <span class="sort-icon">⇅</span></th>
+                                            {{-- <th style="text-align:center; width:100px;"> View </th> --}}
                                         </tr>
                                     </thead>
                                     <tbody id="tableBody">
