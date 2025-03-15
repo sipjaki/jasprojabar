1e852cb15fe24ea916d31b2df240b4c96a29a879
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-15 23:22:59 +0700

    g

--- app/Http/Controllers/PembinaanController.php
+++ app/Http/Controllers/PembinaanController.php
@@ -175,9 +175,6 @@ class PembinaanController extends Controller
         }
 
         $user = Auth::user();
-            // Ambil semua data dari tabel bujkkontraktor berdasarkan asosiasi_id
-            // $datapesertapelatihan = pesertapelatihan::where('agendapelatihan_id', $agendapelatihan->id)->get(['id', 'user_id', 'jeniskelamin', 'instansi'])->paginate(25);
-            // // $databujkkontraktorpaginate = bu::where('asosiasimasjaki_id', $asosiasi->id)->paginate(10);
 
             $datapesertapelatihan = PesertaPelatihan::where('agendapelatihan_id', $agendapelatihan->id)
                         ->select(['id', 'user_id', 'jeniskelamin', 'instansi'])
