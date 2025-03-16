a32faeb112f398430f02468e1cd7e4ac92afe5df
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-17 05:06:19 +0700

    g

--- resources/views/frontend/07_ahsp/03_hspkonstruksiumum/01_divisi1/ahspdivisi1.blade.php
+++ resources/views/frontend/07_ahsp/03_hspkonstruksiumum/01_divisi1/ahspdivisi1.blade.php
@@ -20,7 +20,7 @@
                     <div class="col-lg-12">
                         <button onclick="window.history.back()"
                         style="margin-top: 20px; padding: 15px 40px; width: 175px; height: 60px; font-size: 16px;
-                               background-color: black; color: white; border: none; border-radius: 10px;
+                               background-color: green; color: white; border: none; border-radius: 10px;
                                cursor: pointer; transition: 0.3s;"
                         onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
                         onmouseout="this.style.backgroundColor='black'; this.style.color='white';">
