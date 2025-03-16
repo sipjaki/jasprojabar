450c210a3bc606ca99965219e3af2679c55afe58
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-17 00:20:04 +0700

    g

--- resources/views/frontend/07_ahsp/03_hspkonstruksiumum/01_divisi1/ahspdivisi1.blade.php
+++ resources/views/frontend/07_ahsp/03_hspkonstruksiumum/01_divisi1/ahspdivisi1.blade.php
@@ -74,11 +74,11 @@
                                     <tr><td></td><td>Air</td><td></td><td>liter</td><td>17.415</td><td>50</td><td>870,75</td></tr>
                                     <tr><td></td><td>Residu</td><td></td><td>liter</td><td>0.400</td><td>5.400</td><td>2.160,00</td></tr>
                                     <tr><td colspan="6" class="text-end"><strong>Jumlah Harga Bahan</strong></td><td><strong>239.612,25</strong></td></tr>
-                                    <tr><td>C</td><td>Peralatan</td><td></td><td></td><td></td><td></td><td></td></tr>
+                                    <tr><td style="text-align: center;">C</td><td style="text-align: center;">Peralatan</td><td></td><td></td><td></td><td></td><td></td></tr>
                                     <tr><td colspan="6" class="text-end"><strong>Jumlah Harga Alat</strong></td><td></td></tr>
-                                    <tr><td>D</td><td colspan="5" class="text-end">Jumlah Harga Tenaga Kerja, Bahan dan Peralatan (A+B+C)</td><td>346.221,25</td></tr>
-                                    <tr><td>E</td><td colspan="5" class="text-end">Biaya Umum dan Keuntungan (10%)</td><td>34.622,12</td></tr>
-                                    <tr><td>F</td><td colspan="5" class="text-end"><strong>Harga Satuan Pekerjaan (D+E)</strong></td><td><strong>380.843,37</strong></td></tr>
+                                    <tr><td style="text-align: center;">D</td><td colspan="5" class="text-end">Jumlah Harga Tenaga Kerja, Bahan dan Peralatan (A+B+C)</td><td>346.221,25</td></tr>
+                                    <tr><td style="text-align: center;">E</td><td colspan="5" class="text-end">Biaya Umum dan Keuntungan (10%)</td><td>34.622,12</td></tr>
+                                    <tr><td style="text-align: center;">F</td><td colspan="5" class="text-end"><strong>Harga Satuan Pekerjaan (D+E)</strong></td><td><strong>380.843,37</strong></td></tr>
                                 </tbody>
                             </table>
                         </div>
