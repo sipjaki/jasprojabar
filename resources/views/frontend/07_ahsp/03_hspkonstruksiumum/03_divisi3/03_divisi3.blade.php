0a16a5e7672919b9db12d104fec3d9691db92229
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-16 22:16:23 +0700

    dfgfdg

--- resources/views/frontend/07_ahsp/03_hspkonstruksiumum/03_divisi3/03_divisi3.blade.php
+++ resources/views/frontend/07_ahsp/03_hspkonstruksiumum/03_divisi3/03_divisi3.blade.php
@@ -240,6 +240,7 @@ color: #45a049;
                                                 {{-- <th onclick="sortTable(1)" style="cursor:pointer; text-align:center; width:150px;"> Paket  <span class="sort-icon">⇅</span></th>
                                                 <th onclick="sortTable(4)" style="cursor:pointer; text-align:center; width:250px;"> Item Pekerjaan <span class="sort-icon">⇅</span></th> --}}
                                                 <th onclick="sortTable(7)" style="cursor:pointer; text-align:center; width:600px;"> Jenis Pekerjaan <span class="sort-icon">⇅</span></th>
+                                                <th onclick="sortTable(7)" style="cursor:pointer; text-align:center; width:100px;"> Satuan <span class="sort-icon">⇅</span></th>
                                                 <th onclick="sortTable(6)" style="cursor:pointer; text-align:center; width:80px;"> Rp <span class="sort-icon">⇅</span></th>
                                                 <th onclick="sortTable(8)" style="cursor:pointer; text-align:center; width:200px;"> Harga Satuan <span class="sort-icon">⇅</span></th>
                                             </tr>
@@ -260,6 +261,7 @@ color: #45a049;
                                                 {{-- <td style="text-align: center;">{{$item->hsppaket3->hsppaket3}}</td>
                                                 <td style="text-align: center;">{{$item->hspkodepekerjaan3->namapekerjaan}}</td> --}}
                                                 <td style="cursor: pointer; color: blue; text-decoration: underline;" onclick="showModal('{{ $item->jenispekerjaan }}')">{{$item->jenispekerjaan}}</td>
+                                                <td style="text-align: center; color:red;" >{{$item->satuanmaterial}}</td>
                                                 <td style="text-align: center;">Rp</td>
                                                 <td style="text-align: right;">{{ number_format((float) $item->hargasatuan, 0, ',', '.') }},-</td>
                                             </tr>
