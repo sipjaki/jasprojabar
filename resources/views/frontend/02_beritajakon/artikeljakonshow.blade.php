172aa35be274c7cb6d9712a15badddea78408f88
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-16 21:30:52 +0700

    fd

--- resources/views/frontend/02_beritajakon/artikeljakonshow.blade.php
+++ resources/views/frontend/02_beritajakon/artikeljakonshow.blade.php
@@ -90,7 +90,7 @@
 						<div class="sidebar-widget sidebar-widget-recent-post">
 							<div style="display: flex; justify-content: center;">
                                 <button class="badge"
-                                   style="background-color: lightgreen; color: white; border: none; transition: 0.3s; padding:10px 20px; font-size: 16px; border-radius:15px;"
+                                   style="background-color: green; color: white; border: none; transition: 0.3s; padding:10px 20px; font-size: 16px; border-radius:15px; margin-bottom:20px;"
                                    onmouseover="this.style.backgroundColor='white'; this.style.color='black'; this.style.border='1px solid black';"
                                    onmouseout="this.style.backgroundColor='lightgreen'; this.style.color='white'; this.style.border='none';">
                                     <i class="fas fa-download" style="margin-right:5px;"></i> Download Artikel
