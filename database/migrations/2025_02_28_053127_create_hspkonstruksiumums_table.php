ab79b339ce9d13419743fba73f38b37841cf8973
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-16 22:54:29 +0700

    g

--- database/migrations/2025_02_28_053127_create_hspkonstruksiumums_table.php
+++ database/migrations/2025_02_28_053127_create_hspkonstruksiumums_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('hspkonstruksiumums', function (Blueprint $table) {
-            $table->id();
+            // $table->id();
+            $table->uuid('id')->primary(); // Gunakan UUID
             $table->foreignId('hspdivisi_id')->nullable();
             $table->foreignId('hsppaket_id')->nullable();
             $table->foreignId('hspkodepekerjaan_id')->nullable();
