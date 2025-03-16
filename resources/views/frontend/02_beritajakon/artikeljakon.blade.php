4be754d782c988577f1f975f7fd110348567dc98
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-16 21:10:03 +0700

    g

--- resources/views/frontend/02_beritajakon/artikeljakon.blade.php
+++ resources/views/frontend/02_beritajakon/artikeljakon.blade.php
@@ -51,7 +51,7 @@
                         <br>
 
                         <div style="display: inline-block;">
-                            <a id="downloadLink" href="#" target="_blank" class="btn btn-danger"
+                            <a id="downloadLink" href="{{ asset('storage/' . $item->berkas) }}" target="_blank" class="btn btn-danger"
                                style="background-color: navy; color: white; border: none; transition: 0.3s; padding:10px 20px; border-radius:15px;"
                                onmouseover="this.style.backgroundColor='white'; this.style.color='black'; this.style.border='1px solid black';"
                                onmouseout="this.style.backgroundColor='navy'; this.style.color='white'; this.style.border='none';">
@@ -59,20 +59,6 @@
                             </a>
                         </div>
 
-                        <script>
-                            document.addEventListener("DOMContentLoaded", function () {
-                                var downloadLink = document.getElementById("downloadLink");
-                                var fileUrl = "{{ asset('storage/' . $item->berkas) }}"; // Menggunakan asset() untuk storage
-
-                                if (fileUrl) {
-                                    downloadLink.href = fileUrl;
-                                    downloadLink.setAttribute("download", "{{$item->berkas}}"); // Memastikan file bisa diunduh langsung
-                                } else {
-                                    downloadLink.style.pointerEvents = "none";
-                                    downloadLink.style.opacity = "0.5";
-                                }
-                            });
-                        </script>
 
                     </div><!-- /.news-details-content-box -->
 
