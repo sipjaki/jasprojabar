f8816754d0aabbddcf7f0b956e328666983dcc53
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-18 01:15:25 +0700

    g

--- resources/views/frontend/07_ahsp/03_hspkonstruksiumum/05_divisi5/ahspdivisi5.blade.php
+++ resources/views/frontend/07_ahsp/03_hspkonstruksiumum/05_divisi5/ahspdivisi5.blade.php
@@ -55,8 +55,8 @@
                     <h4 style="font-family: 'Poppins', sans-serif;">
                         Kode AHSP :
                         {{ optional($data->hspdivisi)->id }}.
-                        {{ optional($data->hsppaket)->id }}.
-                        {{ optional($data->hspkodepekerjaan)->kodepekerjaan }}.
+                        {{ optional($data->hsppaket5)->id }}.
+                        {{ optional($data->hspkodepekerjaan5)->kodepekerjaan }}.
                         {{$data->kode}}
                     </h4>
                     <hr>
