001199f8574bd428b78c1ce0efa40b4870175e6f
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-19 20:14:24 +0700

    g

--- resources/views/backend/00_administrator/01_halamanutama/dashboard.blade.php
+++ resources/views/backend/00_administrator/01_halamanutama/dashboard.blade.php
@@ -649,13 +649,12 @@
                       <table class="table table-striped align-middle">
                         <thead>
                           <tr>
-                            <th>Product</th>
-                            <th>Price</th>
-                            <th>Sales</th>
-                            <th>More</th>
+                            <th>Daftar AHSP Konstruksi Umum</th>
+                            <th>Jumlah</th>
                           </tr>
                         </thead>
                         <tbody>
+
                           <tr>
                             <td>
                               <img
@@ -663,84 +662,144 @@
                                 alt="Product 1"
                                 class="rounded-circle img-size-32 me-2"
                               />
-                              Some Product
+                              Divisi I Persiapan Pekerjaan
                             </td>
-                            <td>$13 USD</td>
-                            <td>
-                              <small class="text-success me-1">
-                                <i class="bi bi-arrow-up"></i>
-                                12%
-                              </small>
-                              12,000 Sold
-                            </td>
+                            <td>----</td>
+
                             <td>
                               <a href="#" class="text-secondary"> <i class="bi bi-search"></i> </a>
                             </td>
                           </tr>
+
                           <tr>
                             <td>
                               <img
-                                src="../../dist/assets/img/default-150x150.png"
+                                src="../../assets/00_administrator/dist/assets/img/default-150x150.png"
                                 alt="Product 1"
                                 class="rounded-circle img-size-32 me-2"
                               />
-                              Another Product
+                              Divisi II Pekerjaan Struktur
                             </td>
-                            <td>$29 USD</td>
-                            <td>
-                              <small class="text-info me-1">
-                                <i class="bi bi-arrow-down"></i>
-                                0.5%
-                              </small>
-                              123,234 Sold
-                            </td>
+                            <td>----</td>
+
                             <td>
                               <a href="#" class="text-secondary"> <i class="bi bi-search"></i> </a>
                             </td>
                           </tr>
+
                           <tr>
                             <td>
                               <img
-                                src="../../dist/assets/img/default-150x150.png"
+                                src="../../assets/00_administrator/dist/assets/img/default-150x150.png"
                                 alt="Product 1"
                                 class="rounded-circle img-size-32 me-2"
                               />
-                              Amazing Product
+                              Divisi III Pekerjaan Arsitektur
                             </td>
-                            <td>$1,230 USD</td>
-                            <td>
-                              <small class="text-danger me-1">
-                                <i class="bi bi-arrow-down"></i>
-                                3%
-                              </small>
-                              198 Sold
-                            </td>
+                            <td>----</td>
+
                             <td>
                               <a href="#" class="text-secondary"> <i class="bi bi-search"></i> </a>
                             </td>
                           </tr>
+
                           <tr>
                             <td>
                               <img
-                                src="../../dist/assets/img/default-150x150.png"
+                                src="../../assets/00_administrator/dist/assets/img/default-150x150.png"
                                 alt="Product 1"
                                 class="rounded-circle img-size-32 me-2"
                               />
-                              Perfect Item
-                              <span class="badge text-bg-danger">NEW</span>
+                              Divisi IV Pekerjaan Lansekap
                             </td>
-                            <td>$199 USD</td>
-                            <td>
-                              <small class="text-success me-1">
-                                <i class="bi bi-arrow-up"></i>
-                                63%
-                              </small>
-                              87 Sold
-                            </td>
+                            <td>----</td>
+
                             <td>
                               <a href="#" class="text-secondary"> <i class="bi bi-search"></i> </a>
                             </td>
                           </tr>
+
+                          <tr>
+                            <td>
+                              <img
+                                src="../../assets/00_administrator/dist/assets/img/default-150x150.png"
+                                alt="Product 1"
+                                class="rounded-circle img-size-32 me-2"
+                              />
+                              Divisi V Pekerjaan Mekanikal & Elektrikal
+                            </td>
+                            <td>----</td>
+
+                            <td>
+                              <a href="#" class="text-secondary"> <i class="bi bi-search"></i> </a>
+                            </td>
+                          </tr>
+
+                          <tr>
+                            <td>
+                              <img
+                                src="../../assets/00_administrator/dist/assets/img/default-150x150.png"
+                                alt="Product 1"
+                                class="rounded-circle img-size-32 me-2"
+                              />
+                              Divisi VI Pekerjaan Plumbing
+                            </td>
+                            <td>----</td>
+
+                            <td>
+                              <a href="#" class="text-secondary"> <i class="bi bi-search"></i> </a>
+                            </td>
+                          </tr>
+
+                          <tr>
+                            <td>
+                              <img
+                                src="../../assets/00_administrator/dist/assets/img/default-150x150.png"
+                                alt="Product 1"
+                                class="rounded-circle img-size-32 me-2"
+                              />
+                              Divisi VII Jalan Pada Permukiman
+                            </td>
+                            <td>----</td>
+
+                            <td>
+                              <a href="#" class="text-secondary"> <i class="bi bi-search"></i> </a>
+                            </td>
+                          </tr>
+
+                          <tr>
+                            <td>
+                              <img
+                                src="../../assets/00_administrator/dist/assets/img/default-150x150.png"
+                                alt="Product 1"
+                                class="rounded-circle img-size-32 me-2"
+                              />
+                              Divisi VIII Drainase Jalan
+                            </td>
+                            <td>----</td>
+
+                            <td>
+                              <a href="#" class="text-secondary"> <i class="bi bi-search"></i> </a>
+                            </td>
+                          </tr>
+
+                          <tr>
+                            <td>
+                              <img
+                                src="../../assets/00_administrator/dist/assets/img/default-150x150.png"
+                                alt="Product 1"
+                                class="rounded-circle img-size-32 me-2"
+                              />
+                              Divisi IX Pekerjaan Pipa di Luar Bangunan Gedung
+                            </td>
+                            <td>----</td>
+
+                            <td>
+                              <a href="#" class="text-secondary"> <i class="bi bi-search"></i> </a>
+                            </td>
+                          </tr>
+
+
                         </tbody>
                       </table>
                     </div>
