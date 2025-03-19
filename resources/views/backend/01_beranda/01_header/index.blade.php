82eb8c317f08c353689f913718737c1fe1b93808
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-19 21:49:06 +0700

    g

--- resources/views/backend/01_beranda/01_header/index.blade.php
+++ resources/views/backend/01_beranda/01_header/index.blade.php
@@ -30,76 +30,63 @@
           <!--end::Container-->
         </div>
 
-
-
         <div class="container-fluid">
             <!--begin::Row-->
             <div class="row" style="margin-right: 10px; margin-left:10px;">
                 <!-- /.card -->
                 <div class="card mb-4">
-                  <div class="card-header">
-                    <h3 class="card-title">Halaman Pengaturan {{$title}}</h3>
-                  </div>
-                  <!-- /.card-header -->
-                  <div class="card-body p-0">
-                    <table class="table table-striped">
-                      <thead>
-                        <tr>
-                          <th style="width: 10px">No</th>
-                          <th style="width: 400px">Judul</th>
-                          <th style="width: 500px">Gambar/Foto</th>
-                          <th style="width: 100px">Actions</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-
-                        @foreach ($data as $item )
-
-                        <tr class="align-middle">
-                            <td>{{ $loop->iteration }}</td>
-                            <td>{{!!$item->judul!!}}</td>
-                            <td>
-                                <img src="{{ asset('storage/'. $item->header) }}" alt="Image" width="300">
-                            </td>
-
-                            {{-- <td>
-                                <div class="progress progress-xs">
-                                    <div
-                                    class="progress-bar progress-bar-danger"
-                                    style="width: 55%"
-                                    ></div>
-                                </div>
-                            </td> --}}
-                            <td>
-                                <!-- Show Icon -->
-                                <a href="/404" class="btn btn-sm btn-info" title="Show">
-                                    <i class="bi bi-eye"></i>
-                                </a>
-                                <!-- Update Icon -->
-                                <a href="/404" class="btn btn-sm btn-warning" title="Update">
-                                    <i class="bi bi-pencil-square"></i>
-                                </a>
-                                <!-- Delete Icon -->
-                                <a href="/404" class="btn btn-sm btn-danger" title="Delete">
-                                    <i class="bi bi-trash"></i>
-                                </a>
-                            </td>
-                            {{-- <td><span class="badge text-bg-danger">55%</span></td> --}}
-                        </tr>
-                        @endforeach
-
-                      </tbody>
-                    </table>
-                  </div>
-                  <!-- /.card-body -->
+                    <div class="card-header">
+                        <h3 class="card-title">Halaman Pengaturan {{$title}}</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body p-0">
+                        <div class="table-responsive">
+                            <table class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th style="width: 10px">No</th>
+                                        <th style="width: 400px">Judul</th>
+                                        <th style="width: 500px">Gambar/Foto</th>
+                                        <th style="width: 100px">Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($data as $item )
+                                    <tr class="align-middle">
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{!! $item->judul !!}</td>
+                                        <td>
+                                            <img src="{{ asset('storage/'. $item->header) }}" alt="Image" width="300">
+                                        </td>
+                                        <td>
+                                            <!-- Show Icon -->
+                                            <a href="/404" class="btn btn-sm btn-info" title="Show">
+                                                <i class="bi bi-eye"></i>
+                                            </a>
+                                            <!-- Update Icon -->
+                                            <a href="/404" class="btn btn-sm btn-warning" title="Update">
+                                                <i class="bi bi-pencil-square"></i>
+                                            </a>
+                                            <!-- Delete Icon -->
+                                            <a href="/404" class="btn btn-sm btn-danger" title="Delete">
+                                                <i class="bi bi-trash"></i>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <!-- /.card-body -->
                 </div>
                 <!-- /.card -->
-              </div>
-              <!-- /.col -->
             </div>
-            <!--end::Row-->
-          </div>
-          <!--end::Container-->
+            <!-- /.col -->
+        </div>
+        <!--end::Row-->
+        </div>
+                  <!--end::Container-->
         <!--end::App Content Header-->
         <!--begin::App Content-->
           <!--end::App Content-->
