687e0258b39b816e30769cc2f141b92d2afb63de
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-19 19:11:36 +0700

    g

--- resources/views/backend/00_administrator/01_halamanutama/dashboard.blade.php
+++ resources/views/backend/00_administrator/01_halamanutama/dashboard.blade.php
@@ -231,139 +231,90 @@
         <!--end::App Content Header-->
         <!--begin::App Content-->
         <div class="app-content">
-
-            <div class="row">
-                <div class="col-12 col-sm-6 col-md-3">
-                  <div class="info-box">
-                    <span class="info-box-icon text-bg-primary shadow-sm">
-                      <i class="bi bi-gear-fill"></i>
-                    </span>
-                    <div class="info-box-content">
-                      <span class="info-box-text">CPU Traffic</span>
-                      <span class="info-box-number">
-                        10
-                        <small>%</small>
-                      </span>
-                    </div>
-                    <!-- /.info-box-content -->
-                  </div>
-                  <!-- /.info-box -->
-                </div>
-                <!-- /.col -->
-                <div class="col-12 col-sm-6 col-md-3">
-                  <div class="info-box">
-                    <span class="info-box-icon text-bg-danger shadow-sm">
-                      <i class="bi bi-hand-thumbs-up-fill"></i>
-                    </span>
-                    <div class="info-box-content">
-                      <span class="info-box-text">Likes</span>
-                      <span class="info-box-number">41,410</span>
-                    </div>
-                    <!-- /.info-box-content -->
-                  </div>
-                  <!-- /.info-box -->
-                </div>
-                <!-- /.col -->
-                <!-- fix for small devices only -->
-                <!-- <div class="clearfix hidden-md-up"></div> -->
-                <div class="col-12 col-sm-6 col-md-3">
-                  <div class="info-box">
-                    <span class="info-box-icon text-bg-success shadow-sm">
-                      <i class="bi bi-cart-fill"></i>
-                    </span>
-                    <div class="info-box-content">
-                      <span class="info-box-text">Sales</span>
-                      <span class="info-box-number">760</span>
-                    </div>
-                    <!-- /.info-box-content -->
-                  </div>
-                  <!-- /.info-box -->
-                </div>
-                <!-- /.col -->
-                <div class="col-12 col-sm-6 col-md-3">
-                  <div class="info-box">
-                    <span class="info-box-icon text-bg-warning shadow-sm">
-                      <i class="bi bi-people-fill"></i>
-                    </span>
-                    <div class="info-box-content">
-                      <span class="info-box-text">New Members</span>
-                      <span class="info-box-number">2,000</span>
-                    </div>
-                    <!-- /.info-box-content -->
-                  </div>
-                  <!-- /.info-box -->
-                </div>
-                <!-- /.col -->
-              </div>
-              </div>
-            <div class="row">
-                <div class="col-12 col-sm-6 col-md-3">
-                  <div class="info-box">
-                    <span class="info-box-icon text-bg-primary shadow-sm">
-                      <i class="bi bi-gear-fill"></i>
-                    </span>
-                    <div class="info-box-content">
-                      <span class="info-box-text">CPU Traffic</span>
-                      <span class="info-box-number">
-                        10
-                        <small>%</small>
-                      </span>
-                    </div>
-                    <!-- /.info-box-content -->
-                  </div>
-                  <!-- /.info-box -->
-                </div>
-                <!-- /.col -->
-                <div class="col-12 col-sm-6 col-md-3">
-                  <div class="info-box">
-                    <span class="info-box-icon text-bg-danger shadow-sm">
-                      <i class="bi bi-hand-thumbs-up-fill"></i>
-                    </span>
-                    <div class="info-box-content">
-                      <span class="info-box-text">Likes</span>
-                      <span class="info-box-number">41,410</span>
-                    </div>
-                    <!-- /.info-box-content -->
-                  </div>
-                  <!-- /.info-box -->
-                </div>
-                <!-- /.col -->
-                <!-- fix for small devices only -->
-                <!-- <div class="clearfix hidden-md-up"></div> -->
-                <div class="col-12 col-sm-6 col-md-3">
-                  <div class="info-box">
-                    <span class="info-box-icon text-bg-success shadow-sm">
-                      <i class="bi bi-cart-fill"></i>
-                    </span>
-                    <div class="info-box-content">
-                      <span class="info-box-text">Sales</span>
-                      <span class="info-box-number">760</span>
-                    </div>
-                    <!-- /.info-box-content -->
-                  </div>
-                  <!-- /.info-box -->
-                </div>
-                <!-- /.col -->
-                <div class="col-12 col-sm-6 col-md-3">
-                  <div class="info-box">
-                    <span class="info-box-icon text-bg-warning shadow-sm">
-                      <i class="bi bi-people-fill"></i>
-                    </span>
-                    <div class="info-box-content">
-                      <span class="info-box-text">New Members</span>
-                      <span class="info-box-number">2,000</span>
-                    </div>
-                    <!-- /.info-box-content -->
-                  </div>
-                  <!-- /.info-box -->
-                </div>
-                <!-- /.col -->
-              </div>
-            </div>
           <!--begin::Container-->
           <div class="container-fluid">
             <!--begin::Row-->
-            <!--end::Row-->
+            <div class="row">
+                <div class="col-sm-6"><h3 class="mb-0">Dashboard v2</h3></div>
+                <div class="col-sm-6">
+                  <ol class="breadcrumb float-sm-end">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Dashboard v2</li>
+                  </ol>
+                </div>
+              </div>
+              <!--end::Row-->
+            </div>
+            <!--end::Container-->
+          </div>
+          <div class="app-content">
+            <!--begin::Container-->
+            <div class="container-fluid">
+              <!-- Info boxes -->
+              <div class="row">
+                <div class="col-12 col-sm-6 col-md-3">
+                  <div class="info-box">
+                    <span class="info-box-icon text-bg-primary shadow-sm">
+                      <i class="bi bi-gear-fill"></i>
+                    </span>
+                    <div class="info-box-content">
+                      <span class="info-box-text">CPU Traffic</span>
+                      <span class="info-box-number">
+                        10
+                        <small>%</small>
+                      </span>
+                    </div>
+                    <!-- /.info-box-content -->
+                  </div>
+                  <!-- /.info-box -->
+                </div>
+                <!-- /.col -->
+                <div class="col-12 col-sm-6 col-md-3">
+                  <div class="info-box">
+                    <span class="info-box-icon text-bg-danger shadow-sm">
+                      <i class="bi bi-hand-thumbs-up-fill"></i>
+                    </span>
+                    <div class="info-box-content">
+                      <span class="info-box-text">Likes</span>
+                      <span class="info-box-number">41,410</span>
+                    </div>
+                    <!-- /.info-box-content -->
+                  </div>
+                  <!-- /.info-box -->
+                </div>
+                <!-- /.col -->
+                <!-- fix for small devices only -->
+                <!-- <div class="clearfix hidden-md-up"></div> -->
+                <div class="col-12 col-sm-6 col-md-3">
+                  <div class="info-box">
+                    <span class="info-box-icon text-bg-success shadow-sm">
+                      <i class="bi bi-cart-fill"></i>
+                    </span>
+                    <div class="info-box-content">
+                      <span class="info-box-text">Sales</span>
+                      <span class="info-box-number">760</span>
+                    </div>
+                    <!-- /.info-box-content -->
+                  </div>
+                  <!-- /.info-box -->
+                </div>
+                <!-- /.col -->
+                <div class="col-12 col-sm-6 col-md-3">
+                  <div class="info-box">
+                    <span class="info-box-icon text-bg-warning shadow-sm">
+                      <i class="bi bi-people-fill"></i>
+                    </span>
+                    <div class="info-box-content">
+                      <span class="info-box-text">New Members</span>
+                      <span class="info-box-number">2,000</span>
+                    </div>
+                    <!-- /.info-box-content -->
+                  </div>
+                  <!-- /.info-box -->
+                </div>
+                <!-- /.col -->
+              </div>
+              <!-- /.row -->
             <!--begin::Row-->
             <div class="row">
               <!-- Start col -->
