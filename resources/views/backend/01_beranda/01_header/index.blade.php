8911bf66e44d1a123279bd46002fc1cb004587f2
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-19 21:25:35 +0700

    g

--- resources/views/backend/01_beranda/01_header/index.blade.php
+++ resources/views/backend/01_beranda/01_header/index.blade.php
@@ -23,13 +23,78 @@
               <div class="col-sm-6"><h3 class="mb-0">Selamat datang ! <span style="color: black; font-weight:800;" > {{ Auth::user()->name }}</span> di Dashboard <span style="color: black; font-weight:800;"> {{ Auth::user()->statusadmin->statusadmin }} </span>  Sistem Informasi Pembina Jasa Konstruksi Kab Blora</h3></div>
 
 
+
             </div>
             <!--end::Row-->
           </div>
           <!--end::Container-->
         </div>
 
-
+                <div class="card mb-4">
+                  <div class="card-header">
+                    <h3 class="card-title">Striped Full Width Table</h3>
+                  </div>
+                  <!-- /.card-header -->
+                  <div class="card-body p-0">
+                    <table class="table table-striped">
+                      <thead>
+                        <tr>
+                          <th style="width: 10px">#</th>
+                          <th>Task</th>
+                          <th>Progress</th>
+                          <th style="width: 40px">Label</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr class="align-middle">
+                          <td>1.</td>
+                          <td>Update software</td>
+                          <td>
+                            <div class="progress progress-xs">
+                              <div
+                                class="progress-bar progress-bar-danger"
+                                style="width: 55%"
+                              ></div>
+                            </div>
+                          </td>
+                          <td><span class="badge text-bg-danger">55%</span></td>
+                        </tr>
+                        <tr class="align-middle">
+                          <td>2.</td>
+                          <td>Clean database</td>
+                          <td>
+                            <div class="progress progress-xs">
+                              <div class="progress-bar text-bg-warning" style="width: 70%"></div>
+                            </div>
+                          </td>
+                          <td><span class="badge text-bg-warning">70%</span></td>
+                        </tr>
+                        <tr class="align-middle">
+                          <td>3.</td>
+                          <td>Cron job running</td>
+                          <td>
+                            <div class="progress progress-xs progress-striped active">
+                              <div class="progress-bar text-bg-primary" style="width: 30%"></div>
+                            </div>
+                          </td>
+                          <td><span class="badge text-bg-primary">30%</span></td>
+                        </tr>
+                        <tr class="align-middle">
+                          <td>4.</td>
+                          <td>Fix and squish bugs</td>
+                          <td>
+                            <div class="progress progress-xs progress-striped active">
+                              <div class="progress-bar text-bg-success" style="width: 90%"></div>
+                            </div>
+                          </td>
+                          <td><span class="badge text-bg-success">90%</span></td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
         <!--end::App Content Header-->
         <!--begin::App Content-->
           <!--end::App Content-->
