7fced693375f83ec395a7b943c87171df615138f
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-21 06:30:17 +0700

    g

--- resources/views/backend/03_beritajakon/01_beritajakon/index.blade.php
+++ resources/views/backend/03_beritajakon/01_beritajakon/index.blade.php
@@ -48,7 +48,7 @@
                     </div>
                     <!-- /.card-header -->
                     <div class="card-body p-0">
-                        <div class="table-responsive">
+                        <div class="table-responsive" style="overflow-x: auto; white-space: nowrap;">
                             <table class="table table-striped">
                                 <thead>
                                     <tr>
@@ -56,7 +56,7 @@
                                         <th style="width: 200px; text-align:center;">Penulis</th>
                                         <th style="width: 250px; text-align:center;">Judul Berita</th>
                                         <th style="width: 100px; text-align: center;">Tanggal</th>
-                                        <th style="width: 500px; text-align: center;">isi Berita</th>
+                                        <th style="width: 500px; text-align: center;">Isi Berita</th>
                                         <th style="width: 300px; text-align: center;">Foto</th>
                                         <th style="width: 150px; text-align: center;">Aksi</th>
                                     </tr>
@@ -73,81 +73,64 @@
                                             <img src="{{ asset('storage/'. $item->foto) }}" alt="Image" width="300">
                                         </td>
                                         <td style="text-align: center;">
-                                            <!-- Show Icon -->
                                             <a href="/404" class="btn btn-sm btn-info me-2" title="Show">
                                                 <i class="bi bi-eye"></i>
                                             </a>
-                                            <!-- Update Icon -->
                                             <a href="/404" class="btn btn-sm btn-warning me-2" title="Update">
                                                 <i class="bi bi-pencil-square"></i>
                                             </a>
-                                            <!-- Delete Icon -->
-                                            <!-- Tombol Delete -->
                                             <a href="javascript:void(0)" class="btn btn-sm btn-danger" title="Delete" data-bs-toggle="modal" data-bs-target="#deleteModal"
                                             data-judul="{{ $item->judulberita }}" onclick="setDeleteUrl(this)">
                                             <i class="bi bi-trash"></i>
-                                        </a>
-<!-- Modal Konfirmasi Hapus -->
-                            <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
-                                <div class="modal-dialog">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <img src="/assets/icon/pupr.png" alt="" width="30" style="margin-right: 10px;">
-                                            <h5 class="modal-title" id="deleteModalLabel">DPUPR Kabupaten Blora</h5>
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                        </div>
-                                        <div class="modal-body">
-                                            <p>Apakah Anda Ingin Menghapus Data : <span id="itemName"></span>?</p>
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-                                            <form id="deleteForm" method="POST" action="">
-                                                @csrf
-                                                @method('DELETE') <!-- Menetapkan metode DELETE untuk penghapusan -->
-                                                <button type="submit" class="btn btn-danger">Hapus</button>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-
-                            <script>
-                            function setDeleteUrl(button) {
-                                // Ambil data judul dari elemen yang diklik
-                                var judulberita = button.getAttribute('data-judul');
-
-                                // Perbarui teks di dalam modal dengan nama item
-                                document.getElementById('itemName').innerText = judulberita;
-
-                                // Atur URL penghapusan
-                                var deleteUrl = "/beberitajakon/delete/" + encodeURIComponent(judulberita);
-                                document.getElementById('deleteForm').action = deleteUrl;
-                            }
-
-
-                            </script>
-
-                                            <style>
-                                                /* Hover effect */
-                                                .btn-info:hover, .btn-warning:hover, .btn-danger:hover {
-                                                    background-color: #fff !important; /* Keep the background white on hover */
-                                                    color: black !important; /* Change text color to black on hover */
-                                                }
-
-                                                .btn-info:hover i, .btn-warning:hover i, .btn-danger:hover i {
-                                                    color: black !important; /* Ensure the icon color changes to black */
-                                                }
-                                            </style>
+                                            </a>
                                         </td>
-
                                     </tr>
                                     @endforeach
                                 </tbody>
                             </table>
                         </div>
                     </div>
-                    <!-- /.card-body -->
+
+                    <!-- Modal Konfirmasi Hapus -->
+                    <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <img src="/assets/icon/pupr.png" alt="" width="30" style="margin-right: 10px;">
+                                    <h5 class="modal-title" id="deleteModalLabel">DPUPR Kabupaten Blora</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <p>Apakah Anda Ingin Menghapus Data : <span id="itemName"></span>?</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                                    <form id="deleteForm" method="POST" action="">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger">Hapus</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <script>
+                    function setDeleteUrl(button) {
+                        var judulberita = button.getAttribute('data-judul');
+                        document.getElementById('itemName').innerText = judulberita;
+                        var deleteUrl = "/beberitajakon/delete/" + encodeURIComponent(judulberita);
+                        document.getElementById('deleteForm').action = deleteUrl;
+                    }
+                    </script>
+
+                    <style>
+                        .table-responsive {
+                            max-width: 100%;
+                            overflow-x: auto;
+                        }
+                    </style>
+
                 </div>
                 <!-- /.card -->
             </div>
