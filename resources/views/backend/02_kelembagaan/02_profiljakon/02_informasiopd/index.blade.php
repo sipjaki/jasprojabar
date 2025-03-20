471be9f5215a4f437eafbe385003d00dbd6d3b90
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-21 00:03:18 +0700

    g

--- resources/views/backend/02_kelembagaan/02_profiljakon/02_informasiopd/index.blade.php
+++ resources/views/backend/02_kelembagaan/02_profiljakon/02_informasiopd/index.blade.php
@@ -144,7 +144,7 @@
                             <button
                             onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
                             onmouseout="this.style.backgroundColor='#156f2a'; this.style.color='white';"
-                            style="background-color: #156f2a; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 5px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
+                            style="background-color: #156f2a; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
                             <!-- Ikon Kembali -->
                             <i class="fa fa-file" style="margin-right: 8px;"></i>
                             Update
@@ -154,7 +154,7 @@
                             <button
                             onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
                             onmouseout="this.style.backgroundColor='navy'; this.style.color='white';"
-                            style="background-color: navy; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 5px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
+                            style="background-color: navy; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
                             <!-- Ikon Kembali -->
                             <i class="fa fa-arrow-left" style="margin-right: 8px;"></i>
                             Kembali
