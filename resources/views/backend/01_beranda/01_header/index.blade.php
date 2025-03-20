baf2a013d881c902de8be5d75ca5443123f236ea
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-20 07:14:06 +0700

    g

--- resources/views/backend/01_beranda/01_header/index.blade.php
+++ resources/views/backend/01_beranda/01_header/index.blade.php
@@ -88,27 +88,7 @@
                                                 <i class="bi bi-trash"></i>
                                             </a>
 
-
-<!-- Modal Konfirmasi Hapus -->
-<div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <img src="/assets/icon/logokabupatenblora.png" alt="" width="40" style="margin-right: 10px;">
-                <h5 class="modal-title" id="deleteModalLabel">Konfirmasi Hapus Data</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <p>Apakah Anda akan menghapus data ini? {{ Auth::user()->name }} </p>
-                <p id="itemName" class="font-weight-bold"></p> <!-- Menampilkan nama item atau detail -->
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-                <a id="confirmDelete" href="" class="btn btn-danger">Delete</a>
-            </div>
-        </div>
-    </div>
-</div>
+@include('backend.00_administrator.00_baganterpisah.05_modalcarddelete')
 
                                             <style>
                                                 /* Hover effect */
