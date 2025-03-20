71c85cf1e5ec7eb9ffb00990f0c13b9b0c53b695
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-20 08:26:18 +0700

    g

--- resources/views/backend/00_administrator/00_baganterpisah/05_modalcarddelete.blade.php
+++ resources/views/backend/00_administrator/00_baganterpisah/05_modalcarddelete.blade.php
@@ -3,7 +3,8 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="deleteModalLabel">Konfirmasi Penghapusan</h5>
+                <img src="/assets/icon/pupr.png" alt="" width="30">
+                <h5 class="modal-title" id="deleteModalLabel">DPUPR Kab Blora</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
