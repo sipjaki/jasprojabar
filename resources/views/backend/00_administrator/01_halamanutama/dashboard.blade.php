a4231cd72e423fe20d2749599a1e9f730ef61bc2
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-21 01:26:05 +0700

    g

--- resources/views/backend/00_administrator/01_halamanutama/dashboard.blade.php
+++ resources/views/backend/00_administrator/01_halamanutama/dashboard.blade.php
@@ -20,7 +20,7 @@
             <!--begin::Row-->
             <div class="row">
 
-              <div class="col-sm-6"><h3 class="mb-0">Selamat datang ! <span style="color: black; font-weight:800;" > {{ Auth::user()->name }}</span> di Dashboard <span style="color: black; font-weight:800;"> {{ Auth::user()->statusadmin->statusadmin }} </span>  Sistem Informasi Pembina Jasa Konstruksi Kab Blora</h3></div>
+              <div class="col-sm-12"><h3 class="mb-0">Selamat datang ! <span style="color: black; font-weight:800;" > {{ Auth::user()->name }}</span> di Dashboard <span style="color: black; font-weight:800;"> {{ Auth::user()->statusadmin->statusadmin }} </span>  Sistem Informasi Pembina Jasa Konstruksi Kab Blora</h3></div>
 
 
             </div>
