1d46b8e91c5823c429694cc5c8d1166efacb9743
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-20 07:39:51 +0700

    g

--- resources/views/backend/01_beranda/01_header/index.blade.php
+++ resources/views/backend/01_beranda/01_header/index.blade.php
@@ -45,7 +45,7 @@
                 <!-- /.card -->
                 <div class="card mb-4">
                     <div class="card-header">
-                        <h3 class="card-title">Halaman Pengaturan {{$title}}</h3>
+                        <h2 style="color: black;" class="card-title">Halaman Pengaturan {{$title}}</h2>
                     </div>
                     <!-- /.card-header -->
                     <div class="card-body p-0">
