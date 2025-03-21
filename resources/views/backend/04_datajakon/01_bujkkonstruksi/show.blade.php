c17d83473a126d5abfbdb42ff2da3b7146f270dc
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-21 23:35:24 +0700

    g

--- resources/views/backend/04_datajakon/01_bujkkonstruksi/show.blade.php
+++ resources/views/backend/04_datajakon/01_bujkkonstruksi/show.blade.php
@@ -140,7 +140,7 @@
                     <!-- Button Section -->
                     <br><br>
                     <div style="display: flex; justify-content: flex-end; margin-bottom: 20px;">
-                        <a href="/bekepaladinas/update/{{$item->namalengkap}}">
+                        <a href="/bekepaladinas/update/{{$data->namalengkap}}">
                             <button
                             onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
                             onmouseout="this.style.backgroundColor='#156f2a'; this.style.color='white';"
