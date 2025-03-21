8cd8a435d56eb79c4f7a68389f5e664133f4310d
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-21 22:36:44 +0700

    g

--- resources/views/backend/04_datajakon/01_bujkkonstruksi/index.blade.php
+++ resources/views/backend/04_datajakon/01_bujkkonstruksi/index.blade.php
@@ -132,9 +132,8 @@
                 <button
                 onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
                 onmouseout="this.style.backgroundColor='navy'; this.style.color='white';"
-                style="background-color: navy; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
+                style="background-color: navy; color: white; border: none; margin-right: 10px; padding: 5px 20px; border-radius: 15px; font-size: 14px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
                 <!-- Ikon Kembali -->
-                <i class="fa fa-plus" style="margin-right: 8px;"></i>
                 Lihat
             </button>
             </a>
