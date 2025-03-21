d7d27a38e3c4df15460491af1463eca5a43228e3
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-21 23:29:35 +0700

    g

--- resources/views/backend/04_datajakon/01_bujkkonstruksi/index.blade.php
+++ resources/views/backend/04_datajakon/01_bujkkonstruksi/index.blade.php
@@ -158,10 +158,10 @@
         </td>
 
          <td style="text-align: center; vertical-align: middle;">
-             <a href="/404" class="btn btn-sm btn-info me-2" title="Show">
+             <a href="/bebujkkonstruksi/show/{{$item->namalengkap}}" class="btn btn-sm btn-info me-2" title="Show">
                  <i class="bi bi-eye"></i>
              </a>
-             <a href="//bebujkkonstruksi/show/{{$item->namalengkap}}" class="btn btn-sm btn-warning me-2" title="Update">
+             <a href="/404" class="btn btn-sm btn-warning me-2" title="Update">
                  <i class="bi bi-pencil-square"></i>
              </a>
              <a href="javascript:void(0)" class="btn btn-sm btn-danger" title="Delete"
