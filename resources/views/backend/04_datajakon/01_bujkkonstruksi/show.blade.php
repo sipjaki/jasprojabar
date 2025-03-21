c410dada441e8a6f300c00749163321b4a535fae
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-21 23:30:45 +0700

    g

--- resources/views/backend/04_datajakon/01_bujkkonstruksi/show.blade.php
+++ resources/views/backend/04_datajakon/01_bujkkonstruksi/show.blade.php
@@ -38,6 +38,8 @@
 
         {{-- ======================================================= --}}
 
+            <!-- Menyertakan FontAwesome untuk ikon -->
+
         <div class="container-fluid">
             <!--begin::Row-->
             <div class="row" style="margin-right: 10px; margin-left:10px;">
@@ -47,119 +49,106 @@
                         <h2 style="color: black;" class="card-title">Halaman Pengaturan : {{$title}} </h2>
 
                     </div>
-                    <!-- /.card-header -->
-                    <div class="card-body p-0">
-                        <div class="table-responsive">
-                            <table class="table table-striped">
-                                <thead>
-                                    <tr>
-                                        <th style="width: 10px; text-align:center;">No</th>
-                                        <th style="width: 500px; text-align:center;">Jabatan</th>
-                                        <th style="width: 500px; text-align:center;">Nama Lengkap</th>
-                                        <th style="width: 200px; text-align: center;">Aksi</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($data as $item )
-                                    <tr class="align-middle">
-                                        <td style="text-align: center;">{{ $loop->iteration }}</td>
-                                        <td style="text-align: center;">{!! $item->jabatan !!}</td>
-                                        <td style="text-align: center;">{!! $item->namalengkap !!}</td>
-                                        <td style="text-align: center;">
-                                            <!-- Show Icon -->
-                                            {{-- <a href="/404" class="btn btn-sm btn-info me-2" title="Show">
-                                                <i class="bi bi-eye"></i>
-                                            </a> --}}
-                                            <!-- Update Icon -->
-                                            <a href="/404" class="btn btn-sm btn-warning me-2" title="Update">
-                                                <i class="bi bi-pencil-square"></i>
-                                            </a>
-                                            <!-- Delete Icon -->
-                                            <!-- Tombol Delete -->
-                                            <a href="javascript:void(0)" class="btn btn-sm btn-danger" title="Delete" data-bs-toggle="modal" data-bs-target="#deleteModal"
-                                            data-judul="{{ $item->namalengkap }}" onclick="setDeleteUrl(this)">
-                                            <i class="bi bi-trash"></i>
-                                        </a>
 
-                                        <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
-                                            <div class="modal-dialog">
-                                                <div class="modal-content">
-                                                    <div class="modal-header">
-                                                        <img src="/assets/icon/pupr.png" alt="" width="30" style="margin-right: 10px;">
-                                                        <h5 class="modal-title" id="deleteModalLabel">DPUPR Kabupaten Blora</h5>
-                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                    </div>
-                                                    <div class="modal-body">
-                                                        <p>Apakah Anda Ingin Menghapus Data : <span id="itemName"></span>?</p>
-                                                    </div>
-                                                    <div class="modal-footer">
-                                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-                                                        <form id="deleteForm" method="POST" action="">
-                                                            @csrf
-                                                            @method('DELETE') <!-- Menetapkan metode DELETE untuk penghapusan -->
-                                                            <button type="submit" class="btn btn-danger">Hapus</button>
-                                                        </form>
-                                                    </div>
-                                                </div>
+                    <div class="col-md-12">
+                        <!--begin::Quick Example-->
+                        <div class="card card-primary card-outline mb-6">
+                            <!--begin::Header-->
+                            {{-- <div class="card-header"><div class="card-title">Quick Example</div></div> --}}
+                            <!--end::Header-->
+                            <!--begin::Form-->
+
+                            @foreach ($data as $item)
+
+                            <form>
+                                <!--begin::Body-->
+                                <div class="card-body">
+                                    <div class="row">
+                                        <!-- Left Column (6/12) -->
+                                        <div class="col-md-6">
+                                            <div class="mb-3">
+                                                <label class="form-label">Nama Lengkap</label>
+                                                <input class="form-control" value="{{$item->namalengkap}}" readonly/>
+                                                <div class="form-text"></div>
                                             </div>
+
+                                            <div class="mb-3">
+                                                <label class="form-label">NIP </label>
+                                                <input class="form-control" value="{{$item->nip}} " readonly />
+                                            </div>
+
+                                            <div class="mb-3">
+                                                <label class="form-label">Tempat Tanggal Lahir</label>
+                                                <input class="form-control" value="{{$item->ttl}}" readonly/>
+                                            </div>
+
+                                            <div class="mb-3">
+                                                <label class="form-label">Pangkat Golongan</label>
+                                                <input class="form-control" value="{{$item->pangkatgolongan}}" readonly/>
+                                            </div>
+
                                         </div>
+                                        <!-- End Left Column -->
 
+                                        <!-- Right Column (6/12) -->
+                                        <div class="col-md-6">
+                                            <div class="mb-3">
+                                                <label class="form-label">Jabatan</label>
+                                                <input class="form-control" value="{{$item->jabatan}}" readonly/>
+                                            </div>
 
-                                        <script>
-                                        function setDeleteUrl(button) {
-                                            // Ambil data judul dari elemen yang diklik
-                                            var namalengkap = button.getAttribute('data-judul');
+                                            <div class="mb-3">
+                                                <label class="form-label">Perangkat Daerah</label>
+                                                <input class="form-control" value="{{$item->perangkatdaerah}}" readonly/>
+                                            </div>
 
-                                            // Perbarui teks di dalam modal dengan nama item
-                                            document.getElementById('itemName').innerText = namalengkap;
+                                            <div class="mb-3">
+                                                <label class="form-label">Pendidikan Terakhir</label>
+                                                <input class="form-control" value="{{$item->pendidikanterakhir}}" readonly/>
+                                            </div>
 
-                                            // Atur URL penghapusan
-                                            var deleteUrl = "/bejabatan/delete/" + encodeURIComponent(namalengkap);
-                                            document.getElementById('deleteForm').action = deleteUrl;
-                                        }
+                                        </div>
+                                        <!-- End Right Column -->
 
-
-                                        </script>
-
-                                            <style>
-                                                /* Hover effect */
-                                                .btn-info:hover, .btn-warning:hover, .btn-danger:hover {
-                                                    background-color: #fff !important; /* Keep the background white on hover */
-                                                    color: black !important; /* Change text color to black on hover */
-                                                }
-
-                                                .btn-info:hover i, .btn-warning:hover i, .btn-danger:hover i {
-                                                    color: black !important; /* Ensure the icon color changes to black */
-                                                }
-                                            </style>
-                                        </td>
-
-                                    </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-
-                            <div style="display: flex; justify-content: flex-end; margin-bottom: 20px;">
-
-                                <a href="/beprofiljakon">
-                                    <button
-                                    onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
-                                    onmouseout="this.style.backgroundColor='navy'; this.style.color='white';"
-                                    style="background-color: navy; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
-                                    <!-- Ikon Kembali -->
-                                    <i class="fa fa-arrow-left" style="margin-right: 8px;"></i>
-                                    Kembali
-                                </button>
-                            </a>
+                                    </div> <!-- end row -->
+                                </div>
+                            </form>
+                            <!--end::Form-->
                         </div>
+                        <!--end::Quick Example-->
 
-                        </div>
                     </div>
-                    <!-- /.card-body -->
+                    <!-- /.card -->
+                    <!-- Button Section -->
+                    <br><br>
+                    <div style="display: flex; justify-content: flex-end; margin-bottom: 20px;">
+                        <a href="/bekepaladinas/update/{{$item->namalengkap}}">
+                            <button
+                            onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
+                            onmouseout="this.style.backgroundColor='#156f2a'; this.style.color='white';"
+                            style="background-color: #156f2a; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
+                            <!-- Ikon Kembali -->
+                            <i class="fa fa-file" style="margin-right: 8px;"></i>
+                            Update
+                        </button>
+                        </a>
+                        <a href="/beprofiljakon">
+                            <button
+                            onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
+                            onmouseout="this.style.backgroundColor='navy'; this.style.color='white';"
+                            style="background-color: navy; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
+                            <!-- Ikon Kembali -->
+                            <i class="fa fa-arrow-left" style="margin-right: 8px;"></i>
+                            Kembali
+                        </button>
+                    </a>
                 </div>
-                <!-- /.card -->
-            </div>
-            <!-- /.col -->
+
+                @endforeach
+                    </div>
+                    <!--end::Row-->
+                    </div>
+
         </div>
         <!--end::Row-->
         </div>
