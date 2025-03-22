57fd93bd783742b8f49e4263bcf7ed281142ebb1
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-22 21:11:53 +0700

    g

--- resources/views/backend/04_datajakon/02_bujkkonsultan/index.blade.php
+++ resources/views/backend/04_datajakon/02_bujkkonsultan/index.blade.php
@@ -125,7 +125,7 @@
                             function searchTable() {
                             let input = document.getElementById("searchInput").value;
 
-                            fetch(`/bebujkkonstruksi?search=${input}`)
+                            fetch(`/bebujkkonsultan?search=${input}`)
                                 .then(response => response.text())
                                 .then(html => {
                                     let parser = new DOMParser();
