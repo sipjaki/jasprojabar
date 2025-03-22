d00aefaebe48b680d8a40dc34ac3a545020b9f51
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-22 21:38:16 +0700

    g

--- resources/views/backend/04_datajakon/03_asosiasimasjaki/index.blade.php
+++ resources/views/backend/04_datajakon/03_asosiasimasjaki/index.blade.php
@@ -96,7 +96,7 @@
                             function searchTable() {
                             let input = document.getElementById("searchInput").value;
 
-                            fetch(`/bebujkkonsultan?search=${input}`)
+                            fetch(`/beasosiasi?search=${input}`)
                                 .then(response => response.text())
                                 .then(html => {
                                     let parser = new DOMParser();
