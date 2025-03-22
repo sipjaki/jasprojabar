bb3b344c57995fe10da72c9080f6576a403623c0
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-22 20:45:36 +0700

    g

--- resources/views/backend/00_administrator/00_baganterpisah/07_paginations.blade.php
+++ resources/views/backend/00_administrator/00_baganterpisah/07_paginations.blade.php
@@ -32,7 +32,7 @@
                 style="background-color: #14532D; color: white; padding: 8px 15px; border-radius: 5px; text-decoration: none;
                 display: flex; align-items: center; transition: all 0.3s ease; border: 1px solid #14532D;"
                 onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
-                onmouseout="this.style.backgroundColor='#14532D'; this.style.color='white';">
+                onmouseout="this.style.backgroundColor='#14532D'; this.style.color='black';">
                 <i class="fas fa-arrow-left" style="margin-right: 8px;"></i> Previous
             </a>
         </li>
