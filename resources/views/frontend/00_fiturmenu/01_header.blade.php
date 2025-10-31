db22a8ea0f387d29a240f9c02696e9a99e0ed514
SipjakiBlora <sipjakiblora@gmail.com>
2025-10-31 19:48:23 +0700

    erg

--- resources/views/frontend/00_fiturmenu/01_header.blade.php
+++ resources/views/frontend/00_fiturmenu/01_header.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>{{$title}}</title>
-    <meta name="description" content="Beeko -  Business Consulting HTML Template">
+    <meta name="description" content="Rekapin Aja ">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="shortcut icon" type="image/x-icon" href="/assets/gambar/logo2.png">
